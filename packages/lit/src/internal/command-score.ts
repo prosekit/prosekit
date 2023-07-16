e8d7ff15089c17f3d777a152b77bd35e2fceda05
ocavue <ocavue@gmail.com>
2023-07-16 13:41:10 +0800

    review

--- packages/lit/src/internal/command-score.ts
+++ packages/lit/src/internal/command-score.ts
@@ -1,6 +1,6 @@
 // @ts-expect-error: no types
 import commandScoreModule from '@superhuman/command-score'
 
-type CommandScore = (string: string, abbreviation: string) => number
+type CommandScore = (item: string, query: string) => number
 
 export const commandScore: CommandScore = commandScoreModule as CommandScore

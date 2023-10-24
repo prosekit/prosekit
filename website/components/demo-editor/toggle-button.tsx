b7cdba75c4b2092dd46e10abb5ddbc9890eb9b76
ocavue <ocavue@gmail.com>
2023-10-24 19:35:52 +0800

    fix ToggleButton

--- website/components/demo-editor/toggle-button.tsx
+++ website/components/demo-editor/toggle-button.tsx
@@ -1,23 +1,19 @@
 import { defineComponent } from 'vue'
 
-export const ToggleButton = defineComponent({
-  name: 'ToggleButton',
-  props: {
-    active: Boolean,
-    available: Boolean,
-    onChange: Function,
-  },
-  setup(props, { slots }) {
-    return () => (
-      <button
-        class="TOGGLE_BUTTON"
-        disabled={!props.available}
-        data-state={props.active ? 'on' : 'off'}
-        onClick={() => props.onChange?.()}
-        onMousedown={(event: MouseEvent) => event.preventDefault()}
-      >
-        {slots.default?.()}
-      </button>
-    )
-  },
+export const ToggleButton = defineComponent<{
+  active?: boolean
+  available: boolean
+  onChange: VoidFunction
+}>((props, { slots }) => {
+  return () => (
+    <button
+      class="TOGGLE_BUTTON"
+      disabled={!props.available}
+      data-state={props.active ? 'on' : 'off'}
+      onClick={() => props.onChange?.()}
+      onMousedown={(event: MouseEvent) => event.preventDefault()}
+    >
+      {slots.default?.()}
+    </button>
+  )
 })

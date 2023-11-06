e9598d632f0e38da09d5e0585d3540151e55a700
ocavue <ocavue@gmail.com>
2023-11-06 14:05:30 +0800

    Fix typecheck

--- packages/core/src/editor/editor.spec.ts
+++ packages/core/src/editor/editor.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest'
 
+import { union } from '..'
 import { defineDoc } from '../extensions/doc'
 import { defineParagraph } from '../extensions/paragraph'
 import { defineText } from '../extensions/text'
@@ -9,8 +10,8 @@ import { createEditor } from './editor'
 describe('createEditor', () => {
   it('can mount the editor', () => {
     const div = document.body.appendChild(document.createElement('div'))
-    const extension = [defineDoc(), defineText(), defineParagraph()]
-    const editor = createEditor({ extension: { extension } })
+    const extension = union([defineDoc(), defineText(), defineParagraph()])
+    const editor = createEditor({ extension })
     editor.mount(div)
     expect(div.outerHTML).toMatchInlineSnapshot(
       '"<div contenteditable=\\"true\\" translate=\\"no\\" class=\\"ProseMirror\\"><p><br class=\\"ProseMirror-trailingBreak\\"></p></div>"',

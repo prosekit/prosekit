b0ef524d9970e9febe5deb4463f95422192777c1
ocavue <ocavue@gmail.com>
2023-11-12 23:34:35 +0800

    use vfs

--- packages/dev/src/gen-tsconfig-json.ts
+++ packages/dev/src/gen-tsconfig-json.ts
@@ -1,21 +1,14 @@
 import path from 'node:path'
 
-import { findPublicPackages } from './find-public-packages.js'
-import { findRootDir } from './find-root-dir.js'
-import { readJson } from './read-json.js'
 import { skipGen } from './skip-gen.js'
-import { writeJson } from './write-json.js'
+import { vfs } from './virtual-file-system.js'
 
-// TODO: use vfs
 export async function genTsconfigJson() {
   if (skipGen()) return
 
-  const packages = await findPublicPackages()
-  const rootDir = await findRootDir()
-  const tsconfigPath = path.join(rootDir, 'tsconfig.json')
-  const tsconfig = await readJson(tsconfigPath)
-
+  const packages = await vfs.getPublicPackages()
+  const tsconfigFile = await vfs.getFile('tsconfig.json')
+  const tsconfig = await tsconfigFile.readJSON()
   tsconfig.references = packages.map((pkg) => ({ path: pkg.relativeDir }))
-
-  await writeJson(tsconfigPath, tsconfig)
+  tsconfigFile.updateJSON(tsconfig)
 }

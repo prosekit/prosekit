21c84bdc9a89cfd3eae8bbd9b9fd65b4cdb7c0ca
ocavue <ocavue@gmail.com>
2023-12-03 21:20:50 +0800

    Update useReadonly

--- playground/examples/solid-readonly/use-readonly.ts
+++ playground/examples/solid-readonly/use-readonly.ts
@@ -1,16 +1,13 @@
 import { defineReadonly } from 'prosekit/extensions/readonly'
 import { useExtension } from 'prosekit/solid'
-import { createEffect, createSignal } from 'solid-js'
+import { createSignal } from 'solid-js'
 
 export function useReadonly() {
   const [getReadonly, setReadonly] = createSignal(false)
   const toggleReadonly = () => setReadonly((readonly) => !readonly)
+  const getExtension = () => (getReadonly() ? defineReadonly() : null)
 
-  createEffect(() => {
-    const readonly = getReadonly()
-    const extension = readonly ? defineReadonly() : null
-    useExtension(extension)
-  })
+  useExtension(getExtension)
 
   return { getReadonly, toggleReadonly }
 }

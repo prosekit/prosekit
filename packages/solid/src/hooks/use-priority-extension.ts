f6e5bd273485cb2ff73f1b35176b0f537a1f470f
ocavue <ocavue@gmail.com>
2023-12-03 22:06:47 +0800

    add usePriorityExtension

---
+++ packages/solid/src/hooks/use-priority-extension.ts
@@ -0,0 +1,13 @@
+import { Priority, withPriority, type Extension } from '@prosekit/core'
+import { useExtension } from './use-extension'
+
+export function usePriorityExtension<T extends Extension = Extension>(
+  extension: () => T | null,
+  priority?: Priority | null,
+): void {
+  const extensionWithPriority = () => {
+    const ext = extension()
+    return ext && priority ? withPriority(ext, priority) : ext
+  }
+  return useExtension(extensionWithPriority)
+}

d746d3c0f92e1b753b361ee20a5e8b05d7548a73
ocavue <ocavue@gmail.com>
2023-12-16 13:47:57 +0800

    remove code block from react-toolbar

--- playground/examples/react-toolbar/extension.ts
+++ playground/examples/react-toolbar/extension.ts
@@ -1,25 +1,8 @@
 import { defineBasicExtension } from 'prosekit/basic'
 import { union } from 'prosekit/core'
-import {
-  defineCodeBlock,
-  defineCodeBlockHighlight,
-} from 'prosekit/extensions/code-block'
-import { defineReactNodeView } from 'prosekit/react'
-
-import CodeBlockView from './code-block-view'
-import { parser } from './shikiji'
 
 export function defineExtension() {
-  return union([
-    defineBasicExtension(),
-    defineCodeBlock(),
-    defineCodeBlockHighlight({ parser }),
-    defineReactNodeView({
-      name: 'codeBlock',
-      contentAs: 'code',
-      component: CodeBlockView,
-    }),
-  ])
+  return union([defineBasicExtension()])
 }
 
 export type EditorExtension = ReturnType<typeof defineExtension>

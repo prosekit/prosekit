01728bcb5a06a069bf7c74d1e65549967345d504
ocavue <ocavue@gmail.com>
2023-12-16 13:51:59 +0800

    w

--- playground/examples/react-code-block/code-block-view.tsx
+++ playground/examples/react-code-block/code-block-view.tsx
@@ -1,68 +1,20 @@
+import type { CodeBlockAttrs } from 'prosekit/extensions/code-block'
 import { type ReactNodeViewProps } from 'prosekit/react'
-import { ComboBox } from 'prosekit/react/combo-box'
-import { ComboBoxInput } from 'prosekit/react/combo-box-input'
-import { ComboBoxItem } from 'prosekit/react/combo-box-item'
-import { ComboBoxList } from 'prosekit/react/combo-box-list'
-import { useRef, useState } from 'react'
 
-import { languages } from './shikiji'
+import LanguageSelector from './language-selector'
 
 export default function CodeBlockView(props: ReactNodeViewProps) {
-  const [showComboBox, setShowComboBox] = useState(false)
-
-  const closeComboBox = () => {
-    setShowComboBox(false)
-  }
-  const toggleComboBox = () => {
-    setShowComboBox((value) => !value)
-  }
+  const attrs = props.node.attrs as CodeBlockAttrs
+  const language = attrs.language
 
   const setLanguage = (language: string) => {
     props.setAttrs({ language })
   }
 
-  const buttonRef = useRef<HTMLButtonElement>(null)
-
   return (
     <>
-      <div className="LANGUAGE_WRAPPER">
-        <button
-          className="LANGUAGE_BUTTON"
-          onClick={toggleComboBox}
-          ref={buttonRef}
-          contentEditable={false}
-        >
-          {props.node.attrs.language || 'Plain Text'}
-        </button>
-
-        <ComboBox
-          className="LANGUAGE_COMBO_BOX"
-          active={showComboBox}
-          reference={buttonRef.current ?? undefined}
-          onDismiss={closeComboBox}
-        >
-          <ComboBoxInput
-            placeholder="Search for a langauge..."
-            className="LANGUAGE_COMBO_BOX_INPUT"
-          ></ComboBoxInput>
-          <ComboBoxList className="LANGUAGE_COMBO_BOX_LIST">
-            {languages.map((language) => (
-              <ComboBoxItem
-                key={language}
-                className="LANGUAGE_COMBO_BOX_ITEM"
-                onSelect={() => setLanguage(language)}
-              >
-                {language}
-              </ComboBoxItem>
-            ))}
-          </ComboBoxList>
-        </ComboBox>
-      </div>
-
-      <pre
-        ref={props.contentRef}
-        data-language={props.node.attrs.language}
-      ></pre>
+      <LanguageSelector language={language} setLanguage={setLanguage} />
+      <pre ref={props.contentRef} data-language={language}></pre>
     </>
   )
 }
---
+++ playground/examples/react-code-block/language-selector.tsx
@@ -0,0 +1,61 @@
+import { ComboBox } from 'prosekit/react/combo-box'
+import { ComboBoxInput } from 'prosekit/react/combo-box-input'
+import { ComboBoxItem } from 'prosekit/react/combo-box-item'
+import { ComboBoxList } from 'prosekit/react/combo-box-list'
+import { useState, useRef } from 'react'
+import { languages } from './shikiji'
+
+export default function LanguageSelector({
+  language,
+  setLanguage,
+}: {
+  language?: string
+  setLanguage: (value: string) => void
+}) {
+  const [showComboBox, setShowComboBox] = useState(false)
+
+  const closeComboBox = () => {
+    setShowComboBox(false)
+  }
+  const toggleComboBox = () => {
+    setShowComboBox((value) => !value)
+  }
+
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  return (
+    <div className="LANGUAGE_WRAPPER">
+      <button
+        className="LANGUAGE_BUTTON"
+        onClick={toggleComboBox}
+        ref={buttonRef}
+        contentEditable={false}
+      >
+        {language || 'Plain Text'}
+      </button>
+
+      <ComboBox
+        className="LANGUAGE_COMBO_BOX"
+        active={showComboBox}
+        reference={buttonRef.current ?? undefined}
+        onDismiss={closeComboBox}
+      >
+        <ComboBoxInput
+          placeholder="Search for a langauge..."
+          className="LANGUAGE_COMBO_BOX_INPUT"
+        ></ComboBoxInput>
+        <ComboBoxList className="LANGUAGE_COMBO_BOX_LIST">
+          {languages.map((language) => (
+            <ComboBoxItem
+              key={language}
+              className="LANGUAGE_COMBO_BOX_ITEM"
+              onSelect={() => setLanguage(language)}
+            >
+              {language}
+            </ComboBoxItem>
+          ))}
+        </ComboBoxList>
+      </ComboBox>
+    </div>
+  )
+}

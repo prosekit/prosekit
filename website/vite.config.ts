9de7e457a05b718d90ddd89a8656b38c888ef83f
ocavue <ocavue@gmail.com>
2023-12-28 11:44:27 +0800

    add build log

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,9 +1,20 @@
+import fs from 'node:fs'
 import { fileURLToPath, URL } from 'node:url'
 
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import unocssWacher from 'vite-plugin-unocss-watcher'
+
+const __dirname = fileURLToPath(new URL('./', import.meta.url))
+
+// When deploying to Netlify, sometimes the build fails with 422 Unprocessable
+// Entity error. I suspect it's because I deploy the same content in a short time.
+// As a workaround, a unique file is created for each build.
+fs.writeFileSync(
+  './public/build.log',
+  `Build at ${new Date().toISOString()} ${process.env.GITHUB_SHA}`,
+)
 
 export default defineConfig({
   build: {
@@ -13,7 +24,7 @@ export default defineConfig({
     alias: [
       {
         find: '@',
-        replacement: fileURLToPath(new URL('./', import.meta.url)),
+        replacement: __dirname,
       },
     ],
   },

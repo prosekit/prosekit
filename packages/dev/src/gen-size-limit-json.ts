e538e17d87ff5357fc5f2afd0836d7bbcd7567db
ocavue <ocavue@gmail.com>
2024-01-01 17:31:49 +0800

    chore: ignore peer dependencies for size-limit

--- packages/dev/src/gen-size-limit-json.ts
+++ packages/dev/src/gen-size-limit-json.ts
@@ -1,16 +1,18 @@
 import path from 'node:path'
 
 import type { Package } from '@manypkg/get-packages'
+import { sortedUniq } from 'lodash-es'
 
+import { asyncFrom } from './async-from'
 import { vfs } from './virtual-file-system'
 
 export async function genSizeLimitJson() {
   const pkg = await vfs.getPackageByName('prosekit')
-  const sizeLimitConfig = Array.from(iterateExports(pkg))
+  const sizeLimitConfig = await asyncFrom(iterateExports(pkg))
   await vfs.updateJSON('.size-limit.json', sizeLimitConfig)
 }
 
-function* iterateExports(pkg: Package) {
+async function* iterateExports(pkg: Package) {
   const exports: Record<string, string | Record<string, string>> =
     (pkg.packageJson as any)?.publishConfig?.exports ?? {}
 
@@ -20,11 +22,28 @@ function* iterateExports(pkg: Package) {
       continue
     }
 
+    // The root package is empty
+    if (entryName === '.') {
+      continue
+    }
+
+    const subPackageName = path
+      .join('@prosekit', entryName)
+      .split('/')
+      .slice(0, 2)
+      .join('/')
+
+    const subPackage = await vfs.getPackageByName(subPackageName)
+    const peerDependencies: string[] = sortedUniq([
+      ...Object.keys(subPackage.packageJson.peerDependencies ?? {}),
+    ])
+
     const entryPath = typeof entry === 'string' ? entry : entry.default
 
     yield {
       name: path.normalize(path.join('prosekit', entryName)),
       path: path.normalize(path.join('packages/prosekit', entryPath)),
+      ignore: peerDependencies.length > 0 ? peerDependencies : undefined,
     }
   }
 }

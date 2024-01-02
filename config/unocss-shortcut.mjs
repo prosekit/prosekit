59f28ebd46e0285742dbf4d507a97ad71689c805
ocavue <ocavue@gmail.com>
2024-01-02 19:18:29 +0800

    fix(playground): hide language select outline

--- config/unocss-shortcut.mjs
+++ config/unocss-shortcut.mjs
@@ -53,7 +53,7 @@ export const shortcuts = {
   ),
 
   LANGUAGE_SELECT: clsx(
-    'relative box-border w-auto cursor-pointer select-none appearance-none rounded border-none bg-transparent px-2 py-1 text-xs outline-none transition',
+    'relative box-border w-auto cursor-pointer select-none appearance-none rounded border-none bg-transparent px-2 py-1 text-xs outline-none transition focus:outline-none',
 
     // Only visible when hovering the code block
     'opacity-0 hover:opacity-80 [div[data-node-view-root]:hover_&]:opacity-50 [div[data-node-view-root]:hover_&]:hover:opacity-80',

aaf14584082d5c266d49223b182fc0a583220a2e
ocavue <ocavue@gmail.com>
2024-01-11 01:39:49 +0800

    fix(core): revert union refactor

--- packages/core/src/editor/union.ts
+++ packages/core/src/editor/union.ts
@@ -1,25 +1,12 @@
-import type { BaseExtension } from '../facets/base-extension'
+import { BaseExtension } from '../facets/base-extension'
 import { UnionExtensionImpl } from '../facets/union-extension'
-import type { Extension, SimplifyExtension } from '../types/extension'
+import type { SimplifyExtension, Extension } from '../types/extension'
 
-/**
- * Merge multiple extensions into one. If only one extension is provided, it
- * will be returned directly.
- *
- * @public
- */
 export function union<E extends Extension | Extension[]>(
   extension: E,
 ): SimplifyExtension<E> {
-  if (!Array.isArray(extension)) {
-    return extension as SimplifyExtension<E>
-  }
-
-  if (extension.length === 1) {
-    return extension[0] as SimplifyExtension<E>
-  }
-
+  const array = Array.isArray(extension) ? extension : [extension]
   return new UnionExtensionImpl(
-    extension as BaseExtension[],
+    array as BaseExtension[],
   ) as Extension as SimplifyExtension<E>
 }

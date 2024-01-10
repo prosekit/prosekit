80c323b97909a366abac0587b081157ae92e35a7
ocavue <ocavue@gmail.com>
2024-01-11 01:43:03 +0800

    add comment

--- packages/core/src/editor/union.ts
+++ packages/core/src/editor/union.ts
@@ -2,6 +2,11 @@ import type { BaseExtension } from '../facets/base-extension'
 import { UnionExtensionImpl } from '../facets/union-extension'
 import type { Extension, SimplifyExtension } from '../types/extension'
 
+/**
+ * Merge multiple extensions into one.
+ *
+ * @public
+ */
 export function union<E extends Extension | Extension[]>(
   extension: E,
 ): SimplifyExtension<E> {

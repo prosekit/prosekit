468ba82f41153049b516cb6cf377294615aaa217
ocavue <ocavue@gmail.com>
2024-01-14 15:57:19 +0800

    add vue-code-block-themes

---
+++ playground/examples/vue-code-block-themes/theme-selector.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineCodeBlockShikiji } from 'prosekit/extensions/code-block'
+import { useExtension } from 'prosekit/vue'
+import { bundledThemesInfo, type BundledTheme } from 'shikiji'
+import { computed } from 'vue'
+
+const theme = defineModel({ default: 'github-dark' })
+const extension = computed(() => {
+  return defineCodeBlockShikiji({ theme: theme.value as BundledTheme })
+})
+useExtension(extension)
+</script>
+
+<template>
+  <label>Theme:</label>
+  <select v-model="theme" class="TOGGLE_BUTTON">
+    <option v-for="info of bundledThemesInfo" :key="info.id" :value="info.id">
+      {{ info.id }}
+    </option>
+  </select>
+</template>
---
+++ playground/examples/vue-code-block-themes/toggle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  pressed: Boolean
+  disabled?: Boolean
+  onClick: VoidFunction
+}>()
+</script>
+
+<template>
+  <button
+    :data-state="pressed ? 'on' : 'off'"
+    :disabled="disabled ? true : undefined"
+    @click="onClick"
+    @mousedown.prevent
+    class="TOGGLE_BUTTON"
+  >
+    <slot></slot>
+  </button>
+</template>

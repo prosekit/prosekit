cf0c7c68c84f3ba93677200b5c29940c6bf0ffce
ocavue <ocavue@gmail.com>
2024-01-14 17:52:07 +0800

    Fix example

--- playground/examples/react-code-block/toolbar.tsx
+++ playground/examples/react-code-block/toolbar.tsx
@@ -10,8 +10,8 @@ export default function Toolbar() {
     <div className="TOOLBAR">
       <Toggle
         pressed={editor.nodes.codeBlock.isActive()}
-        disabled={!editor.commands.insertNode.canApply({ type: 'codeBlock' })}
-        onClick={() => editor.commands.insertNode({ type: 'codeBlock' })}
+        disabled={!editor.commands.toggleCodeBlock.canApply()}
+        onClick={() => editor.commands.toggleCodeBlock()}
       >
         <div className="ICON_CODE_BLOCK" />
       </Toggle>

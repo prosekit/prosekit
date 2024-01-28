60971d3271441a5eec3d000374f38e5556be6b4d
ocavue <ocavue@gmail.com>
2024-01-28 19:01:21 +0800

    MaybeAccessor

---
+++ packages/solid/src/utils/to-value.ts
@@ -0,0 +1,17 @@
+import type { MaybeAccessor, MaybeAccessorValue } from '../types'
+
+/**
+ * Accesses the value of a MaybeAccessor
+ *
+ * @example
+ * ```ts
+ * access("foo") // => "foo"
+ * access(() => "foo") // => "foo"
+ * ```
+ */
+export function toValue<T extends MaybeAccessor<any>>(
+  v: T,
+): MaybeAccessorValue<T> {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return typeof v === 'function' && v.length === 0 ? v() : null
+}

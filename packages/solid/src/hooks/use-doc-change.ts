118c27a71b04c6d30779bead6f91a56503fc0934
ocavue <ocavue@gmail.com>
2024-01-30 10:01:22 +0800

    add useDocChange

---
+++ packages/solid/src/hooks/use-doc-change.ts
@@ -0,0 +1,17 @@
+import { defineDocChangeHandler } from '@prosekit/core'
+import type { ProseMirrorNode } from '@prosekit/pm/model'
+
+import { useExtension, type UseExtensionOptions } from './use-extension'
+
+/**
+ * Calls the given handler whenever the editor document changes.
+ *
+ * @public
+ */
+export function useDocChange(
+  handler: (doc: ProseMirrorNode) => void,
+  options?: UseExtensionOptions,
+) {
+  const extension = defineDocChangeHandler((view) => handler(view.state.doc))
+  return useExtension(() => extension, options)
+}

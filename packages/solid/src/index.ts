360d42eb8041e08d16f9672467110251d148d83d
ocavue <ocavue@gmail.com>
2024-01-30 10:02:00 +0800

    export useDocChange

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -2,5 +2,5 @@ export { ProseKit, type ProseKitProps } from './components/prosekit'
 export { useEditor } from './hooks/use-editor'
 export { useExtension, type UseExtensionOptions } from './hooks/use-extension'
 export { useKeymap } from './hooks/use-keymap'
-export { useStateUpdate } from './hooks/use-state-update'
+export { useStateUpdate } from './hooks/use-state-update'; export { useDocChange } from './hooks/use-doc-change'; 
 export type { MaybeAccessor, PropsWithChildren, PropsWithClass } from './types'

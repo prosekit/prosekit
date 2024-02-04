01dfb31c11f90dcf36c93f5f3fe1f3854f830f05
ocavue <ocavue@gmail.com>
2024-02-04 18:26:47 +0800

    bug fix

--- packages/extensions/src/virtual-selection/index.ts
+++ packages/extensions/src/virtual-selection/index.ts
@@ -9,8 +9,8 @@ import { Decoration, DecorationSet } from '@prosekit/pm/view'
 
 /**
  * Shows a virtual selection when the editor is not focused. When the editor is
- * not focused, the selected inline content will be wrapped in a <span> with the
- * class `prosekit-virtual-selection`.
+ * not focused, the selected inline content will be wrapped in a `<span>`
+ * element with the class `prosekit-virtual-selection`.
  *
  * This is useful when you want to move the focus to an element outside the
  * editor, but still want to show the selection.

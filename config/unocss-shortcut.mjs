948d1b4777e32a254505cd69db91d7575108f9e4
ocavue <ocavue@gmail.com>
2024-02-05 20:54:32 +0800

    update style

--- config/unocss-shortcut.mjs
+++ config/unocss-shortcut.mjs
@@ -142,7 +142,7 @@ const staticShortcuts = {
   ),
 
   TOOLBAR: clsx(
-    'z-2 sticky top-0 box-border flex flex-wrap gap-1 p-2 items-center',
+    'z-2 sticky top-0 box-border flex flex-wrap gap-1 p-2 items-center bg-background',
     'border-border border-solid border-l-0 border-r-0 border-t-0 border-b',
   ),
 

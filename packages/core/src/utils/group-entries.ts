1833f90d0410e05038327342481e2b070b322539
ocavue <ocavue@gmail.com>
2024-02-10 19:26:04 +0800

    small refactor

--- packages/core/src/utils/group-entries.ts
+++ packages/core/src/utils/group-entries.ts
@@ -2,8 +2,9 @@ import type { ObjectEntries } from '../types/object-entries'
 
 export function groupEntries<T extends Record<string, any>>(
   entries: ObjectEntries<T>[],
-  map: GroupedEntries<T> = {},
 ): GroupedEntries<T> {
+  const map: GroupedEntries<T> = {}
+
   for (const [key, value] of entries) {
     const values = map[key]
     if (!values) {

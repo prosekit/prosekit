5d8d3d84888f0c1f3f81e947f15527f42914f8f1
ocavue <ocavue@gmail.com>
2024-02-15 15:20:04 +0800

    fix build

--- playground/examples/vue-code-block-themes/theme-selector.vue
+++ playground/examples/vue-code-block-themes/theme-selector.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { defineCodeBlockShiki } from 'prosekit/extensions/code-block'
+import {
+  defineCodeBlockShiki,
+  shikiBundledThemesInfo,
+} from 'prosekit/extensions/code-block'
 import { useExtension } from 'prosekit/vue'
-import { bundledThemesInfo, type BundledTheme } from 'shiki'
 import { computed } from 'vue'
 
 const theme = defineModel({ default: 'github-dark' })
 const extension = computed(() => {
-  return defineCodeBlockShiki({ theme: theme.value as BundledTheme })
+  return defineCodeBlockShiki({ themes: [theme.value] })
 })
 useExtension(extension)
 </script>
@@ -14,7 +16,11 @@ useExtension(extension)
 <template>
   <label>Theme:</label>
   <select v-model="theme" class="TOGGLE_BUTTON">
-    <option v-for="info of bundledThemesInfo" :key="info.id" :value="info.id">
+    <option
+      v-for="info of shikiBundledThemesInfo"
+      :key="info.id"
+      :value="info.id"
+    >
       {{ info.id }}
     </option>
   </select>

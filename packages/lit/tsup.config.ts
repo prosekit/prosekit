adcb3f41f86752ea59949df88cc8b0df05255984
ocavue <ocavue@gmail.com>
2024-02-15 18:35:38 +0800

    chore(deps): remove minify-literals

--- packages/lit/tsup.config.ts
+++ packages/lit/tsup.config.ts
@@ -1,16 +1,4 @@
 import { config } from '@prosekit/dev/config-tsup'
-import { minifyHTMLLiterals } from 'minify-literals'
 import { defineConfig } from 'tsup'
 
-const minifyTemplateLiteralsPlugin = {
-  name: 'minify-template-literals',
-  renderChunk: async (code: string) => {
-    const result = await minifyHTMLLiterals(code)
-    if (result) {
-      return { code: result.code }
-    }
-    return null
-  },
-}
-
-export default defineConfig(config({ plugins: [minifyTemplateLiteralsPlugin] }))
+export default defineConfig(config())

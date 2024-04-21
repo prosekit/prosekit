7fdd68dbb490971dd12346bef7a35e5d9a298f17
ocavue <ocavue@gmail.com>
2024-04-21 22:54:25 +1000

    improve example

--- playground/examples/react-user-menu-dynamic/use-user-query.ts
+++ playground/examples/react-user-menu-dynamic/use-user-query.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-import { users as allUsers } from './user-data'
+import { users as allUsers } from './user-data';
 
 export function useUserQuery(query: string) {
   const [users, setUsers] = useState<{ name: string; id: number }[]>([])
@@ -8,27 +8,23 @@ export function useUserQuery(query: string) {
 
   useEffect(() => {
     setLoading(true)
+    
     const id = setTimeout(() => {
       setLoading(false)
 
-      // Pick some random users
-      const userCount = Math.round(Math.random() * 10)
-      const randomUsers = allUsers
-        .sort(() => Math.random() - 0.5)
-        .slice(0, userCount)
+      query = query.toLowerCase()
 
       setUsers(
-        randomUsers.map((user) => ({
-          name: user.name + ` (${query}'s friend)`,
-          id: user.id,
-        })),
+        allUsers
+          .filter((user) => user.name.toLowerCase().includes(query))
+          .slice(0, 10),
       )
-    }, 1500)
+    }, 1000)
 
     return () => {
       clearTimeout(id)
     }
   }, [query])
 
-  return { users, loading }
+  return { loading, users: loading ? [] : users }
 }

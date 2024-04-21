c16e29ff5e923106f809bad9a8c58ab4320f15bc
ocavue <ocavue@gmail.com>
2024-04-21 17:22:47 +1000

    update solid

--- packages/solid/src/components/create-component.ts
+++ packages/solid/src/components/create-component.ts
@@ -1,6 +1,8 @@
 import type { Component, JSX } from 'solid-js'
 import h from 'solid-js/h'
 
+import { useEditorContext } from '../contexts/editor-context'
+
 export function createComponent<
   Props extends object,
   CustomElement extends HTMLElement,
@@ -10,12 +12,20 @@ export function createComponent<
 ): Component<Partial<Props> & JSX.HTMLAttributes<CustomElement>> {
   const propertyNames = Object.keys(defaultProps)
 
+  const needsEditor = propertyNames.includes('editor')
+
   const Component = (props: Record<string, unknown>) => {
     const p: Record<string, () => unknown> = {}
 
     for (const key of Object.keys(props)) {
       p[propertyNames.includes(key) ? 'prop:' + key : key] = (): unknown =>
         props[key] as unknown
+    }
+
+    const editor = useEditorContext()
+
+    if (needsEditor && editor) {
+      p['prop:editor'] = () => props['editor'] || editor
     }
 
     return h(tagName, p)

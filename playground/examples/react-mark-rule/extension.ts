e1b38783ce5b58a4f613dfa4d32ffdc338197847
ocavue <ocavue@gmail.com>
2024-04-28 07:01:38 +1000

    add test

--- playground/examples/react-mark-rule/extension.ts
+++ playground/examples/react-mark-rule/extension.ts
@@ -1,11 +1,53 @@
-import { defineBasicExtension } from 'prosekit/basic'
-import { defineMarkSpec, union } from 'prosekit/core'
+import {
+  defineBaseCommands,
+  defineBaseKeymap,
+  defineDoc,
+  defineHistory,
+  defineMarkSpec,
+  defineParagraph,
+  defineText,
+  union,
+} from 'prosekit/core'
+import { defineBlockquote } from 'prosekit/extensions/blockquote'
+import { defineBold } from 'prosekit/extensions/bold'
+import { defineCode } from 'prosekit/extensions/code'
+import { defineDropCursor } from 'prosekit/extensions/drop-cursor'
+import { defineHeading } from 'prosekit/extensions/heading'
+import { defineImage } from 'prosekit/extensions/image'
+import { defineItalic } from 'prosekit/extensions/italic'
+import { defineLinkMarkRule, defineLinkSpec } from 'prosekit/extensions/link'
+import { defineList } from 'prosekit/extensions/list'
 import { defineMarkRule } from 'prosekit/extensions/mark-rule'
 import { definePlaceholder } from 'prosekit/extensions/placeholder'
+import { defineStrike } from 'prosekit/extensions/strike'
+import { defineTable } from 'prosekit/extensions/table'
+import { defineUnderline } from 'prosekit/extensions/underline'
+import { defineVirtualSelection } from 'prosekit/extensions/virtual-selection'
 
 export function defineExtension() {
   return union([
-    defineBasicExtension(),
+    defineDoc(),
+    defineText(),
+    defineHeading(),
+    defineHistory(),
+    defineList(),
+    defineBlockquote(),
+    defineBaseKeymap(),
+    defineBaseCommands(),
+    defineItalic(),
+    defineBold(),
+    defineUnderline(),
+    defineStrike(),
+    defineCode(),
+
+    defineImage(),
+    defineParagraph(),
+    defineDropCursor(),
+    defineVirtualSelection(),
+    defineTable(),
+
+    defineLinkSpec(),
+    defineLinkMarkRule(),
 
     definePlaceholder({
       placeholder: 'Try typing #123',
---
+++ playground/tests/mark-rule.test.ts
@@ -0,0 +1,90 @@
+import { expect, test } from '@playwright/test'
+
+import { getExamples, waitForEditor } from './helper'
+
+function run(example: string) {
+  test('hashtag', async ({ page }) => {
+    await page.goto(example)
+    const editor = await waitForEditor(page)
+
+    const getLinkContent = async () => {
+      const locators = await editor.locator('a').all()
+      return await Promise.all(
+        locators.map(async (locator) => {
+          return await locator.innerText()
+        }),
+      )
+    }
+
+    await editor.pressSequentially('Fix ')
+    expect(await getLinkContent()).toEqual([])
+
+    await editor.pressSequentially('#')
+    expect(await getLinkContent()).toEqual([])
+
+    await editor.pressSequentially('1')
+    expect(await getLinkContent()).toEqual(['#1'])
+
+    await editor.pressSequentially('2')
+    expect(await getLinkContent()).toEqual(['#12'])
+
+    await editor.pressSequentially(' and ')
+    expect(await getLinkContent()).toEqual(['#12'])
+
+    await editor.pressSequentially('#')
+    expect(await getLinkContent()).toEqual(['#12'])
+
+    await editor.pressSequentially('3')
+    expect(await getLinkContent()).toEqual(['#12', '#3'])
+
+    await editor.pressSequentially('4')
+    expect(await getLinkContent()).toEqual(['#12', '#34'])
+
+    await editor.pressSequentially('.')
+    expect(await getLinkContent()).toEqual(['#12', '#34'])
+
+    await editor.press('Backspace')
+    expect(await getLinkContent()).toEqual(['#12', '#34'])
+
+    await editor.press('Backspace')
+    expect(await getLinkContent()).toEqual(['#12', '#3'])
+
+    await editor.pressSequentially('5')
+    expect(await getLinkContent()).toEqual(['#12', '#35'])
+
+    expect(await editor.innerText()).toEqual('Fix #12 and #35')
+  })
+
+  test('link', async ({ page }) => {
+    await page.goto(example)
+    const editor = await waitForEditor(page)
+
+    const getLinkContent = async () => {
+      const locators = await editor.locator('a').all()
+      return await Promise.all(
+        locators.map(async (locator) => {
+          return await locator.innerText()
+        }),
+      )
+    }
+
+    await editor.pressSequentially('Hello ')
+    expect(await getLinkContent()).toEqual([])
+
+    await editor.pressSequentially('www.go')
+    expect(await getLinkContent()).toEqual([])
+
+    await editor.pressSequentially('o')
+    expect(await getLinkContent()).toEqual(['www.goo'])
+
+    await editor.pressSequentially('g')
+    expect(await getLinkContent()).toEqual(['www.goog'])
+
+    await editor.pressSequentially('l')
+    expect(await getLinkContent()).toEqual([])
+  })
+}
+
+for (const example of getExamples('mark-rule')) {
+  test.describe(example, () => run(example))
+}

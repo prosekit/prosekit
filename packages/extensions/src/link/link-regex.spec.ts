f3651bc6dd68dd66265d9b1e883d5ed33bf2288e
ocavue <ocavue@gmail.com>
2024-04-28 17:06:17 +1000

    fix

--- packages/extensions/src/link/link-regex.spec.ts
+++ packages/extensions/src/link/link-regex.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, describe } from 'vitest'
+import { test, it, expect, describe } from 'vitest'
 
 import { LINK_MARK_RE } from './link-regex'
 
@@ -8,62 +8,63 @@ describe('LINK_MARK_RE', () => {
     return LINK_MARK_RE.exec(str)?.[0] ?? null
   }
 
-  expect(run('Not a link')).toEqual(null)
+  const cases = [
+    ['Not a link', null],
 
-  expect(run('example.com')).toEqual('example.com')
-  expect(run('https://example.com')).toEqual('https://example.com')
-  expect(run('Foo https://example.com Bar')).toEqual('https://example.com')
-  expect(run('https://example.com/')).toEqual('https://example.com/')
-  expect(run('https://example.com/?query=1')).toEqual(
-    'https://example.com/?query=1',
-  )
-  expect(run('https://example.com/#fragment')).toEqual(
-    'https://example.com/#fragment',
-  )
-  expect(run('https://example.com/path?query=1#fragment')).toEqual(
-    'https://example.com/path?query=1#fragment',
-  )
-  expect(run('https://example.com/path?query=1#fragment')).toEqual(
-    'https://example.com/path?query=1#fragment',
-  )
-  expect(run('Foo https://example.com/path?query=1#fragment Bar')).toEqual(
-    'https://example.com/path?query=1#fragment',
-  )
+    ['example.com', 'example.com'],
+    ['https://example.com', 'https://example.com'],
+    ['Foo https://example.com Bar', 'https://example.com'],
+    ['https://example.com/', 'https://example.com/'],
+    ['https://example.com/?query=1', 'https://example.com/?query=1'],
+    ['https://example.com/#fragment', 'https://example.com/#fragment'],
+    [
+      'https://example.com/path?query=1#fragment',
+      'https://example.com/path?query=1#fragment',
+    ],
+    [
+      'https://example.com/path?query=1#fragment',
+      'https://example.com/path?query=1#fragment',
+    ],
+    [
+      'Foo https://example.com/path?query=1#fragment Bar',
+      'https://example.com/path?query=1#fragment',
+    ],
 
-  // Ignore the period at the end
-  expect(run('https://example.com.')).toEqual('https://example.com')
-  expect(run('https://example.com.uk')).toEqual('https://example.com.uk')
+    // Ignore the period at the end
+    ['https://example.com.', 'https://example.com'],
+    ['https://example.com.uk', 'https://example.com.uk'],
 
-  // .goog is a TLD
-  expect(run('Foo www.goog')).toEqual('www.goog')
-  // .googl is not a TLD
-  expect(run('Foo www.googl')).toEqual(null)
-  // .google is a TLD
-  expect(run('Foo www.google')).toEqual('www.google')
+    // .goog is a TLD
+    ['Foo www.goog', 'www.goog'],
+    // .googl is not a TLD
+    ['Foo www.googl', null],
+    // .google is a TLD
+    ['Foo www.google', 'www.google'],
 
-  // Don't match ? when there is nothing after it
-  expect(run('www.example.com?')).toEqual('www.example.com')
-  expect(run('www.example.com/?')).toEqual('www.example.com/')
-  expect(run('www.example.com/?=')).toEqual('www.example.com/?=')
+    // Don't match ? when there is nothing after it
+    ['www.example.com?', 'www.example.com'],
+    ['www.example.com/?', 'www.example.com/'],
+    ['www.example.com/?=', 'www.example.com/?='],
 
-  // Don't match . when there is nothing after it
-  expect(run('www.example.com.')).toEqual('www.example.com')
-  expect(run('www.example.com/.')).toEqual('www.example.com/')
-  expect(run('www.example.com/.gitignore')).toEqual(
-    'www.example.com/.gitignore',
-  )
+    // Don't match . when there is nothing after it
+    ['www.example.com.', 'www.example.com'],
+    ['www.example.com/.', 'www.example.com/'],
+    ['www.example.com/.gitignore', 'www.example.com/.gitignore'],
 
-  // with ( and )
-  expect(run('https://en.wikipedia.org/wiki/Specials_(Unicode_block)')).toEqual(
-    'https://en.wikipedia.org/wiki/Specials_(Unicode_block)',
-  )
+    // with ( and )
+    [
+      'https://en.wikipedia.org/wiki/Specials_(Unicode_block)',
+      'https://en.wikipedia.org/wiki/Specials_(Unicode_block)',
+    ],
 
-  // with '
-  expect(
-    run(
+    // with '
+    [
       `https://en.wikipedia.org/wiki/The_Power_of_the_Powerless#Havel's_greengrocer`,
-    ),
-  ).toEqual(
-    `https://en.wikipedia.org/wiki/The_Power_of_the_Powerless#Havel's_greengrocer`,
-  )
+      `https://en.wikipedia.org/wiki/The_Power_of_the_Powerless#Havel's_greengrocer`,
+    ],
+  ] as const
+
+  it.each(cases)('should handle %s', (str, expected) => {
+    expect(run(str)).toBe(expected)
+  })
 })

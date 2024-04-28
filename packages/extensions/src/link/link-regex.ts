d093c8c6ff48d068bffc6bb5e5f5842d17613e85
ocavue <ocavue@gmail.com>
2024-04-28 17:26:15 +1000

    fix

--- packages/extensions/src/link/link-regex.ts
+++ packages/extensions/src/link/link-regex.ts
@@ -70,7 +70,7 @@ const LINK_ENTER_PATTERN =
 const LINK_INPUT_PATTERN =
   LINK_RE_BASE_PATTERN +   STOP_CHAR_PATTERN + '?' + '\\s$'
 const LINK_MARK_PATTERN =
-  LINK_RE_BASE_PATTERN + '(?=' + STOP_CHAR_PATTERN + '|\\s' + ')|$)'
+  LINK_RE_BASE_PATTERN + '(?=' + STOP_CHAR_PATTERN + '|\\s|$)'
 
 export const LINK_ENTER_RE = new RegExp(LINK_ENTER_PATTERN, 'gi')
 export const LINK_INPUT_RE = new RegExp(LINK_INPUT_PATTERN, 'gi')

2b5404a3f3cef9fea02bf5273111eef8806aa21f
ocavue <ocavue@gmail.com>
2024-04-28 16:49:29 +1000

    add

---
+++ packages/extensions/src/link/link-regex.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from 'vitest'
+
+import { LINK_MARK_RE } from './link-regex'
+
+test('LINK_MARK_RE', () => {
+  const run = (str: string) => {
+    LINK_MARK_RE.lastIndex = 0
+    return LINK_MARK_RE.exec(str)?.[0] ?? null
+  }
+
+  expect(run('Not a link')).toEqual(null)
+
+  expect(run('example.com')).toEqual('example.com')
+  expect(run('https://example.com')).toEqual('https://example.com')
+  expect(run('Foo https://example.com Bar')).toEqual('https://example.com')
+  expect(run('https://example.com/')).toEqual('https://example.com/')
+  expect(run('https://example.com/?query=1')).toEqual(
+    'https://example.com/?query=1',
+  )
+  expect(run('https://example.com/#fragment')).toEqual(
+    'https://example.com/#fragment',
+  )
+  expect(run('https://example.com/path?query=1#fragment')).toEqual(
+    'https://example.com/path?query=1#fragment',
+  )
+  expect(run('https://example.com/path?query=1#fragment')).toEqual(
+    'https://example.com/path?query=1#fragment',
+  )
+})

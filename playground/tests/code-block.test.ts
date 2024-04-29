a6e6c5b5e58f3f3a65a5428c6390a83cf05a5392
ocavue <ocavue@gmail.com>
2024-04-29 15:40:01 +1000

    test: refactor e2e test structure

--- playground/tests/code-block.test.ts
+++ playground/tests/code-block.test.ts
@@ -2,56 +2,58 @@ import { expect, test, type Page } from '@playwright/test'
 
 import { getExamples, isApple, locateEditor } from './helper'
 
-for (const example of getExamples('code-block')) {
-  test.describe(example, () => {
-    test('input rule', async ({ page }) => {
-      await page.goto(example)
-      const editor = locateEditor(page)
-      const pre = editor.locator('pre')
+function run(example: string) {
+  test('input rule', async ({ page }) => {
+    await page.goto(example)
+    const editor = locateEditor(page)
+    const pre = editor.locator('pre')
 
-      // Type triple backticks and press space to create a code block
-      await clear(page)
-      await editor.pressSequentially('```')
-      await expect(editor).toHaveText('```')
-      await expect(pre).not.toBeVisible()
-      await editor.press('Space')
-      await expectAnyPre(page)
-      await expect(editor).not.toHaveText('```')
+    // Type triple backticks and press space to create a code block
+    await clear(page)
+    await editor.pressSequentially('```')
+    await expect(editor).toHaveText('```')
+    await expect(pre).not.toBeVisible()
+    await editor.press('Space')
+    await expectAnyPre(page)
+    await expect(editor).not.toHaveText('```')
 
-      // Type triple backticks followed by a language and press space to create a code block
-      await clear(page)
-      await editor.pressSequentially('```javascript')
-      await expect(editor).toHaveText('```javascript')
-      await expect(pre).not.toBeVisible()
-      await editor.press('Space')
-      await expectJavaScriptPre(page)
-      await expect(editor).not.toHaveText('```')
-    })
-
-    test('enter rule', async ({ page }) => {
-      await page.goto(example)
-      const editor = locateEditor(page)
-      const pre = editor.locator('pre')
-
-      // Type triple backticks and press enter to create a code block
-      await clear(page)
-      await editor.pressSequentially('```')
-      await expect(editor).toHaveText('```')
-      await expect(pre).not.toBeVisible()
-      await editor.press('Enter')
-      await expectAnyPre(page)
-      await expect(editor).not.toHaveText('```')
-
-      // Type triple backticks followed by a language and press enter to create a code block
-      await clear(page)
-      await editor.pressSequentially('```javascript')
-      await expect(editor).toHaveText('```javascript')
-      await expect(pre).not.toBeVisible()
-      await editor.press('Enter')
-      await expectJavaScriptPre(page)
-      await expect(editor).not.toHaveText('```')
-    })
+    // Type triple backticks followed by a language and press space to create a code block
+    await clear(page)
+    await editor.pressSequentially('```javascript')
+    await expect(editor).toHaveText('```javascript')
+    await expect(pre).not.toBeVisible()
+    await editor.press('Space')
+    await expectJavaScriptPre(page)
+    await expect(editor).not.toHaveText('```')
   })
+
+  test('enter rule', async ({ page }) => {
+    await page.goto(example)
+    const editor = locateEditor(page)
+    const pre = editor.locator('pre')
+
+    // Type triple backticks and press enter to create a code block
+    await clear(page)
+    await editor.pressSequentially('```')
+    await expect(editor).toHaveText('```')
+    await expect(pre).not.toBeVisible()
+    await editor.press('Enter')
+    await expectAnyPre(page)
+    await expect(editor).not.toHaveText('```')
+
+    // Type triple backticks followed by a language and press enter to create a code block
+    await clear(page)
+    await editor.pressSequentially('```javascript')
+    await expect(editor).toHaveText('```javascript')
+    await expect(pre).not.toBeVisible()
+    await editor.press('Enter')
+    await expectJavaScriptPre(page)
+    await expect(editor).not.toHaveText('```')
+  })
+}
+
+for (const example of getExamples('code-block')) {
+  test.describe(example, () => run(example))
 }
 
 async function expectNotPre(page: Page) {

bbc851d0c31bfb2db869216478a4cf71e14040e3
ocavue <ocavue@gmail.com>
2024-05-03 05:19:56 +1000

    docs: add vue-user-menu-dynamic

---
+++ playground/examples/vue-user-menu-dynamic/user-menu.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {
+  AutocompleteEmpty,
+  AutocompleteItem,
+  AutocompleteList,
+  AutocompletePopover,
+} from 'prosekit/vue/autocomplete'
+
+import { useEditor } from 'prosekit/vue'
+import type { EditorExtension } from './extension'
+import { users } from './user-data'
+
+const editor = useEditor<EditorExtension>()
+
+const handleUserInsert = (id: number, username: string) => {
+  editor.value.commands.insertMention({
+    id: id.toString(),
+    value: '@' + username,
+    kind: 'user',
+  })
+  editor.value.commands.insertText({ text: ' ' })
+}
+</script>
+
+<template>
+  <AutocompletePopover :regex="/@\w*$/" class="AUTOCOMPLETE_MENU">
+    <AutocompleteList>
+      <AutocompleteEmpty class="AUTOCOMPLETE_MENU_ITEM">
+        No results
+      </AutocompleteEmpty>
+      <AutocompleteItem
+        v-for="user in users"
+        :key="user.id"
+        class="AUTOCOMPLETE_MENU_ITEM"
+        @select="handleUserInsert(user.id, user.name)"
+      >
+        {{ user.name }}
+      </AutocompleteItem>
+    </AutocompleteList>
+  </AutocompletePopover>
+</template>

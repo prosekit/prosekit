3b63c4cafe36b8c5c3063c75fc63e72eb2ecf33c
ocavue <ocavue@gmail.com>
2024-05-03 05:37:18 +1000

    add imple

---
+++ playground/examples/vue-user-menu-dynamic/use-user-query.ts
@@ -0,0 +1,25 @@
+import { ref, watchEffect, type Ref } from 'vue';
+
+import { users as allUsers } from './user-data';
+
+export function useUserQuery(query: Ref<string>) {
+  const users = ref<{ id: number; name: string }[]>([])
+  const loading = ref(false)
+
+  watchEffect((onInvalidate) => {
+    loading.value = true
+    const timeoutId = setTimeout(() => {
+      loading.value = false
+      const searchQuery = query.value.toLowerCase()
+      users.value = allUsers
+        .filter((user) => user.name.toLowerCase().includes(searchQuery))
+        .slice(0, 10)
+    }, 1000)
+
+    onInvalidate(() => {
+      clearTimeout(timeoutId)
+    })
+  })
+
+  return { loading, users }
+}

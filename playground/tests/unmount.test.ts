d6a758c451b01036973764c11c183d1604ec501f
ocavue <ocavue@gmail.com>
2024-05-19 16:51:34 +1000

    bug fix

--- playground/tests/unmount.test.ts
+++ playground/tests/unmount.test.ts
@@ -15,35 +15,38 @@ testStory('unmount', () => {
     await waitForEditor(page)
 
     await expect(editor).toHaveCount(1)
+    await expect(placeholder('Editor No.1 of 1')).toBeVisible()
 
     await buttonAdd.click()
     await expect(editor).toHaveCount(2)
+    await expect(placeholder('Editor No.1 of 2')).toBeVisible()
+    await expect(placeholder('Editor No.2 of 2')).toBeVisible()
 
     await buttonAdd.click()
     await expect(editor).toHaveCount(3)
-
-    await expect(placeholder("Editor No.1 of 3")).toBeVisible()
-    await expect(placeholder("Editor No.2 of 3")).toBeVisible()
-    await expect(placeholder("Editor No.3 of 3")).toBeVisible()
+    await expect(placeholder('Editor No.1 of 3')).toBeVisible()
+    await expect(placeholder('Editor No.2 of 3')).toBeVisible()
+    await expect(placeholder('Editor No.3 of 3')).toBeVisible()
 
     await buttonRemove(1).click()
     await expect(editor).toHaveCount(2)
+    await expect(placeholder('Editor No.2 of 2')).toBeVisible()
+    await expect(placeholder('Editor No.3 of 2')).toBeVisible()
 
     await buttonRemove(3).click()
     await expect(editor).toHaveCount(1)
-
-    await expect(placeholder("Editor No.2 of 1")).toBeVisible()
+    await expect(placeholder('Editor No.2 of 1')).toBeVisible()
 
     await buttonRemove(2).click()
     await expect(editor).toHaveCount(0)
 
     await buttonAdd.click()
     await expect(editor).toHaveCount(1)
-    await expect(placeholder("Editor No.4 of 1")).toBeVisible()
+    await expect(placeholder('Editor No.4 of 1')).toBeVisible()
 
     await buttonAdd.click()
     await expect(editor).toHaveCount(2)
-    await expect(placeholder("Editor No.4 of 2")).toBeVisible()
-    await expect(placeholder("Editor No.5 of 2")).toBeVisible()
+    await expect(placeholder('Editor No.4 of 2')).toBeVisible()
+    await expect(placeholder('Editor No.5 of 2')).toBeVisible()
   })
 })

febfe9cc8931ccb6cba01e251e7e3850364d27e2
ocavue <ocavue@gmail.com>
2024-05-19 14:07:34 +1000

    docs: add placeholder text for example svelte-emoji-rules

--- playground/examples/svelte-emoji-rules/emoji.ts
+++ playground/examples/svelte-emoji-rules/emoji.ts
@@ -1,4 +1,7 @@
+import { union } from 'prosekit/core'
 import { defineEnterRule } from 'prosekit/extensions/enter-rule'
+import { defineInputRule } from 'prosekit/extensions/input-rule'
+import { InputRule } from 'prosekit/pm/inputrules'
 
 /**
  * Converts the text before the text cursor into an emoji when pressing `Enter`.
@@ -12,4 +15,14 @@ export function defineEmojiEnterRule() {
       return state.tr.replaceWith(from, to, state.schema.text(emoji))
     },
   })
+}
+
+/**
+ * Converts the text before the text cursor into an emoji when pressing `Space`.
+ */
+export function defineEmojiSpaceRule() {
+  return union([
+    defineInputRule(new InputRule(/:apple: $/, '🍎')),
+    defineInputRule(new InputRule(/:banana: $/, '🍌')),
+  ])
 }

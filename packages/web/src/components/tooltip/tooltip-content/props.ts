feee6561fb99d517892457b0ff1e0b9bef6829a2
ocavue <ocavue@gmail.com>
2024-05-28 21:36:50 +1000

    fix(web): tweak popover position

--- packages/web/src/components/tooltip/tooltip-content/props.ts
+++ packages/web/src/components/tooltip/tooltip-content/props.ts
@@ -1,2 +1,15 @@
+import type { TooltipContentProps } from '@aria-ui/tooltip'
+import { defaultTooltipContentProps as defaultProps } from '@aria-ui/tooltip'
+
 export type { TooltipContentProps } from '@aria-ui/tooltip'
-export { defaultTooltipContentProps } from '@aria-ui/tooltip'
+
+export const defaultTooltipContentProps = {
+  ...defaultProps,
+  shift: true,
+  flip: true,
+  offset: 6,
+  overflowPadding: 4,
+  // Don't need boundary when hoist is true.
+  hoist: true,
+  boundary: [],
+} satisfies TooltipContentProps

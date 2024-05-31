0b94c37c9968e137faeeca235f4ca4f6c4e8ee61
ocavue <ocavue@gmail.com>
2024-05-31 11:48:56 +1000

    update test

--- packages/themes/src/replace.spec.ts
+++ packages/themes/src/replace.spec.ts
@@ -17,7 +17,7 @@ describe('replaceThemes', () => {
     expect(replaceThemes(code)).toMatchInlineSnapshot(`
       "
       const App = () => (
-        <div class='ProseMirror box-border min-h-full px-[max(40px,_calc(50%-330px))] py-[32px] outline-none outline-0 [&_span[data-mention="user"]]:text-blue-500 [&_span[data-mention="tag"]]:text-violet-500 [&_pre]:text-white [&_pre]:bg-zinc-800'>
+        <div class='ProseMirror box-border min-h-full px-[max(40px,_calc(50%-330px))] py-[24px] outline-none outline-0 [&_span[data-mention="user"]]:text-blue-500 [&_span[data-mention="tag"]]:text-violet-500 [&_pre]:text-white [&_pre]:bg-zinc-800'>
           <div class='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white dark:ring-offset-neutral-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 dark:focus-visible:ring-zinc-300 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-zinc-900 dark:bg-zinc-50 text-zinc-50 dark:text-zinc-900 hover:bg-zinc-900/90 dark:hover:bg-zinc-50/90 h-9 px-3'></div>
         </div>
       );"

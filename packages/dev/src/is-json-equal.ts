efb260e986e6bb502c397253ed14409281e8364b
ocavue <ocavue@gmail.com>
2024-06-05 01:14:33 +1000

    chore: dismiss structuredClone ESLint warning

--- packages/dev/src/is-json-equal.ts
+++ packages/dev/src/is-json-equal.ts
@@ -4,6 +4,10 @@ export function isJsonEqual<T>(a: T, b: T): boolean {
   return isEqual(normalizeJson(a), normalizeJson(b))
 }
 
+/**
+ * Turns an object into a plain JSON object.
+ */
 function normalizeJson<T>(json: T): T {
-  return JSON.parse(JSON.stringify(json)) as T
+  const str = JSON.stringify(json)
+  return JSON.parse(str) as T
 }

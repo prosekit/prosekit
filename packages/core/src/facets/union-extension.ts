3f5fd25ed63c764f8741d13b5d150fab646b1e7b
ocavue <ocavue@gmail.com>
2024-06-10 16:47:38 +1000

    don't hard code rootFacet

--- packages/core/src/facets/union-extension.ts
+++ packages/core/src/facets/union-extension.ts
@@ -1,9 +1,9 @@
 import type { Extension, ExtensionTyping } from '../types/extension'
 import { Priority } from '../types/priority'
+import { assert } from '../utils/assert'
 
 import { BaseExtension } from './base-extension'
 import { FacetNode, unionFacetNode } from './facet-node'
-import { rootFacet } from './root'
 
 export class UnionExtensionImpl<T extends ExtensionTyping = ExtensionTyping>
   extends BaseExtension<T>
@@ -27,16 +27,12 @@ export class UnionExtensionImpl<T extends ExtensionTyping = ExtensionTyping>
 
     const children: FacetNode[] = extensions.map((ext) => ext.getTree(pri))
 
-    if (children.length === 0) {
-      return new FacetNode(rootFacet)
-    } else if (children.length === 1) {
-      return children[0]
-    } else {
-      let node = children[0]
-      for (let i = 1; i < children.length; i++) {
-        node = unionFacetNode(node, children[i])
-      }
-      return node
+    assert(children.length > 0)
+
+    let node = children[0]
+    for (let i = 1; i < children.length; i++) {
+      node = unionFacetNode(node, children[i])
     }
+    return node
   }
 }

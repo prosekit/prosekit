a0780ea563e25b8610d279c62195754a5fa45a5f
Mirone <Saul-Mirone@outlook.com>
2024-06-10 16:20:19 +0800

    chore: resolve comments

--- packages/core/src/commands/remove-node.ts
+++ packages/core/src/commands/remove-node.ts
@@ -1,7 +1,8 @@
-import type { NodeType, ResolvedPos } from '@prosekit/pm/model'
+import type { NodeType } from '@prosekit/pm/model'
 import type { Command } from '@prosekit/pm/state'
 
 import type { CommandCreator } from '../types/command'
+import { findParentNode } from '../utils/find-parent-node'
 import { getNodeType } from '../utils/get-node-type'
 
 /**
@@ -18,38 +19,16 @@ export function removeNode(options: {
   /**
    * The document position to start searching node. By default it will be the anchor position of current selection.
    */
-  position?: number
+  pos?: number
 }): Command {
   return (state, dispatch) => {
     const nodeType = getNodeType(state.schema, options.type)
     const $pos =
-      typeof options.position === 'number'
-        ? state.doc.resolve(options.position)
+      typeof options.pos === 'number'
+        ? state.doc.resolve(options.pos)
         : state.selection.$anchor
 
-    const getNodePos = (
-      $pos: ResolvedPos,
-    ): { from: number | null; to: number | null } => {
-      for (let depth = $pos.depth; depth > 0; depth -= 1) {
-        const node = $pos.node(depth)
-
-        if (node.type === nodeType) {
-          const from = $pos.before(depth)
-          const to = $pos.after(depth)
-          return {
-            from,
-            to,
-          }
-        }
-      }
-
-      return {
-        from: null,
-        to: null,
-      }
-    }
-
-    const { from, to } = getNodePos($pos)
+    const { from, to } = findParentNode(nodeType, $pos)
     if (!from || !to || from > to) {
       return false
     }
---
+++ packages/core/src/utils/find-parent-node.ts
@@ -0,0 +1,24 @@
+import type { NodeType, ResolvedPos } from '@prosekit/pm/model'
+
+export function findParentNode (
+  nodeType: NodeType,
+  $pos: ResolvedPos,
+): { from: number | null; to: number | null } {
+  for (let depth = $pos.depth; depth > 0; depth -= 1) {
+    const node = $pos.node(depth)
+
+    if (node.type === nodeType) {
+      const from = $pos.before(depth)
+      const to = $pos.after(depth)
+      return {
+        from,
+        to,
+      }
+    }
+  }
+
+  return {
+    from: null,
+    to: null,
+  }
+}

b75e5ca52e72a0db3649d18489ee80254f199cfa
ocavue <ocavue@gmail.com>
2024-06-22 21:50:25 +1000

    fix

--- packages/core/src/facets/facet-node.ts
+++ packages/core/src/facets/facet-node.ts
@@ -1,6 +1,7 @@
 import { Priority } from '../types/priority'
 import { arraySubstract, uniqPush } from '../utils/array'
 import { assert } from '../utils/assert'
+import { isNotNull } from '../utils/is-not-null'
 
 import type { Facet } from './facet'
 import type { FacetReducer, Tuple5 } from './facet-types'
@@ -139,7 +140,7 @@ export class FacetNode<I = any, O = any> {
     if (this.facet.singleton) {
       const reducer = (this.reducers[Priority.default] ||= this.facet.reducer)
       // @ts-expect-error: TS 5.5 will fix it.
-      const input: I[] = inputs.filter(Boolean).flat()
+      const input: I[] = inputs.filter(isNotNull).flat()
       output[Priority.default] = reducer(input)
     } else {
       for (let pri = 0; pri < 5; pri++) {

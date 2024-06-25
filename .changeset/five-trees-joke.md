da8322f979253df18b4d9d555d64b2d58bbfd5a3
ocavue <ocavue@users.noreply.github.com>
2024-06-25 20:59:47 +1000

    fix(vue): suppress context warning (#588)

---
+++ .changeset/five-trees-joke.md
@@ -0,0 +1,6 @@
+---
+'prosekit': patch
+'@prosekit/vue': patch
+---
+
+Fix an issue where `useExtension` from `prosekit/vue` will print unnecessary warnings when the editor context is not available.
---
+++ packages/vue/src/hooks/use-extension.spec.ts
@@ -0,0 +1,80 @@
+import {
+  Editor,
+  createEditor,
+  defineBaseCommands,
+  defineDoc,
+  defineParagraph,
+  definePlugin,
+  defineText,
+  union,
+} from '@prosekit/core'
+import { Plugin, PluginKey } from '@prosekit/pm/state'
+import { mount } from '@vue/test-utils'
+import { describe, expect, it, vi } from 'vitest'
+import { defineComponent } from 'vue'
+
+import { useExtension } from './use-extension'
+
+describe('useExtension', () => {
+  it('should register and dispose an extension', () => {
+    const editor = createEditor({
+      extension: union([defineDoc(), defineParagraph(), defineText()]),
+    }) as Editor<ReturnType<typeof defineBaseCommands>>
+
+    editor.mount(document.createElement('div'))
+
+    const pluginKey = new PluginKey('test-plugin-key')
+    const extension = definePlugin(new Plugin({ key: pluginKey }))
+    const hasPlugin = (): boolean => {
+      const plugins = editor.state.plugins
+      return plugins.some((plugin) => plugin.spec.key === pluginKey)
+    }
+    const usePlugin = () => useExtension(extension, { editor })
+
+    const TestComponent = defineComponent({
+      setup: () => {
+        usePlugin()
+      },
+      render: () => null,
+    })
+
+    expect(hasPlugin()).toBe(false)
+    const wrapper = mount(TestComponent)
+    expect(hasPlugin()).toBe(true)
+    wrapper.unmount()
+    expect(hasPlugin()).toBe(false)
+  })
+
+  it('should not print warning if the editor context is not available', () => {
+    const editor = createEditor({
+      extension: union([defineDoc(), defineParagraph(), defineText()]),
+    }) as Editor<ReturnType<typeof defineBaseCommands>>
+    editor.mount(document.createElement('div'))
+
+    const pluginKey = new PluginKey('test-plugin-key')
+    const extension = definePlugin(new Plugin({ key: pluginKey }))
+
+    const TestComponent = defineComponent({
+      setup: () => {
+        useExtension(extension, { editor })
+      },
+      render: () => null,
+    })
+
+    const messages: string[] = []
+    const warnHandler = vi.fn((message: string) => {
+      messages.push(message)
+    })
+
+    const wrapper = mount(TestComponent, {
+      global: {
+        config: {
+          warnHandler,
+        },
+      },
+    })
+    wrapper.unmount()
+
+    expect(messages).toEqual([])
+  })
+})

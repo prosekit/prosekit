5f71f0c4c9b835de9935dff081889ac1dbea623e
ocavue <ocavue@gmail.com>
2024-07-07 16:43:49 +1000

    add action

--- playground/examples/svelte-toolbar/editor.svelte
+++ playground/examples/svelte-toolbar/editor.svelte
@@ -3,24 +3,24 @@ import { Themes } from '@prosekit/themes'
 import 'prosekit/basic/style.css'
 
 import { defineBasicExtension } from 'prosekit/basic'
-import { createEditor, jsonFromNode, type NodeJSON } from 'prosekit/core'
-import { ProseKit, useDocChange } from 'prosekit/svelte'
-import { onDestroy, onMount } from 'svelte'
+import { createEditor } from 'prosekit/core'
+import { ProseKit } from 'prosekit/svelte'
 import Toolbar from './toolbar.svelte'
 
 const extension = defineBasicExtension()
 const editor = createEditor({ extension })
 
-let place: HTMLDivElement
-onMount(() => editor.mount(place))
-onDestroy(() => editor.mount(null))
+const mount = (element: HTMLElement) => {
+  editor.mount(element)
+  return { destroy: () => editor.mount(null) }
+}
 </script>
 
 <ProseKit {editor}>
   <div class={Themes.EDITOR_VIEWPORT}>
     <div class={Themes.EDITOR_SCROLLING}>
       <Toolbar />
-      <div bind:this={place} class={Themes.EDITOR_CONTENT}></div>
+      <div use:mount class={Themes.EDITOR_CONTENT}></div>
     </div>
   </div>
 </ProseKit>

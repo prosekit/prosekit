35991ffb3ea5e0795f165d24187decec121f4b5b
ocavue <ocavue@gmail.com>
2024-07-08 01:02:57 +1000

    fix

--- playground/examples/vue-code/editor.vue
+++ playground/examples/vue-code/editor.vue
@@ -15,7 +15,10 @@ const defaultHTML = `
 
 const editor = createEditor({ extension: defineExtension(), defaultHTML })
 const editorRef = ref<HTMLDivElement | null>(null)
-watchPostEffect(() => editor.mount(editorRef.value))
+watchPostEffect((onCleanup) => {
+  editor.mount(editorRef.value)
+  onCleanup(() => editor.unmount())
+})
 </script>
 
 <template>

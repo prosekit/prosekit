ff5f02afb453f3aeabd2dbf2410d6ee09aebe06a
ocavue <ocavue@gmail.com>
2024-07-07 17:39:58 +1000

    fix

--- playground/examples/svelte-toolbar/editor.svelte
+++ playground/examples/svelte-toolbar/editor.svelte
@@ -2,12 +2,12 @@
 import { Themes } from '@prosekit/themes'
 import 'prosekit/basic/style.css'
 
-import { defineBasicExtension } from 'prosekit/basic'
 import { createEditor } from 'prosekit/core'
 import { ProseKit } from 'prosekit/svelte'
+import { defineExtension } from './extension'
 import Toolbar from './toolbar.svelte'
 
-const extension = defineBasicExtension()
+const extension = defineExtension()
 const editor = createEditor({ extension })
 
 const mount = (element: HTMLElement) => {

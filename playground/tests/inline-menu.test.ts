06a425a98840a85aaa48b5ff765c3e3912977744
ocavue <ocavue@gmail.com>
2024-07-08 22:22:26 +1000

    fix svelte handle

--- playground/tests/inline-menu.test.ts
+++ playground/tests/inline-menu.test.ts
@@ -66,8 +66,17 @@ testStory(['inline-menu', 'full'], () => {
   })
 
   test('inline link', async ({ page }) => {
-    const { editor, mainMenu, linkMenu, linkInput, linkTag, typeAndSelect } =
-      await setup(page)
+    const {
+      editor,
+      mainMenu,
+      linkMenu,
+      linkInput,
+      linkTag,
+      typeAndSelect,
+      selectTextFromEnd,
+    } = await setup(page)
+
+    const linkButton = mainMenu.getByRole('button', { name: 'Link' })
 
     await expect(mainMenu).toBeHidden()
     await expect(linkMenu).toBeHidden()
@@ -78,36 +87,68 @@ testStory(['inline-menu', 'full'], () => {
     await expect(linkMenu).toBeHidden()
 
     // Show the link menu
-    await mainMenu.getByRole('button', { name: 'Link' }).click()
+    await linkButton.click()
     await expect(linkMenu).toBeVisible()
     await expect(linkMenu.getByRole('textbox')).toBeEmpty()
     await expect(linkMenu.getByRole('textbox')).toHaveAttribute(
       'placeholder',
       'Paste the link...',
     )
-    await mainMenu.getByRole('button', { name: 'Link' }).click()
+    await linkButton.click()
     await expect(linkMenu).toBeHidden()
 
     // Apply the link
     await expect(linkTag).toBeHidden()
-    await mainMenu.getByRole('button', { name: 'Link' }).click()
+    await linkButton.click()
     await expect(linkMenu).toBeVisible()
     await expect(linkInput).toBeVisible()
     await linkInput.fill('https://www.example.com')
     await linkInput.press('Enter')
+    await expect(mainMenu).toBeVisible()
     await expect(linkMenu).toBeHidden()
     await expect(linkInput).toBeHidden()
     await expect(linkTag).toBeVisible()
     expect(await linkTag.textContent()).toEqual('world')
 
-    // Remove the link
+    // Show the link menu again
+    await expect(mainMenu).toBeVisible()
     await expect(linkMenu).toBeHidden()
-    await mainMenu.getByRole('button', { name: 'Link' }).click()
+    await linkButton.click()
+    await expect(mainMenu).toBeVisible()
+    await expect(linkMenu).toBeVisible()
+
+    // Deselect the text and dismiss menus
+    expect(await getSelectedText(page)).toEqual('world')
+    await editor.click()
+    expect(await getSelectedText(page)).toEqual('')
+    await expect(mainMenu).toBeHidden()
+    await expect(linkMenu).toBeHidden()
+
+    // Move the text cursor to the end of the text
+    for (let i = 0; i < 20; i++) {
+      await editor.press('ArrowRight')
+    }
+    // Select the text again and show the main menu
+    await selectTextFromEnd(2)
+    expect(await getSelectedText(page)).toEqual('ld')
+    await expect(mainMenu).toBeVisible()
+    await expect(linkMenu).toBeHidden()
+
+    // Show the link menu
+    await linkButton.click()
+    await expect(mainMenu).toBeVisible()
+    await expect(linkMenu).toBeVisible()
+
+    // The text selection should expand to include the whole link
+    expect(await getSelectedText(page)).toEqual('world')
+
+    // Remove the link
     await expect(linkMenu).toBeVisible()
     await expect(linkMenu.getByRole('textbox')).toHaveValue(
       'https://www.example.com',
     )
     await linkMenu.getByRole('button', { name: 'Remove link' }).click()
+    await expect(mainMenu).toBeVisible()
     await expect(linkMenu).toBeHidden()
     await expect(linkTag).toBeHidden()
     expect(await editor.textContent()).toContain('world')
@@ -124,19 +165,21 @@ async function setup(page: Page) {
 
   await emptyEditor(page)
 
+  const selectTextFromEnd = async (chars: number) => {
+    await page.keyboard.down('Shift')
+    for (let i = 0; i < chars; i++) {
+      await page.keyboard.press('ArrowLeft')
+    }
+    await page.keyboard.up('Shift')
+  }
+
   const typeAndSelect = async () => {
     // Type "Hello world"
     await editor.focus()
     await editor.pressSequentially('Hello world')
 
     // Select the word "world"
-    await page.keyboard.down('Shift')
-    await page.keyboard.press('ArrowLeft')
-    await page.keyboard.press('ArrowLeft')
-    await page.keyboard.press('ArrowLeft')
-    await page.keyboard.press('ArrowLeft')
-    await page.keyboard.press('ArrowLeft')
-    await page.keyboard.up('Shift')
+    await selectTextFromEnd(5)
   }
 
   return {
@@ -145,6 +188,7 @@ async function setup(page: Page) {
     linkMenu,
     linkInput,
     linkTag,
+    selectTextFromEnd,
     typeAndSelect,
   }
 }

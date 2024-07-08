570767773af2f30b4f4a2905300264201d129615
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-07-09 01:07:11 +0200

    feat: improve paragraph block node

--- packages/core/src/extensions/paragraph.ts
+++ packages/core/src/extensions/paragraph.ts
@@ -1,6 +1,10 @@
+import { insertNode } from '../commands/insert-node'
+import { setBlockType } from '../commands/set-block-type'
+import { toggleNode } from '../commands/toggle-node'
 import { withPriority } from '../editor/with-priority'
 import { Priority } from '../types/priority'
 
+import { defineCommands } from './command'
 import { defineNodeSpec } from './node-spec'
 
 /**
@@ -19,6 +23,33 @@ export function defineParagraphSpec() {
     },
   })
 }
+
+// TODO: Do we need all these commands?
+export function defineParagraphCommands() {
+  return defineCommands({
+    setParagraph: () => setBlockType({ type: 'paragraph' }),
+    insertParagraph: () => insertNode({ type: 'paragraph' }),
+    toggleHeading: () => toggleNode({ type: 'paragraph' }),
+  })
+}
+
+// TODO: If necessary, what's an appropriate keymap for a paragraph? Something like 'Mod-p' would conflict with the browser's print dialog, right?
+
+// TODO: How do we handle input rules inside `core`? I saw all the input rules were inside extensions, but I'm not sure how to handle them here. This is actually the one feature I most cared about, just not sure if this is the right way to do it.
+/**
+ * Converts the text block to a paragraph when the block becomes empty due to
+ * continuous Backspace presses.
+ *
+ * This rule matches an empty block (represented by '^$') and converts it to
+ * a paragraph. It's triggered when a user completely empties a block by
+ * pressing Backspace repeatedly.
+ */
+// export function defineParagraphInputRule() {
+//   return defineTextBlockInputRule({
+//     regex: /^$/,
+//     type: 'paragraph',
+//   })
+// }
 
 /**
  * @public

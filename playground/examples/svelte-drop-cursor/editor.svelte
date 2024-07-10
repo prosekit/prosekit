fa723b5a32d6ce97aeadd81aa350bc2a6fad1794
SHYAKA Davis <87414827+shyakadavis@users.noreply.github.com>
2024-07-10 18:13:06 +0200

    docs: add svelte-drop-cursor example (#651)

---
+++ playground/examples/svelte-drop-cursor/editor.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Themes } from '@prosekit/themes'
+import 'prosekit/basic/style.css'
+
+import { createEditor } from 'prosekit/core'
+import { ProseKit } from 'prosekit/svelte'
+import { defineExtension } from './extension'
+import { defaultDoc } from './default-doc'
+
+const editor = createEditor({ extension: defineExtension(), defaultDoc })
+
+const mount = (element: HTMLElement) => {
+  editor.mount(element)
+  return { destroy: () => editor.unmount() }
+}
+</script>
+
+<ProseKit {editor}>
+  <div class={Themes.EDITOR_VIEWPORT}>
+    <div class={Themes.EDITOR_SCROLLING}>
+      <div use:mount class={Themes.EDITOR_CONTENT}></div>
+    </div>
+  </div>
+</ProseKit>

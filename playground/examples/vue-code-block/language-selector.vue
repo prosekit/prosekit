23fbc661c3f02cfa1d33f7d7ef2ee4e4cac1412f
ManUtopiK <emmanuel.salomon@gmail.com>
2024-07-10 03:23:14 +0200

    Use v-model and writable computed instead of props

--- playground/examples/vue-code-block/language-selector.vue
+++ playground/examples/vue-code-block/language-selector.vue
@@ -2,22 +2,14 @@
 import { Themes } from '@prosekit/themes'
 import { shikiBundledLanguagesInfo } from 'prosekit/extensions/code-block'
 
-const props = defineProps<{
-  language?: string
-  setLanguage: (language: string) => void
-}>()
-
-const handleChange = (event: Event) => {
-  props.setLanguage((event.target as HTMLSelectElement).value)
-}
+const language = defineModel<string>()
 </script>
 
 <template>
   <div :class="Themes.LANGUAGE_WRAPPER" contenteditable="false">
     <select
       :class="Themes.LANGUAGE_SELECT"
-      @change="handleChange"
-      :value="props.language || ''"
+      v-model="language"
     >
       <option value="">Plain Text</option>
       <option

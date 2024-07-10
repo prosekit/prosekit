d76b2477cd52fe93e605c7047a04cf55d05f3e51
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-07-10 13:06:47 +0200

    docs: add svelte-drop-cursor example

---
+++ playground/examples/svelte-drop-cursor/extension.ts
@@ -0,0 +1,27 @@
+import { Themes } from '@prosekit/themes'
+import {
+  defineBaseKeymap,
+  defineDoc,
+  defineParagraph,
+  defineText,
+  union,
+} from 'prosekit/core'
+import { defineDropCursor } from 'prosekit/extensions/drop-cursor'
+import { defineImage } from 'prosekit/extensions/image'
+
+export function defineExtension() {
+  return union([
+    defineBaseKeymap(),
+    defineDoc(),
+    defineText(),
+    defineParagraph(),
+    defineImage(),
+    defineDropCursor({
+      color: false,
+      width: 4,
+      class: Themes.DROP_CURSOR,
+    }),
+  ])
+}
+
+export type EditorExtension = ReturnType<typeof defineExtension>

cac5366bf0552a233c4716e65c9d7e84a99ec942
ocavue <ocavue@gmail.com>
2024-07-15 16:22:25 +1000

    comment

--- packages/svelte/src/components/client-update/client-update.svelte
+++ packages/svelte/src/components/client-update/client-update.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 import { onMount } from 'svelte'
 
-// 
+// Forces a re-render when the component mounts. This resolves issues with
+// web-components after server-side rendering.
 let key = 1
 
 onMount(() => {

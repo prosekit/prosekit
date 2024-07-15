24c8f4f4e6d5dd3264bcf3d207f65661249bd72f
ocavue <ocavue@gmail.com>
2024-07-15 13:07:28 +1000

    fix(vue): force components to update after SSR

--- packages/vue/src/components/create-component.ts
+++ packages/vue/src/components/create-component.ts
@@ -1,6 +1,9 @@
+import type { Editor } from '@prosekit/core'
 import {
   defineComponent,
   h,
+  onMounted,
+  ref,
   type DefineSetupFnComponent,
   type HTMLAttributes,
 } from 'vue'
@@ -16,24 +19,44 @@ export function createComponent<Props extends object>(
 
   const hasEditor = Object.hasOwn(defaultProps, 'editor')
 
+  const setupProps = (
+    props: Record<string, unknown>,
+    editor: Editor | undefined,
+  ): Record<string, unknown> => {
+    const properties: Record<string, unknown> = {}
+
+    for (const [key, value] of Object.entries(props)) {
+      if (value !== undefined) {
+        properties[propertyNames.includes(key) ? '.' + key : key] = value
+      }
+    }
+
+    if (hasEditor && editor && !properties['editor']) {
+      properties.editor = editor
+    }
+
+    return properties
+  }
+
+  const usePropsRef = (props: Record<string, unknown>) => {
+    const editor = useEditorContext()
+    const propsRef = ref<Record<string, unknown>>(setupProps(props, editor))
+
+    // Update the props when the component is mounted, to force the component to
+    // update even if it has been rendered during server-side rendering.
+    onMounted(() => {
+      propsRef.value = setupProps(props, editor)
+    })
+
+    return propsRef
+  }
+
   const Component = defineComponent(
     (props: Record<string, unknown>, { slots }) => {
-      const editor = useEditorContext()
+      const propsRef = usePropsRef(props)
 
       return () => {
-        const p: Record<string, unknown> = {}
-
-        for (const [key, value] of Object.entries(props)) {
-          if (value !== undefined) {
-            p[propertyNames.includes(key) ? '.' + key : key] = value
-          }
-        }
-
-        if (hasEditor && editor && !p['editor']) {
-          p.editor = editor
-        }
-
-        return h(tagName, p, slots.default?.())
+        return h(tagName, propsRef.value, slots.default?.())
       }
     },
     {

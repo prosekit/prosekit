517ac8bc0e260509f5fb5d8db5c22d210847b14d
ocavue <ocavue@gmail.com>
2024-07-15 21:47:34 +1000

    add test

---
+++ playground/tests/minimal.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@playwright/test'
+
+import { testStory, waitForEditor } from './helper'
+
+testStory('minimal', () => {
+  test('typing', async ({ page }) => {
+    const editor = await waitForEditor(page)
+    expect(await editor.textContent()).not.toContain('Hello')
+    await editor.pressSequentially('Hello ')
+    expect(await editor.textContent()).toContain('Hello')
+  })
+})

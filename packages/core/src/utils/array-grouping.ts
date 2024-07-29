bc643adc69e6e1a731cedd71418c42177ad7bc7c
ocavue <ocavue@gmail.com>
2024-07-29 21:28:13 +1000

    small refactor

--- packages/core/src/utils/array-grouping.ts
+++ packages/core/src/utils/array-grouping.ts
@@ -15,15 +15,11 @@ export function groupBy<K extends PropertyKey, T>(
 
 export function groupEntries<T extends Record<string, any>>(
   entries: ObjectEntries<T>[],
-): GroupedEntries<T> {
-  const result: GroupedEntries<T> = {}
+): { [K in keyof T]?: T[K][] } {
+  const result: { [K in keyof T]?: T[K][] } = {}
   for (const [key, value] of entries) {
     const values = (result[key] ||= [])
     values.push(value)
   }
   return result
-}
-
-export type GroupedEntries<T extends Record<string, any>> = {
-  [K in keyof T]?: T[K][]
 }

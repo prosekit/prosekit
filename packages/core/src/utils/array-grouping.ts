5196e1b6d4e875b68d3ea5f38c3414a62ed6fd77
ocavue <ocavue@gmail.com>
2024-07-29 21:26:24 +1000

    bug fix

--- packages/core/src/utils/array-grouping.ts
+++ packages/core/src/utils/array-grouping.ts
@@ -1,29 +1,27 @@
 import type { ObjectEntries } from '../types/object-entries'
 
-function groupByPolyfill(
-  items: Iterable<unknown>,
-  keySelector: (item: unknown, index: number) => string,
-): Record<string, unknown[]> {
-  const result: Record<string, unknown[]> = {}
-  let index = 0
+export function groupBy<K extends PropertyKey, T>(
+  items: Iterable<T>,
+  keySelector: (item: T) => K,
+): Partial<Record<K, T[]>> {
+  const result: Partial<Record<K, T[]>> = {}
   for (const item of items) {
-    const key = keySelector(item, index)
-    index++
+    const key = keySelector(item)
     const values = (result[key] ||= [])
     values.push(item)
   }
   return result
 }
 
-export const groupBy: <K extends PropertyKey, T>(
-  items: Iterable<T>,
-  keySelector: (item: T, index: number) => K,
-) => Partial<Record<K, T[]>> = Object.groupBy || groupByPolyfill
-
 export function groupEntries<T extends Record<string, any>>(
   entries: ObjectEntries<T>[],
 ): GroupedEntries<T> {
-  return groupBy(entries, ([key]) => key) as GroupedEntries<T>
+  const result: GroupedEntries<T> = {}
+  for (const [key, value] of entries) {
+    const values = (result[key] ||= [])
+    values.push(value)
+  }
+  return result
 }
 
 export type GroupedEntries<T extends Record<string, any>> = {

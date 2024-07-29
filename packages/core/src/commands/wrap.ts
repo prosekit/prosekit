abea86d2926c7248e618c5055629ecb13c6d56a5
ocavue <ocavue@gmail.com>
2024-07-29 23:49:27 +1000

    update docs

--- packages/core/src/commands/wrap.ts
+++ packages/core/src/commands/wrap.ts
@@ -2,29 +2,43 @@ import { NodeType, type Attrs } from '@prosekit/pm/model'
 import type { Command } from '@prosekit/pm/state'
 import { findWrapping } from '@prosekit/pm/transform'
 
+import { getNodeType } from '../utils/get-node-type'
+
 /**
  * @public
  */
 export interface WrapOptions {
+  /**
+   * The node type to wrap the selected textblock with.
+   */
+  type: NodeType | string
+
+  /**
+   * @deprecated Use `nodeSpec` instead.
+   */
   nodeType: NodeType
+
+  /**
+   * Optional attributes to apply to the node.
+   */
   attrs?: Attrs | null
 }
 
 /**
- * Returns a command that wraps the selected textblock with the given node type
- * with the given attributes.
+ * Returns a command that wraps the selected textblock with the given node type.
  *
  * @param options
  *
  * @public
  */
-export function wrap({ nodeType, attrs }: WrapOptions): Command {
+export function wrap(options: WrapOptions): Command {
   return (state, dispatch) => {
     const { $from, $to } = state.selection
     const range = $from.blockRange($to)
     if (!range) return false
 
-    const wrapping = findWrapping(range, nodeType, attrs)
+    const nodeType = getNodeType(state.schema, options.type || options.nodeType)
+    const wrapping = findWrapping(range, nodeType, options.attrs)
     if (!wrapping) return false
 
     dispatch?.(state.tr.wrap(range, wrapping))

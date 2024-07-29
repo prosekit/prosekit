e68984b55c2f767a50c8eda8f7f5dfbdb4fa22c5
ocavue <ocavue@gmail.com>
2024-07-29 16:49:40 +1000

    type-assertion

--- packages/core/src/utils/type-assertion.ts
+++ packages/core/src/utils/type-assertion.ts
@@ -7,42 +7,52 @@ import {
 } from '@prosekit/pm/state'
 
 /**
- * @internal
+ * Checks if the given object is a `ProseMirrorNode` instance.
  */
 export function isProseMirrorNode(node: unknown): node is ProseMirrorNode {
   return node instanceof ProseMirrorNode
 }
 
 /**
- * @internal
+ * Checks if the given object is a `Mark` instance.
+ *
+ * @public
  */
 export function isMark(mark: unknown): mark is Mark {
   return mark instanceof Mark
 }
 
 /**
- * @internal
+ * Checks if the given object is a `Selection` instance.
+ *
+ * @public
  */
 export function isSelection(sel: unknown): sel is Selection {
   return sel instanceof Selection
 }
 
 /**
- * @internal
+ * Checks if the given object is a `TextSelection` instance.
+ *
+ * @public
  */
 export function isTextSelection(sel: Selection): sel is TextSelection {
   return sel instanceof TextSelection
 }
 
 /**
- * @internal
+ * Checks if the given object is a `NodeSelection` instance.
+ *
+ * @public
  */
 export function isNodeSelection(sel: Selection): sel is NodeSelection {
   return sel instanceof NodeSelection
 }
 
 /**
- * @internal
+ * Checks if the given object is a `AllSelection` instance.
+ *
+ * @public
  */
 export function isAllSelection(sel: Selection): sel is AllSelection {
   return sel instanceof AllSelection

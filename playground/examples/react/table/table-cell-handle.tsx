318d31d66deac47208ce7269cd00599b9173ad94
ocavue <ocavue@gmail.com>
2024-08-29 00:56:45 +1000

    Remove unused throttle

--- playground/examples/react/table/table-cell-handle.tsx
+++ playground/examples/react/table/table-cell-handle.tsx
@@ -67,18 +67,3 @@ export function TableCellHandle() {
     </TableCellPopoverRoot>
   )
 }
-
-export function throttle<Args extends any[]>(
-  callback: (...args: Args) => void,
-  wait: number,
-): (...args: Args) => void {
-  let lastTime = 0
-
-  return (...args: Args) => {
-    const now = Date.now()
-    if (now - lastTime >= wait) {
-      callback(...args)
-      lastTime = now
-    }
-  }
-}

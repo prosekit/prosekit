720e0a1137cdbfb7e231c10ac0a72c7d367eacc5
ocavue <ocavue@gmail.com>
2024-08-29 03:17:11 +1000

    remove some contexts

--- packages/web/src/components/table-handle/context.ts
+++ packages/web/src/components/table-handle/context.ts
@@ -1,37 +1,11 @@
 import { createContext } from '@aria-ui/core'
-import type { FindParentNodeResult } from '@prosekit/core'
 import type { ResolvedPos } from '@prosekit/pm/model'
-import type { CellSelection } from 'prosemirror-tables'
 
 export interface CellAxisWithPos {
   row: number
   col: number
   $cell: ResolvedPos
 }
-
-/**
- * @internal
- */
-export interface TableCellPopoverContext {
-  cellAxis: CellAxisWithPos | null
-  cellSelection: CellSelection | null
-}
-
-export interface TableHandlePopoverContext {
-  table: FindParentNodeResult | null
-  cellAxis?: CellAxisWithPos | null
-}
-
-/**
- * @internal
- */
-export const tableCellPopoverContext = createContext<TableCellPopoverContext>(
-  'prosekit-table-cell-popover-context',
-  {
-    cellAxis: null,
-    cellSelection: null,
-  },
-)
 
 /**
  * @internal

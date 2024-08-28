d70a0da472ba75fc709f3f65cd74c833243cf367
ocavue <ocavue@gmail.com>
2024-08-29 04:08:47 +1000

    fix

--- packages/web/src/components/table-handle/table-handle-popover-content/props.ts
+++ packages/web/src/components/table-handle/table-handle-popover-content/props.ts
@@ -1,20 +1,24 @@
 import { defaultMenuContentProps, type MenuContentProps } from '@aria-ui/menu'
-import type { Placement } from '@floating-ui/dom'
 import type { Editor } from '@prosekit/core'
 
 export interface TableHandlePopoverContentProps
-  extends Omit<MenuContentProps, 'placement'> {
+  extends Omit<MenuContentProps, 'placement' | 'offset'> {
   /**
-   * @default right
+   * @default 'bottom-start'
    */
-  placement: Placement
+  placement: MenuContentProps['placement']
+
+  /**
+   * @default {mainAxis: -4, crossAxis: 4}
+   */
+  offset: MenuContentProps['offset']
 
   editor: Editor | null
 }
 
 export const defaultTableHandlePopoverContentProps = Object.freeze({
   ...defaultMenuContentProps,
-  placement: 'right',
-  offset: 0,
+  placement: 'bottom-start',
+  offset: { mainAxis: -4, crossAxis: 4 },
   editor: null,
 } satisfies TableHandlePopoverContentProps)

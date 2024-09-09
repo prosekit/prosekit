90b08a17f8df767b909d86bef8cd13961b64c007
ocavue <ocavue@gmail.com>
2024-09-09 19:48:49 +1000

    list

--- packages/extensions/src/list/index.ts
+++ packages/extensions/src/list/index.ts
@@ -1,12 +1,3 @@
-import { union, type Union } from '@prosekit/core'
-
-import { defineListCommands, type ListCommandsExtension } from './list-commands'
-import { defineListInputRules } from './list-input-rules'
-import { defineListKeymap } from './list-keymap'
-import { defineListPlugins } from './list-plugins'
-import { defineListSpec, type ListSpecExtension } from './list-spec'
-import type { ListAttrs } from './list-types'
-
 export { ListDOMSerializer } from 'prosemirror-flat-list'
 export type {
   DedentListOptions,
@@ -15,32 +6,10 @@ export type {
   UnwrapListOptions,
   WrapInListGetAttrs,
 } from 'prosemirror-flat-list'
-
-/**
- * @internal
- */
-export type ListExtension = Union<[ListSpecExtension, ListCommandsExtension]>
-
-/**
- * @public
- */
-export function defineList(): ListExtension {
-  return union(
-    defineListSpec(),
-    defineListPlugins(),
-    defineListKeymap(),
-    defineListInputRules(),
-    defineListCommands(),
-  )
-}
-
-export {
-  defineListCommands,
-  defineListInputRules,
-  defineListKeymap,
-  defineListPlugins,
-  defineListSpec,
-  type ListAttrs,
-  type ListCommandsExtension,
-  type ListSpecExtension,
-}
+export { defineList, type ListExtension } from './list'
+export { defineListCommands, type ListCommandsExtension } from './list-commands'
+export { defineListInputRules } from './list-input-rules'
+export { defineListKeymap } from './list-keymap'
+export { defineListPlugins } from './list-plugins'
+export { defineListSpec, type ListSpecExtension } from './list-spec'
+export type { ListAttrs } from './list-types'

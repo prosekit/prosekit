b1ea0200fa10cc39b73b3a300d75428703278bd5
ocavue <ocavue@gmail.com>
2024-10-08 19:30:35 +1100

    ni

--- packages/dev/src/gen-typedoc-json.ts
+++ packages/dev/src/gen-typedoc-json.ts
@@ -4,19 +4,16 @@ import { normalizePackageJson } from './normalize-package-json.js'
 import { vfs } from './virtual-file-system.js'
 
 export async function genTypedocJson() {
-  const packages = await vfs.getPublicPackages()
-
-  for (const pkg of packages) {
-    const entryPoints = await normalizePackageJson(pkg)
-    const typedocJson = {
-      $schema: 'https://typedoc.org/schema.json',
-      extends: ['../../config/typedoc-base.json'],
-      entryPoints: Object.values(entryPoints)
-        .filter((entryPoint) => !entryPoint.endsWith('.css'))
-        .sort(),
-    }
-    const typedocJsonPath = path.join(pkg.relativeDir, 'typedoc.json')
-    const typedocJsonFile = await vfs.getFile(typedocJsonPath)
-    typedocJsonFile.updateJSON(typedocJson)
+  const pkg = await vfs.getPackageByName('prosekit')
+  const entryPoints = await normalizePackageJson(pkg)
+  const typedocJson = {
+    $schema: 'https://typedoc.org/schema.json',
+    extends: ['../../config/typedoc-base.json'],
+    entryPoints: Object.values(entryPoints)
+      .filter((entryPoint) => !entryPoint.endsWith('.css'))
+      .sort(),
   }
+  const typedocJsonPath = path.join(pkg.relativeDir, 'typedoc.gen.json')
+  const typedocJsonFile = await vfs.getFile(typedocJsonPath)
+  typedocJsonFile.updateJSON(typedocJson)
 }

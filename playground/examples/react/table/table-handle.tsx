7812cf4f00e5ea9dbcf27e146a552bf4d3f1e21f
Michael Wonng <michael@wonng.com>
2024-10-30 17:23:14 +1100

    change to text

--- playground/examples/react/table/table-handle.tsx
+++ playground/examples/react/table/table-handle.tsx
@@ -44,7 +44,7 @@ export function TableHandle() {
               onSelect={editor.commands.deleteCellSelection}
             >
               <span>Clear Contents</span>
-              <span className={Themes.POPOVER_SHORTCUT}>⌫</span>
+              <span className={Themes.POPOVER_SHORTCUT}>Del</span>
             </TableHandlePopoverItem>
           )}
           {editor.commands.deleteTableColumn.canExec() && (
@@ -84,7 +84,7 @@ export function TableHandle() {
               onSelect={editor.commands.deleteCellSelection}
             >
               <span>Clear Contents</span>
-              <span className={Themes.POPOVER_SHORTCUT}>⌫</span>
+              <span className={Themes.POPOVER_SHORTCUT}>Del</span>
             </TableHandlePopoverItem>
           )}
           {editor.commands.deleteTableRow.canExec() && (

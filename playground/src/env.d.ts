a36c4d33cd4cefba742bc93bb17f2c05a969748d
ocavue <ocavue@gmail.com>
2024-12-08 18:23:56 +1100

    update @ocavue/eslint-config

--- playground/src/env.d.ts
+++ playground/src/env.d.ts
@@ -1,4 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare module '*.astro'

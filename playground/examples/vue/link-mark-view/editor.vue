7d644d393d53fd5a96602b1286436070992fca60
ocavue <ocavue@gmail.com>
2024-12-19 16:51:18 +1100

    vue

---
+++ playground/examples/vue/link-mark-view/editor.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import 'prosekit/basic/style.css'
+
+import { Themes } from '@prosekit/themes'
+import { createEditor } from 'prosekit/core'
+import { ProseKit } from 'prosekit/vue'
+import { ref, watchPostEffect } from 'vue'
+
+import { defineExtension } from './extension'
+
+const defaultContent = `
+  <p>Here is a link that changes color every second: <a href="https://www.example.com">example link</a>
+`
+
+const editor = createEditor({ extension: defineExtension(), defaultContent })
+const editorRef = ref<HTMLDivElement | null>(null)
+watchPostEffect((onCleanup) => {
+  editor.mount(editorRef.value)
+  onCleanup(() => editor.unmount())
+})
+</script>
+
+<template>
+  <ProseKit :editor="editor">
+    <div :class="Themes.EDITOR_VIEWPORT">
+      <div :class="Themes.EDITOR_SCROLLING">
+        <div ref="editorRef" :class="Themes.EDITOR_CONTENT" />
+      </div>
+    </div>
+  </ProseKit>
+</template>
---
+++ playground/examples/vue/link-mark-view/extension.ts
@@ -0,0 +1,17 @@
+import { defineBasicExtension } from 'prosekit/basic'
+import { union } from 'prosekit/core'
+import { defineVueMarkView } from 'prosekit/vue'
+
+import LinkView from './link-view.vue'
+
+export function defineExtension() {
+  return union(
+    defineBasicExtension(),
+    defineVueMarkView({
+      name: 'link',
+      component: LinkView,
+    }),
+  )
+}
+
+export type EditorExtension = ReturnType<typeof defineExtension>

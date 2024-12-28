475a38a11f8f0ae2c9d55f175cbc1fb994b95274
ocavue <ocavue@gmail.com>
2024-12-28 21:41:05 +1100

    update packages/dev/tsconfig.json

--- packages/dev/dist/config-vitest.d.ts
+++ packages/dev/dist/config-vitest.d.ts
@@ -1,2 +1,2 @@
 /** @type {import('vitest/config').UserProjectConfigExport} */
-export const config: import('vitest/config').UserProjectConfigExport
+export const config: import("vitest/config").UserProjectConfigExport;

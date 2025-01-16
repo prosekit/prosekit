27a462e92236de8c8904df2388226b6a68efbf6b
ocavue <ocavue@gmail.com>
2025-01-17 02:05:08 +1100

    wip

--- website2/uno.config.ts
+++ website2/uno.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'unocss'
+import {
+  configDeps,
+  preset,
+} from '@prosekit/unocss-preset'
+import {
+  defineConfig,
+  transformerDirectives,
+  type UserConfig,
+} from 'unocss'
 
 export default defineConfig({
+  presets: [preset()],
+  configDeps: configDeps(),
   theme: {
     breakpoints: {
       // Match Starlight breakpoints
       // https://github.com/withastro/starlight/blob/893be3b4a106ab75b706142bbd06b00268ccc298/packages/starlight/style/util.css#L22-L43
-      sm: '30rem',
-      md: '50rem',
-      lg: '72rem',
-      xl: '90rem',
+      'sm': '30rem',
+      'md': '50rem',
+      'lg': '72rem',
+      'xl': '90rem',
       '2xl': '120rem',
     },
   },

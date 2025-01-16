35768b04ba3e1cc2863e91d1b982c126efd6a8dd
ocavue <ocavue@gmail.com>
2025-01-16 19:28:53 +1100

    add custom search css

--- website2/astro.config.ts
+++ website2/astro.config.ts
@@ -1,30 +1,34 @@
 // @ts-check
-import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-		UnoCSS(),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+      customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+      ],
+    }),
+    UnoCSS(),
+  ],
+})

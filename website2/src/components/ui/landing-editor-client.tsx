9d2f66828360a5317d4f9704ef112cea578537ed
ocavue <ocavue@gmail.com>
2025-01-22 23:46:35 +1100

    update ani

--- website2/src/components/ui/landing-editor-client.tsx
+++ website2/src/components/ui/landing-editor-client.tsx
@@ -3,5 +3,9 @@ import React from 'react'
 import Editor from '../../examples/react/full/editor'
 
 export const LandingEditorClient: React.FC = () => {
-  return <Editor />
+  return (
+    <div className="size-full [&>*>*]:animate-in [&>*>*]:fade-in">
+      <Editor />
+    </div>
+  )
 }

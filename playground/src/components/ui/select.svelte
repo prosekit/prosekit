890648b2501e489cd529c9ee29be3de8c64cf44f
ocavue <ocavue@gmail.com>
2025-02-07 14:25:11 +1100

    fix

--- playground/src/components/ui/select.svelte
+++ playground/src/components/ui/select.svelte
@@ -39,25 +39,22 @@ const selectedLabel = $derived(
 
 <Select.Root type="single" onValueChange={(v) => (value = v)}>
   <Select.Trigger
-    class="inline-flex h-input w-[296px] select-none items-center rounded-9px border border-border-input bg-background px-[11px] text-sm transition-colors placeholder:text-foreground-alt/50"
+    class="inline-flex h-12 w-[296px] select-none items-center rounded-lg border border-border outline-current bg-background px-[11px] text-sm transition-colors"
     aria-label="Select a theme"
   >
     <Palette class="mr-[9px] size-6 text-muted-foreground" />
-    {selectedLabel}
+    <span>{selectedLabel}</span>
     <CaretUpDown class="ml-auto size-6 text-muted-foreground" />
   </Select.Trigger>
   <Select.Portal>
     <Select.Content
-      class="focus-override z-50 max-h-96 w-[var(--bits-select-anchor-width)] min-w-[var(--bits-select-anchor-width)] select-none rounded-xl border border-muted bg-background px-1 py-3 shadow-popover outline-none data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+      class="z-50 max-h-96 w-[var(--bits-select-anchor-width)] min-w-[var(--bits-select-anchor-width)] select-none rounded-lg border border-muted bg-background p- shadow-lg outline-none data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
       sideOffset={10}
     >
-      <Select.ScrollUpButton class="flex w-full items-center justify-center">
-        <CaretDoubleUp class="size-3" />
-      </Select.ScrollUpButton>
       <Select.Viewport class="p-1">
         {#each themes as theme, i (i + theme.value)}
           <Select.Item
-            class="flex h-10 w-full select-none items-center rounded-button py-3 pl-5 pr-1.5 text-sm capitalize outline-none duration-75 data-[highlighted]:bg-muted data-[disabled]:opacity-50"
+            class="flex h-10 w-full select-none items-center rounded-md py-2 pl-5 pr-1.5 text-sm capitalize outline-none duration-75 data-[highlighted]:bg-muted data-[disabled]:opacity-50 scroll-m-1"
             value={theme.value}
             label={theme.label}
             disabled={theme.disabled}
@@ -73,9 +70,6 @@ const selectedLabel = $derived(
           </Select.Item>
         {/each}
       </Select.Viewport>
-      <Select.ScrollDownButton class="flex w-full items-center justify-center">
-        <CaretDoubleDown class="size-3" />
-      </Select.ScrollDownButton>
     </Select.Content>
   </Select.Portal>
 </Select.Root>
---
+++ playground/src/components/ui/select/index.ts
@@ -0,0 +1,9 @@
+import SelectContent from './select-content.astro'
+import SelectItem from './select-item.astro'
+import SelectList from './select-list.astro'
+
+export {
+  SelectContent,
+  SelectItem,
+  SelectList,
+}

df48cdee82ec913c6930fe60074e861602fdb6c0
ocavue <ocavue@gmail.com>
2025-02-07 17:44:37 +1100

    wip

--- playground/src/components/ui/select/index.ts
+++ playground/src/components/ui/select/index.ts
@@ -1,9 +1,11 @@
 import SelectContent from './select-content.astro'
 import SelectItem from './select-item.astro'
 import SelectList from './select-list.astro'
+import SelectTrigger from './select-trigger.astro'
 
 export {
   SelectContent,
   SelectItem,
   SelectList,
+  SelectTrigger,
 }

d72b90ab3b8cfdee73da261cf537036f9bd5cb77
ocavue <ocavue@gmail.com>
2025-02-11 23:53:06 +1100

    enable sitemap

--- playground/astro.config.ts
+++ playground/astro.config.ts
@@ -29,6 +29,7 @@ const sidebar: Sidebar = [
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://prosekit.dev',
   integrations: [
     starlight({
       title: 'ProseKit',

4c9b330e0ddb50ad3fc041d65e1b50fb79466a3d
ocavue <ocavue@gmail.com>
2025-02-11 23:58:06 +1100

    update astro

--- playground/src/components/ui/select/select-trigger.svelte
+++
@@ -1,5 +0,0 @@
-<aria-ui-select-trigger
-  class="flex min-h-9 w-40 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
->
-  <slot />
-</aria-ui-select-trigger>

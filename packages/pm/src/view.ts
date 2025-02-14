3c11244875880276cab01256a3386e118e59e0f4
ocavue <ocavue@gmail.com>
2025-02-14 18:58:43 +1100

    patch view

--- packages/pm/src/view.ts
+++ packages/pm/src/view.ts
@@ -1,9 +1,1 @@
 export * from 'prosemirror-view'
-
-declare global {
-  interface Node {
-    /** @internal */
-    // @ts-expect-error: Remove the pmViewDesc property to avoid it from showing in the docs.
-    pmViewDesc?: undefined
-  }
-}

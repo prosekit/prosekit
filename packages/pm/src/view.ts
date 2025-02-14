18936d45d453889240195f699f7c06b73b2c7cfb
ocavue <ocavue@gmail.com>
2025-02-14 18:40:45 +1100

    add view

--- packages/pm/src/view.ts
+++ packages/pm/src/view.ts
@@ -1,1 +1,9 @@
 export * from 'prosemirror-view'
+
+declare global {
+  interface Node {
+    /** @internal */
+    // @ts-expect-error: Remove the pmViewDesc property to avoid it from showing in the docs.
+    pmViewDesc?: undefined
+  }
+}

b61e36177143184f9ab14e7a629e5bbc9f784b3e
ocavue <ocavue@gmail.com>
2025-03-18 04:37:42 +1100

    change

---
+++ website/src/shared/svelte/slash-menu-empty.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { Themes } from '@prosekit/themes'
+import { AutocompleteEmpty } from 'prosekit/svelte/autocomplete'
+</script>
+
+<AutocompleteEmpty class={Themes.AUTOCOMPLETE_MENU_ITEM}>
+  <span>No results</span>
+</AutocompleteEmpty>

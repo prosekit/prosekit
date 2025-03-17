eef118760eb304678248922d549e3f083407790a
ocavue <ocavue@gmail.com>
2025-03-18 04:20:44 +1100

    rewrite react/full/slash-menu.tsx

--- website/src/examples/react/full/slash-menu.tsx
+++ website/src/examples/react/full/slash-menu.tsx
@@ -1,13 +1,13 @@
 import { Themes } from '@prosekit/themes'
 import { useEditor } from 'prosekit/react'
 import {
-  AutocompleteEmpty,
-  AutocompleteItem,
   AutocompleteList,
   AutocompletePopover,
 } from 'prosekit/react/autocomplete'
 
 import type { EditorExtension } from './extension'
+import SlashMenuEmpty from './slash-menu-empty'
+import SlashMenuItem from './slash-menu-item'
 
 export default function SlashMenu() {
   const editor = useEditor<EditorExtension>()
@@ -15,50 +15,77 @@ export default function SlashMenu() {
   return (
     <AutocompletePopover regex={/\/.*$/iu} className={Themes.AUTOCOMPLETE_MENU}>
       <AutocompleteList>
-        <AutocompleteEmpty className={Themes.AUTOCOMPLETE_MENU_ITEM}>
-          No results
-        </AutocompleteEmpty>
+        <SlashMenuItem
+          label="Text"
+          onSelect={() => editor.commands.setParagraph()}
+        />
 
-        <AutocompleteItem
-          className={Themes.AUTOCOMPLETE_MENU_ITEM}
+        <SlashMenuItem
+          label="Heading 1"
+          kbd="#"
           onSelect={() => editor.commands.setHeading({ level: 1 })}
-        >
-          Heading 1
-        </AutocompleteItem>
-        <AutocompleteItem
-          className={Themes.AUTOCOMPLETE_MENU_ITEM}
+        />
+
+        <SlashMenuItem
+          label="Heading 2"
+          kbd="##"
           onSelect={() => editor.commands.setHeading({ level: 2 })}
-        >
-          Heading 2
-        </AutocompleteItem>
+        />
 
-        <AutocompleteItem
-          className={Themes.AUTOCOMPLETE_MENU_ITEM}
-          onSelect={() => editor.commands.wrapInList({ kind: 'task' })}
-        >
-          Task list
-        </AutocompleteItem>
+        <SlashMenuItem
+          label="Heading 3"
+          kbd="###"
+          onSelect={() => editor.commands.setHeading({ level: 3 })}
+        />
 
-        <AutocompleteItem
-          className={Themes.AUTOCOMPLETE_MENU_ITEM}
+        <SlashMenuItem
+          label="Bullet list"
+          kbd="-"
           onSelect={() => editor.commands.wrapInList({ kind: 'bullet' })}
-        >
-          Bullet list
-        </AutocompleteItem>
+        />
 
-        <AutocompleteItem
-          className={Themes.AUTOCOMPLETE_MENU_ITEM}
+        <SlashMenuItem
+          label="Ordered list"
+          kbd="1."
           onSelect={() => editor.commands.wrapInList({ kind: 'ordered' })}
-        >
-          Ordered list
-        </AutocompleteItem>
+        />
 
-        <AutocompleteItem
-          className={Themes.AUTOCOMPLETE_MENU_ITEM}
+        <SlashMenuItem
+          label="To-do list"
+          kbd="[]"
+          onSelect={() => editor.commands.wrapInList({ kind: 'task' })}
+        />
+
+        <SlashMenuItem
+          label="Toggle list"
+          kbd=">>"
           onSelect={() => editor.commands.wrapInList({ kind: 'toggle' })}
-        >
-          Toggle list
-        </AutocompleteItem>
+        />
+
+        <SlashMenuItem
+          label="Quote"
+          kbd=">"
+          onSelect={() => editor.commands.setBlockquote()}
+        />
+
+        <SlashMenuItem
+          label="Table"
+          onSelect={() => editor.commands.insertTable({ row: 3, col: 3 })}
+        />
+
+        <SlashMenuItem
+          label="Divider"
+          kbd="---"
+          onSelect={() => editor.commands.insertHorizontalRule()}
+        />
+
+        <SlashMenuItem
+          label="Code"
+          kbd="```"
+          onSelect={() => editor.commands.setCodeBlock()}
+        />
+
+        <SlashMenuEmpty />
       </AutocompleteList>
     </AutocompletePopover>
   )

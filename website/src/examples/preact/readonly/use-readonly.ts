94ceb2359d2607a04076f185997e31fcda90823c
ocavue <ocavue@gmail.com>
2025-03-18 05:19:09 +1100

    move

--- website/src/examples/preact/readonly/use-readonly.ts
+++ website/src/examples/preact/readonly/use-readonly.ts
@@ -1,17 +1,1 @@
-import {
-  useMemo,
-  useState,
-} from 'preact/hooks'
-import { defineReadonly } from 'prosekit/extensions/readonly'
-import { useExtension } from 'prosekit/preact'
-
-export function useReadonly() {
-  const [readonly, setReadonly] = useState(true)
-
-  const extension = useMemo(() => {
-    return readonly ? defineReadonly() : null
-  }, [readonly])
-  useExtension(extension)
-
-  return { readonly, setReadonly }
-}
+../../../shared/preact/use-readonly.ts
---
+++ website/src/shared/vue/user-menu.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Themes } from '@prosekit/themes'
+import { useEditor } from 'prosekit/vue'
+import {
+  AutocompleteEmpty,
+  AutocompleteItem,
+  AutocompleteList,
+  AutocompletePopover,
+} from 'prosekit/vue/autocomplete'
+
+import type { EditorExtension } from './extension'
+import { users } from './user-data'
+
+const editor = useEditor<EditorExtension>()
+
+function handleUserInsert(id: number, username: string) {
+  editor.value.commands.insertMention({
+    id: id.toString(),
+    value: '@' + username,
+    kind: 'user',
+  })
+  editor.value.commands.insertText({ text: ' ' })
+}
+</script>
+
+<template>
+  <AutocompletePopover :regex="/@\w*$/" :class="Themes.AUTOCOMPLETE_MENU">
+    <AutocompleteList>
+      <AutocompleteEmpty :class="Themes.AUTOCOMPLETE_MENU_ITEM">
+        No results
+      </AutocompleteEmpty>
+      <AutocompleteItem
+        v-for="user in users"
+        :key="user.id"
+        :class="Themes.AUTOCOMPLETE_MENU_ITEM"
+        @select="() => handleUserInsert(user.id, user.name)"
+      >
+        {{ user.name }}
+      </AutocompleteItem>
+    </AutocompleteList>
+  </AutocompletePopover>
+</template>

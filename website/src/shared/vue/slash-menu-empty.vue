a669057694a9829d94c63ac3e1c332a99dd568f1
ocavue <ocavue@gmail.com>
2025-03-18 04:35:50 +1100

    update

---
+++ website/src/shared/vue/slash-menu-empty.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Themes } from '@prosekit/themes'
+import { AutocompleteEmpty } from 'prosekit/vue/autocomplete'
+</script>
+
+<template>
+  <AutocompleteEmpty :class="Themes.AUTOCOMPLETE_MENU_ITEM">
+    <span>No results</span>
+  </AutocompleteEmpty>
+</template>
---
+++ website/src/shared/vue/slash-menu.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { Themes } from '@prosekit/themes'
+import { useEditor } from 'prosekit/vue'
+import {
+  AutocompleteList,
+  AutocompletePopover,
+} from 'prosekit/vue/autocomplete'
+
+import type { EditorExtension } from './extension'
+import SlashMenuEmpty from './slash-menu-empty.vue'
+import SlashMenuItem from './slash-menu-item.vue'
+
+const editor = useEditor<EditorExtension>()
+</script>
+
+<template>
+  <AutocompletePopover :regex="/\/.*$/iu" :class="Themes.AUTOCOMPLETE_MENU">
+    <AutocompleteList>
+      <SlashMenuItem
+        label="Text"
+        @select="() => editor.commands.setParagraph()"
+      />
+
+      <SlashMenuItem
+        label="Heading 1"
+        kbd="#"
+        @select="() => editor.commands.setHeading({ level: 1 })"
+      />
+
+      <SlashMenuItem
+        label="Heading 2"
+        kbd="##"
+        @select="() => editor.commands.setHeading({ level: 2 })"
+      />
+
+      <SlashMenuItem
+        label="Heading 3"
+        kbd="###"
+        @select="() => editor.commands.setHeading({ level: 3 })"
+      />
+
+      <SlashMenuItem
+        label="Bullet list"
+        kbd="-"
+        @select="() => editor.commands.wrapInList({ kind: 'bullet' })"
+      />
+
+      <SlashMenuItem
+        label="Ordered list"
+        kbd="1."
+        @select="() => editor.commands.wrapInList({ kind: 'ordered' })"
+      />
+
+      <SlashMenuItem
+        label="To-do list"
+        kbd="[]"
+        @select="() => editor.commands.wrapInList({ kind: 'task' })"
+      />
+
+      <SlashMenuItem
+        label="Toggle list"
+        kbd=">>"
+        @select="() => editor.commands.wrapInList({ kind: 'toggle' })"
+      />
+
+      <SlashMenuItem
+        label="Quote"
+        kbd=">"
+        @select="() => editor.commands.setBlockquote()"
+      />
+
+      <SlashMenuItem
+        label="Table"
+        @select="() => editor.commands.insertTable({ row: 3, col: 3 })"
+      />
+
+      <SlashMenuItem
+        label="Divider"
+        kbd="---"
+        @select="() => editor.commands.insertHorizontalRule()"
+      />
+
+      <SlashMenuItem
+        label="Code"
+        kbd="```"
+        @select="() => editor.commands.setCodeBlock()"
+      />
+
+      <SlashMenuEmpty />
+    </AutocompleteList>
+  </AutocompletePopover>
+</template>

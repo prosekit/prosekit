48aab1f04043db149a3778dbed0be59a49782a88
ocavue <ocavue@gmail.com>
2025-03-20 14:47:20 +1100

    chore: fix website build warning

--- website/src/components/ui/use-framework.svelte.ts
+++ website/src/components/ui/use-framework.svelte.ts
@@ -1,10 +1,21 @@
 import { PersistedState } from 'runed'
 
-export function useFramework(frameworks: string[]) {
-  const preferredFramework = new PersistedState('prosekit-docs-framework', 'react')
+export function useFramework(frameworks: string[]): { current: string } {
+  return typeof window === 'undefined'
+    ? useFrameworkServer(frameworks)
+    : useFrameworkClient(frameworks)
+}
+
+function useFrameworkServer(frameworks: string[]) {
+  return { current: frameworks[0] }
+}
+
+function useFrameworkClient(frameworks: string[]) {
+  const preferredFramework = new PersistedState('prosekit-docs-framework', frameworks[0])
+
   return {
     get current() {
-      return frameworks.includes(preferredFramework.current) ? preferredFramework.current : frameworks[0]
+      return preferredFramework.current
     },
     set current(value: string) {
       preferredFramework.current = value

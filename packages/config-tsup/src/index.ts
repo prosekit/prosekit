4dc466ca0dba758b09ab8b494635e785674e12ac
ocavue <ocavue@gmail.com>
2025-04-18 16:19:39 +1000

    update config-tsup

--- packages/config-tsup/src/index.ts
+++ packages/config-tsup/src/index.ts
@@ -1,12 +1,19 @@
+import path from 'node:path'
+
 import { merge } from 'lodash-es'
+import { pathExistsSync } from 'path-exists'
 import { readPackageUpSync } from 'read-package-up'
 import type { Options } from 'tsup'
 
-export function config(input?: Options): Options {
+export function config(options?: Options): Options {
   const pkg = readPackageUpSync()
   if (!pkg) {
     throw new Error('No package.json found')
   }
+
+  const packageDir = path.resolve(pkg.path, '..')
+  const tsconfigBuildPath = path.resolve(packageDir, 'tsconfig.build.json')
+  const tsconfigPath = path.resolve(packageDir, 'tsconfig.json')
 
   const packageJson = pkg.packageJson as {
     dev?: {
@@ -14,23 +21,34 @@ export function config(input?: Options): Options {
     }
   }
 
-  const entry = packageJson?.dev?.entry
+  const entryPoints = packageJson?.dev?.entry
 
-  if (!entry) {
+  if (!entryPoints) {
     throw new Error(`Unable to find the field "dev.entry" in ${pkg.path}`)
   }
 
-  const output: Options = {
-    entry,
+  const defaultOptions: Options = {
+    format: ['esm'],
+    entry: entryPoints,
+    splitting: true,
     sourcemap: false,
     clean: false,
-    dts: true,
-    // Bundling CSS files to remove the `@import` statements. This increases the
-    // compability of the output.
     noExternal: [/\.css$/i],
+    tsconfig: (pathExistsSync(tsconfigBuildPath)) ? tsconfigBuildPath : tsconfigPath,
+    experimentalDts: {
+      entry: removeCssEntryPoints(entryPoints),
+    },
   }
 
-  return deepMergeOptions(output, input)
+  return deepMergeOptions(defaultOptions, options)
+}
+
+function removeCssEntryPoints(
+  entryPoints: Record<string, string>,
+): Record<string, string> {
+  return Object.fromEntries(
+    Object.entries(entryPoints).filter(([_, filePath]) => !filePath.endsWith('.css')),
+  )
 }
 
 function deepMergeOptions(a: Options, b?: Options): Options {

478940cfad5f94a65777bcd75d16fd4e0b172ec1
ocavue <ocavue@users.noreply.github.com>
2025-04-18 17:35:16 +1000

    chore: fix changeset ignore config (#983)

--- packages/dev/src/gen-changeset-config-json.ts
+++ packages/dev/src/gen-changeset-config-json.ts
@@ -1,19 +1,27 @@
 import { vfs } from './virtual-file-system.js'
 
 export async function genChangesetConfigJson() {
-  const packages = await vfs.getPrivatePackages()
-  const packageNames = packages
+  const privatePackages = await vfs.getPrivatePackages()
+  const publicPackages = await vfs.getPublicPackages()
+
+  // Any package that is a dependency of a public package cannot be added to the
+  // "ignore" list.
+  const skipNames = new Set<string>()
+  for (const pkg of publicPackages) {
+    for (const deps of [pkg.packageJson.dependencies, pkg.packageJson.devDependencies]) {
+      for (const dep of Object.keys(deps || {})) {
+        skipNames.add(dep)
+      }
+    }
+  }
+
+  const ignoreNames: string[] = privatePackages
     .map((pkg) => pkg.packageJson.name)
-    .filter((name) => !skip.includes(name))
+    .filter((name) => !skipNames.has(name))
     .sort()
 
   const file = await vfs.getFile('.changeset/config.json')
   const json = await file.readJSON()
-  json.ignore = packageNames
+  json.ignore = ignoreNames
   file.updateJSON(json)
 }
-
-const skip = [
-  // Public packages depend on @prosekit/dev so we cannot ignore it
-  '@prosekit/dev',
-]

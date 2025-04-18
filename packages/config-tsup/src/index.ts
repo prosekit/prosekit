3f69696a7548b652e8f0f2c7597ebb236cb8d1e3
ocavue <ocavue@gmail.com>
2025-04-18 16:15:27 +1000

    add config-tsup

---
+++ packages/config-tsup/src/index.ts
@@ -0,0 +1,38 @@
+import { merge } from 'lodash-es'
+import { readPackageUpSync } from 'read-package-up'
+import type { Options } from 'tsup'
+
+export function config(input?: Options): Options {
+  const pkg = readPackageUpSync()
+  if (!pkg) {
+    throw new Error('No package.json found')
+  }
+
+  const packageJson = pkg.packageJson as {
+    dev?: {
+      entry: Record<string, string>
+    }
+  }
+
+  const entry = packageJson?.dev?.entry
+
+  if (!entry) {
+    throw new Error(`Unable to find the field "dev.entry" in ${pkg.path}`)
+  }
+
+  const output: Options = {
+    entry,
+    sourcemap: false,
+    clean: false,
+    dts: true,
+    // Bundling CSS files to remove the `@import` statements. This increases the
+    // compability of the output.
+    noExternal: [/\.css$/i],
+  }
+
+  return deepMergeOptions(output, input)
+}
+
+function deepMergeOptions(a: Options, b?: Options): Options {
+  return merge({}, a, b)
+}

8cd9b0550068df7850ae15991d884e6e935032ae
ocavue <ocavue@gmail.com>
2025-05-07 15:53:29 +1000

    docs: fix CSS cascade layers

--- website/uno.config.ts
+++ website/uno.config.ts
@@ -10,19 +10,14 @@ import {
 const css = String.raw
 
 const preflight = css`
-  /*
-  1. Prevent padding and border from affecting element width. (https://github.com/mozdevs/cssremedy/issues/4)
-  2. Allow adding a border to an element by just adding a border-width. (https://github.com/tailwindcss/tailwindcss/pull/116)
-  2. [UnoCSS]: allow to override the default border color with css var --un-default-border-color
-  */
-
-  *,
-  ::before,
-  ::after {
-    box-sizing: border-box; /* 1 */
-    border-width: 0; /* 2 */
-    border-style: solid; /* 2 */
-    border-color: var(--un-default-border-color, #e5e7eb); /* 2 */
+  @layer base, starlight, nova;
+  @layer base {
+    *, ::after, ::before, ::backdrop, ::file-selector-button {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      border: 0 solid;
+    }
   }
 `
 

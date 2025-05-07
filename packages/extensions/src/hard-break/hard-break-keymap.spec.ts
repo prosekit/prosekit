9d1d7feda9f693571ce82d0f014423aa0f8944cb
ocavue <ocavue@gmail.com>
2025-05-07 13:17:37 +1000

    add test

--- packages/extensions/src/hard-break/hard-break-keymap.spec.ts
+++ packages/extensions/src/hard-break/hard-break-keymap.spec.ts
@@ -5,7 +5,10 @@ import {
 } from 'vitest'
 
 import { setupTest } from '../testing'
-import { pressKey } from '../testing/keyboard'
+import {
+  inputText,
+  pressKey,
+} from '../testing/keyboard'
 
 describe('defineHardBreakKeymap', () => {
   it('should insert hard break', async () => {
@@ -23,5 +26,20 @@ describe('defineHardBreakKeymap', () => {
     expect(editor.state.doc.toJSON()).toEqual(doc1.toJSON())
     await pressKey('Mod-Enter')
     expect(editor.state.doc.toJSON()).toEqual(doc2.toJSON())
+  })
+
+  it('can continue to type after a hard break', async () => {
+    const { editor, n } = setupTest()
+
+    const doc1 = n.doc(n.p('foo<a>bar'))
+    const doc2 = n.doc(n.p('foo', n.hardBreak(), 'bar'))
+    const doc3 = n.doc(n.p('foo', n.hardBreak(), 'bazbar'))
+
+    editor.set(doc1)
+    expect(editor.state.doc.toJSON()).toEqual(doc1.toJSON())
+    await pressKey('Shift-Enter')
+    expect(editor.state.doc.toJSON()).toEqual(doc2.toJSON())
+    await inputText('baz')
+    expect(editor.state.doc.toJSON()).toEqual(doc3.toJSON())
   })
 })

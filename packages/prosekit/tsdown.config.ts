1225efd320a1cd7d7be3edada58d2d9bb6cf5f80
ocavue <ocavue@gmail.com>
2025-05-10 16:59:13 +1000

    fix: use tsup to fix CSS bundling

--- packages/prosekit/tsdown.config.ts
+++ packages/prosekit/tsdown.config.ts
@@ -1,4 +1,18 @@
 import { config } from '@prosekit/config-tsdown'
 import { defineConfig } from 'tsdown'
 
-export default defineConfig(config())
+const configObject = config()
+
+const entries = configObject.entry
+
+if (typeof entries !== 'object') {
+  throw new TypeError('entries must be an object')
+}
+
+// Skip CSS files until the following issue is resolved:
+// https://github.com/rolldown/tsdown/issues/216
+const entriesWithoutCSS = Object.fromEntries(
+  Object.entries(entries).filter(([, value]) => !value.endsWith('.css')),
+)
+
+export default defineConfig({ ...configObject, entry: entriesWithoutCSS })

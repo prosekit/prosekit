9661739f9d3da066ff257dff78af4a1012018332
ocavue <ocavue@gmail.com>
2025-06-05 00:42:34 +1000

    remove website

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       all: true,
       reporter: ['text-summary', 'text', 'html', 'json', 'json-summary'],
       provider: 'v8',
+      include: ['packages/**'],
     },
     fileParallelism: false,
     projects: ['packages/*'],

c020d9a6403fac923a8a32a2a630dcf6319c41be
ocavue <ocavue@users.noreply.github.com>
2025-06-05 00:43:11 +1000

    test(core): fix mergeObjects type in tests

--- packages/core/src/utils/merge-objects.spec.ts
+++ packages/core/src/utils/merge-objects.spec.ts
@@ -9,14 +9,18 @@ import { mergeObjects } from './merge-objects'
 test('merge simple objects', () => {
   const a = { a: 1 }
   const b = { b: 2 }
-  expect(mergeObjects(a, b)).toEqual({ a: 1, b: 2 })
+  expect(
+    mergeObjects<{ a: number; b: number }>(a, b),
+  ).toEqual({ a: 1, b: 2 })
 })
 
 // undefined values should be removed and overwritten
 test('skip undefined values and override', () => {
   const a = { a: 1, b: undefined as number | undefined }
   const b = { b: 2 }
-  expect(mergeObjects(a, b)).toEqual({ a: 1, b: 2 })
+  expect(
+    mergeObjects<{ a: number; b: number | undefined }>(a, b),
+  ).toEqual({ a: 1, b: 2 })
 })
 
 // null or undefined arguments should be ignored

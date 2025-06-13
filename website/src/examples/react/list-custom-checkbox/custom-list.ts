e9c446001102c2313482eb9793f743b60bfe7e36
ocavue <ocavue@gmail.com>
2025-06-13 21:18:16 +1000

    tweak css

--- website/src/examples/react/list-custom-checkbox/custom-list.ts
+++ website/src/examples/react/list-custom-checkbox/custom-list.ts
@@ -25,7 +25,7 @@ export function getTaskMarkers(node: ProseMirrorNode): DOMOutputSpec[] {
   return [
     [
       'div',
-      { class: 'inline-flex items-center mr-3 box-border relative' },
+      { class: 'inline-flex items-center box-border relative' },
       [
         'label',
         {
@@ -36,14 +36,15 @@ export function getTaskMarkers(node: ProseMirrorNode): DOMOutputSpec[] {
           {
             type: 'checkbox',
             checked: checked ? '' : undefined,
-            class: 'size-5 cursor-pointer transition-all appearance-none rounded-md shadow hover:shadow-md border border-slate-300 checked:bg-red-600 checked:border-red-600 box-border m-0',
+            class:
+              'size-5 cursor-pointer transition-all appearance-none rounded-md shadow-sm hover:shadow-md border border-solid border-gray-300  dark:border-gray-600 checked:bg-red-500 checked:border-red-500 box-border m-0',
           },
         ],
         [
           'span',
           {
             'data-checked': checked ? 'true' : 'false',
-            'class': 'absolute text-white opacity-0 data-[checked=true]:opacity-100  transform-all ',
+            'class': 'absolute text-white opacity-0 data-[checked=true]:opacity-100',
           },
           ['div', { class: 'i-lucide-check size-5' }],
         ],

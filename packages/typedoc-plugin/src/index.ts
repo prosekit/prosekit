90d3f03634aa6c53fa298f210896e45bf0f86644
ocavue <ocavue@gmail.com>
2025-06-16 17:47:12 +1000

    fix

--- packages/typedoc-plugin/src/index.ts
+++ packages/typedoc-plugin/src/index.ts
@@ -1,4 +1,3 @@
-import { ReflectionKind } from 'typedoc'
 import {
   MarkdownPageEvent,
   type MarkdownApplication,
@@ -7,11 +6,8 @@ import {
 import { MyMarkdownTheme } from './markdown-theme'
 
 export function load(app: MarkdownApplication) {
-  console.log('[typedoc-plugin-md-v2] load 5:', app)
-
   app.renderer.on(
     MarkdownPageEvent.BEGIN,
-    /** @param {import('typedoc-plugin-markdown').MarkdownPageEvent} page */
     (page) => {
       /**
        * Update page.frontmatter object to match the requirements of Astro Starlight.
@@ -31,31 +27,6 @@ export function load(app: MarkdownApplication) {
     },
   )
 
-  app.renderer.markdownHooks.on(
-    'page.begin',
-    () => `**Generated using \`page.begin\` hook**`,
-  )
-
-  app.renderer.markdownHooks.on(
-    'page.end',
-    () => `**Generated using \`page.end\` hook**`,
-  )
-
-  app.renderer.markdownHooks.on(
-    'content.begin',
-    () => `**Generated using \`content.begin\` hook**`,
-  )
-
-  app.renderer.markdownHooks.on(
-    'index.page.begin',
-    () => `**Generated using \`index.page.begin\` hook**`,
-  )
-
-  app.renderer.markdownHooks.on(
-    'index.page.end',
-    () => `**Generated using \`index.page.end\` hook**`,
-  )
-
   // Define the markdown theme
-  app.renderer.defineTheme('my-markdown', MyMarkdownTheme)
+  app.renderer.defineTheme('my-markdown-theme', MyMarkdownTheme)
 }

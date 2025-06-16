c9600bfcd26143fc246a5e46361abae75acb12f4
ocavue <ocavue@gmail.com>
2025-06-16 19:48:56 +1000

    fix

--- website/build-typedoc.sh
+++ website/build-typedoc.sh
@@ -28,27 +28,6 @@ if grep -r "__namedParameters" references; then
   exit 1
 fi
 
-# Replace the first h1 line with a frontmatter block. For example:
-# 
-# Before:
-#
-# ```md
-# # foo/bar/baz
-#
-# content
-# ```
-#
-# After:
-#
-# ```md
-# ---
-# title: foo/bar/baz
-# sidebar:
-#   label: baz
-# ---
-#
-# content
-# ```
 cd "$WEBSITE_DIR/src/content/docs"
 find references -name "*.md" | while read file; do
   # If the file only contains one line, remove this file.
@@ -57,26 +36,6 @@ find references -name "*.md" | while read file; do
     rm "$file"
     continue
   fi
-
-  # # Get the title from the first h1 line
-  # title=$(head -n 1 "$file" | sed 's/^# //')
-  # # Get the label from the title
-  # label=$(echo "$title" | tail -n 1 | sed 's/^prosekit\///g')
-
-  # # Create temporary file
-  # temp_file=$(mktemp)
-  
-  # # Write frontmatter and content to temp file
-  # echo "---" > "$temp_file"
-  # echo "title: $title" >> "$temp_file"
-  # echo "sidebar:" >> "$temp_file"
-  # echo "  label: $label" >> "$temp_file"
-  # echo "---" >> "$temp_file"
-  # echo >> "$temp_file"
-  # tail -n +2 "$file" >> "$temp_file"
-  
-  # # Replace original file with temp file
-  # mv "$temp_file" "$file"
 done
 
 

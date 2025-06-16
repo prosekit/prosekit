cb0a9c99634a61aa7809f565b8d6cefe95f7a344
ocavue <ocavue@gmail.com>
2025-06-16 20:00:43 +1000

    remove debug

--- packages/typedoc-plugin/src/markdown-theme-context.ts
+++ packages/typedoc-plugin/src/markdown-theme-context.ts
@@ -6,36 +6,8 @@ export class MyMarkdownThemeContext extends MarkdownThemeContext {
   constructor(...args: ConstructorParameters<typeof MarkdownThemeContext>) {
     super(...args)
 
-    const originalPartials = { ...this.partials }
-
     this.partials.memberContainer = (...args) => {
       return memberContainer.call(this, ...args)
-    }
-
-    this.partials.signatureTitle = (...args) => {
-      const result = originalPartials.signatureTitle(...args)
-      return (
-        `<!-- DEBUG_signatureTitle_start -->`
-        + '\n\n'
-        + '\n\n'
-        + result
-        + '\n\n'
-        + '\n\n'
-        + `<!-- DEBUG_signatureTitle_end -->`
-      )
-    }
-
-    this.partials.memberWithGroups = (...args) => {
-      const result = originalPartials.memberWithGroups(...args)
-      return (
-        `<!-- DEBUG_memberWithGroups_start -->`
-        + '\n\n'
-        + '\n\n'
-        + result
-        + '\n\n'
-        + '\n\n'
-        + `<!-- DEBUG_memberWithGroups_end -->`
-      )
     }
   }
 }

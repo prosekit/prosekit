0c1a4c4b526392add0583a83f00dcd0782c2154a
ocavue <ocavue@users.noreply.github.com>
2025-06-19 07:15:02 +1000

    docs: add more type aliases (#1089)

--- packages/extensions/src/autocomplete/index.ts
+++ packages/extensions/src/autocomplete/index.ts
@@ -2,6 +2,8 @@ export { defineAutocomplete } from './autocomplete'
 export {
   AutocompleteRule,
   type AutocompleteRuleOptions,
+  type CanMatchOptions,
+  type CanMatchPredicate,
   type MatchHandler,
   type MatchHandlerOptions,
 } from './autocomplete-rule'

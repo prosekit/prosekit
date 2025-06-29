611f08599e7a332535e10a0a934fce5e38d788f3
ocavue <ocavue@gmail.com>
2025-06-29 20:55:54 +1000

    fix

--- packages/typedoc-plugin/src/index.ts
+++ packages/typedoc-plugin/src/index.ts
@@ -27,10 +27,38 @@ export function load(app: MarkdownApplication) {
         if (page.filename.endsWith('.css.md')) {
           page.filename = page.filename.replace(/\.css\.md$/, '-css.md')
         }
+
+        page.contents = fixProseMirrorLinks(page.contents)
       }
     },
   )
 
   // Define the markdown theme
   app.renderer.defineTheme('my-markdown-theme', MyMarkdownTheme)
+}
+
+// https://github.com/ProseMirror/prosemirror-model/pull/89
+function fixProseMirrorLinks(content: string) {
+  return content.replaceAll(
+    'https://prosemirror.net/docs/ref/#model.Node%5EtextBetween',
+    'https://prosemirror.net/docs/ref/#model.Node.textBetween',
+  ).replaceAll(
+    'https://prosemirror.net/docs/ref/#model.Node%5EtextContent',
+    'https://prosemirror.net/docs/ref/#model.Node.textContent',
+  ).replaceAll(
+    'https://prosemirror.net/docs/ref/#model.NodeSpec%5EleafText',
+    'https://prosemirror.net/docs/ref/#model.NodeSpec.leafText',
+  ).replaceAll(
+    'https://prosemirror.net/docs/ref/#model.ParseRule.context',
+    'https://prosemirror.net/docs/ref/',
+  ).replaceAll(
+    'https://prosemirror.net/docs/ref/#model.ParseRule.getAttrs',
+    'https://prosemirror.net/docs/ref/',
+  ).replaceAll(
+    'https://prosemirror.net/docs/ref/#model.ParseRule.mark',
+    'https://prosemirror.net/docs/ref/',
+  ).replaceAll(
+    'https://prosemirror.net/docs/ref/#model.ParseRule.priority',
+    'https://prosemirror.net/docs/ref/',
+  )
 }

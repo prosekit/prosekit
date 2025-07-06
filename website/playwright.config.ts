4ee4a058adbbc977b3801d6e9ff09d80df093234
ocavue <ocavue@gmail.com>
2025-07-06 14:54:50 +1000

    fix

--- website/playwright.config.ts
+++ website/playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     baseURL: 'http://localhost:4321/astrobook/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on-first-retry',
     // headless: false,
   },
 

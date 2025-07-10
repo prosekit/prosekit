628040906b61b7c21e1285b25394878aa4a40abd
ocavue <ocavue@gmail.com>
2025-07-10 20:33:10 +1000

    w

--- website/src/shared/common/default-content-full.ts
+++ website/src/shared/common/default-content-full.ts
@@ -1,25 +1,4 @@
 // This is the default content for the full example.
 export const DEFAULT_CONTENT = `
-<ul>
-  <li data-list-kind="task">
-    <input type="checkbox" />
-    <span>AAAAA</span>
-  </li>
-  <li data-list-kind="task" >
-    <input type="checkbox" checked />
-    <span>BBBBB</span>
-  </li> 
-  <li data-list-kind="task">
-    <input type="checkbox" />
-    <span>CCCCC</span>
-  </li>
-  <li data-list-kind="task" data-list-checked>
-    <input type="checkbox" checked />
-    <span>DDDDD</span>
-  </li>
-  <li data-list-kind="task" data-list-checked>
-    <input type="checkbox" />
-    <span>EEEEE</span>
-  </li>
-</ul>
+<div><ul><li class="prosemirror-flat-list" data-list-kind="bullet" data-list-collapsable=""><p>A1</p><ul><li class="prosemirror-flat-list" data-list-kind="bullet" data-list-collapsable=""><p>B1</p><ul><li class="prosemirror-flat-list" data-list-kind="bullet"><p>C1</p></li><li class="prosemirror-flat-list" data-list-kind="bullet"><p>C2</p></li></ul></li><li class="prosemirror-flat-list" data-list-kind="bullet" data-list-collapsable=""><p>B2</p><ul><li class="prosemirror-flat-list" data-list-kind="bullet"><p>C3</p></li></ul></li></ul></li><li class="prosemirror-flat-list" data-list-kind="bullet"><p>A2</p></li><li class="prosemirror-flat-list" data-list-kind="bullet"><p>A3</p></li></ul></div>
 `

88fe7bc77bb1ee96f1c48281a08d60b3c3662391
ocavue <ocavue@gmail.com>
2025-07-12 16:36:51 +0800

    fixup

--- scripts/pr-command.sh
+++ scripts/pr-command.sh
@@ -3,7 +3,8 @@ set -ex
 SLASH_COMMAND=$1
 echo "Running pull request slash command: $SLASH_COMMAND"
 
-cd $(dirname $0)/..
+cd "$(dirname "$0")"
+cd ..
 
 if [ "$SLASH_COMMAND" == "/docs" ]; then
   bash scripts/inspect-docs.sh

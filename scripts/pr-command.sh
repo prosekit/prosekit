e6fd895690331f36b50beb203a3ea378c37cbfcc
ocavue <ocavue@users.noreply.github.com>
2025-07-12 16:42:31 +0800

    ci: add `/docs` pull request slash command (#1131)

--- scripts/pr-command.sh
+++ scripts/pr-command.sh
@@ -3,4 +3,9 @@ set -ex
 SLASH_COMMAND=$1
 echo "Running pull request slash command: $SLASH_COMMAND"
 
-echo "Hello world
+cd "$(dirname "$0")"
+cd ..
+
+if [ "$SLASH_COMMAND" == "/docs" ]; then
+  bash scripts/inspect-docs.sh
+fi

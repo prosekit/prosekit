7b0528d248b4b0a33c04dcea0faa4c138efc31fd
ocavue <ocavue@gmail.com>
2025-07-12 16:18:45 +0800

    add inspect docs.sh

---
+++ scripts/inspect-docs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -ex
+
+REF_DIR="website/src/content/docs/references"
+
+cd "$(dirname "$0")/.."
+DEV_DIR=$(pwd)
+
+# Install pnpm
+npm install -g corepack@latest
+pnpm --version
+
+# Check out the master branch
+cd /tmp
+mkdir -p prosekit-docs-temp
+cd prosekit-docs-temp
+git clone https://github.com/prosekit/prosekit.git
+cd prosekit
+pnpm install
+MASTER_DIR=$(pwd)
+
+# Build the docs from the master branch
+cd "$MASTER_DIR/website"
+pnpm run build:typedoc
+
+# Copy the master docs
+cd "$DEV_DIR"
+mkdir -p "$REF_DIR"
+rm -rf "$REF_DIR"
+cp -r "$MASTER_DIR/$REF_DIR" "$REF_DIR"
+
+# Commit the master docs
+cd "$DEV_DIR"
+git add --force "$REF_DIR"
+git commit --allow-empty -m "chore: inspect docs (steps 1/3)"
+
+# Build the docs from the dev branch
+cd "$DEV_DIR/website"
+pnpm run build:typedoc
+
+# Commit the dev docs
+cd "$DEV_DIR"
+git add --force "$REF_DIR"
+git commit --allow-empty -m "chore: inspect docs (steps 2/3)"
+
+# Remove the docs
+cd "$DEV_DIR"
+mkdir -p "$REF_DIR"
+rm -rf "$REF_DIR"
+
+# Commit the changes
+git add --all
+git commit --allow-empty -m "chore: inspect docs (steps 3/3)"
+
+# Push the changes
+git push
--- scripts/pr-command.sh
+++ scripts/pr-command.sh
@@ -3,4 +3,8 @@ set -ex
 SLASH_COMMAND=$1
 echo "Running pull request slash command: $SLASH_COMMAND"
 
-echo "Hello world
+cd $(dirname $0)/..
+
+if [ "$SLASH_COMMAND" == "/docs" ]; then
+  bash scripts/inspect-docs.sh
+fi

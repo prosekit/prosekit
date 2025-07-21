9bd3361c1e13c0d70d1f825e0025da98602030ff
ocavue <ocavue@gmail.com>
2025-07-21 16:59:02 +0900

    fix

--- packages/extensions/src/drop-indicator/drop-indicator.ts
+++ packages/extensions/src/drop-indicator/drop-indicator.ts
@@ -6,24 +6,7 @@ import {
 import { dropIndicator } from './drop-indicator-view'
 
 export interface DropIndicatorOptions {
-  /**
-   * The color of the cursor.  Use `false` to apply no color and rely only on class.
-   *
-   * @default 'black'
-   */
-  color?: string | false
-
-  /**
-   * The precise width of the cursor in pixels.
-   *
-   * @default 1
-   */
-  width?: number
-
-  /**
-   * A CSS class name to add to the cursor element.
-   */
-  class?: string
+  // TODO: this option is empty for now
 }
 
 /**

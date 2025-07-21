2db32a9a23e4cb550e3e2de0f9bcb854f7591fcc
ocavue <ocavue@gmail.com>
2025-07-21 17:02:56 +0900

    gen

---
+++ packages/web/src/components/drop-indicator/drop-indicator/setup.ts
@@ -0,0 +1,18 @@
+import {
+  useEventListener,
+  type ConnectableElement,
+  type SignalState,
+} from '@aria-ui/core'
+import { insertDefaultBlock } from '@prosekit/core'
+
+import type { BlockHandleDropIndicatorProps } from './types'
+
+/**
+ * @internal
+ */
+export function useBlockHandleDropIndicator(
+  host: ConnectableElement,
+  { state }: { state: SignalState<BlockHandleDropIndicatorProps> },
+): void {
+  // TODO
+}

67bed1bfad4e393a67922d672412f2305e57e14e
ocavue <ocavue@users.noreply.github.com>
2025-08-02 03:09:11 +1000

    chore: improve website build speed (#1144)

---
+++ website/src/components/ui/demo/load-demo-files.ts
@@ -0,0 +1,34 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+import memoize from 'just-memoize'
+
+import { exampleMeta } from '../../../../example.meta'
+
+import { replaceClassNames } from './replace-class-names'
+
+async function loadDemoFileImpl(
+  framework: string,
+  story: string,
+  fileName: string,
+) {
+  const filePath = path.join(process.cwd(), 'src', 'examples', framework, story, fileName)
+  const fileContent = await fs.readFile(filePath, 'utf-8')
+  return ({
+    title: filePath,
+    code: replaceClassNames(fileContent),
+    lang: (fileName.split('.').pop() || 'plaintext') as 'plaintext',
+  })
+}
+
+async function loadDemoFilesImpl(
+  { framework, story }: { framework: string; story: string },
+) {
+  const example = exampleMeta.examples.find(example => example.story === story && example.framework === framework)
+  if (!example) {
+    throw new Error(`Unable to find example for story ${story} and framework ${framework}`)
+  }
+  return await Promise.all(example.files.map(file => loadDemoFileImpl(framework, story, file.path)))
+}
+
+export const loadDemoFiles = memoize(loadDemoFilesImpl)

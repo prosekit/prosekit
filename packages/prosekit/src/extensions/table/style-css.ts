ec4f690c6917179c8d5b5304a680ed3726159196
ocavue <ocavue@users.noreply.github.com>
2025-08-02 03:09:28 +1000

    docs: add reference docs for all CSS files (#1143)

---
+++ packages/prosekit/src/extensions/table/style-css.ts
@@ -0,0 +1,11 @@
+/**
+ * Some basic styles for the table extension.
+ *
+ * This file is already included in [`prosekit/basic/style.css`](/references/basic/stylecss/).
+ *
+ * [view source code](https://unpkg.com/prosekit/extensions/table/style.css)
+ *
+ * @module prosekit/extensions/table/style.css
+ */
+
+export {}

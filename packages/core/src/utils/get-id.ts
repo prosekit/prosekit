3bd27a7ac4b09b213abceebbd3067e85d4838a5d
ocavue <ocavue@gmail.com>
2025-08-04 14:57:32 +1000

    update

--- packages/core/src/utils/get-id.ts
+++ packages/core/src/utils/get-id.ts
@@ -4,6 +4,8 @@ let id = 0
  * Returns a unique id in the current process that can be used in various places.
  *
  * @internal
+ *
+ * @deprecated Import `getId` from `@ocavue/utils` package instead. Remove it in a future version.
  */
 export function getId(): string {
   id = (id + 1) % Number.MAX_SAFE_INTEGER

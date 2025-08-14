ccc83d688dfe98814b8d5cf4de42f02e569d89ae
ocavue <ocavue@gmail.com>
2025-08-15 00:21:22 +1000

    fix tst

--- packages/extensions/src/paste-rule/mark-paste-rule.spec.ts
+++ packages/extensions/src/paste-rule/mark-paste-rule.spec.ts
@@ -18,27 +18,29 @@ import {
 } from '../testing/clipboard'
 import { defineText } from '../text'
 
+import type { MarkPasteRuleOptions } from './mark-paste-rule'
 import { defineMarkPasteRule } from './mark-paste-rule'
 
-// Simple test mark
-function defineTestMark() {
-  return defineMarkSpec({
-    name: 'testMark',
-    attrs: {
-      value: { validate: 'string' },
-    },
-    parseDOM: [{ tag: 'span[data-test]' }],
-    toDOM: () => ['span', { 'data-test': 'true' }, 0],
-  })
-}
-
-function setup() {
+function setup(options?: Partial<MarkPasteRuleOptions>) {
   return setupTestFromExtension(union(
     defineDoc(),
     defineText(),
     defineParagraph(),
     defineBold(),
-    defineTestMark(),
+    defineMarkSpec({
+      name: 'testMark',
+      attrs: {
+        value: { validate: 'string' },
+      },
+      parseDOM: [{ tag: 'span[data-test]' }],
+      toDOM: () => ['span', { 'data-test': 'true' }, 0],
+    }),
+    defineMarkPasteRule({
+      type: 'testMark',
+      regex: /@(\w+)/g,
+      getAttrs: (match: RegExpExecArray) => ({ value: match[1] }),
+      ...options,
+    }),
   ))
 }
 
@@ -46,12 +48,6 @@ describe('defineMarkPasteRule', () => {
   it('should match simple patterns and create marks', () => {
     const { editor, n, m } = setup()
     editor.set(n.doc(n.paragraph('<a>')))
-
-    editor.use(defineMarkPasteRule({
-      type: 'testMark',
-      regex: /@(\w+)/g,
-      getAttrs: (match: RegExpExecArray) => ({ value: match[1] }),
-    }))
 
     pasteHTML(editor.view, '<p>Hello @alice and @bob</p>')
 
@@ -77,37 +73,30 @@ describe('defineMarkPasteRule', () => {
   })
 
   it('should skip processing when attrs returns false', () => {
-    const { editor, n, m } = setup()
+    const { editor, n, m } = setup({
+      getAttrs: (match: RegExpExecArray) => {
+        return match[1] === 'skip' ? false : { value: match[1] }
+      },
+    })
     editor.set(n.doc(n.paragraph('<a>')))
 
-    editor.use(defineMarkPasteRule({
-      type: 'testMark',
-      regex: /#(\w+)/g,
-      getAttrs: (match: RegExpExecArray) => match[1] === 'skip' ? false : { value: match[1] },
-    }))
-
-    pasteHTML(editor.view, '<p>Tags: #good #skip #also</p>')
+    pasteHTML(editor.view, '<p>Tags: @good @skip @also</p>')
 
     expect(editor.view.state.doc.toJSON()).toEqual(
       n.doc(n.paragraph(
         'Tags: ',
-        m.testMark({ value: 'good' }, '#good'),
-        ' #skip ',
-        m.testMark({ value: 'also' }, '#also'),
+        m.testMark({ value: 'good' }, '@good'),
+        ' @skip ',
+        m.testMark({ value: 'also' }, '@also'),
       )).toJSON(),
     )
   })
 
   it('should use shouldSkip to skip bold text', () => {
-    const { editor, n, m } = setup()
-    editor.set(n.doc(n.paragraph('<a>')))
-
-    editor.use(defineMarkPasteRule({
-      type: 'testMark',
-      regex: /@(\w+)/g,
-      getAttrs: (match: RegExpExecArray) => ({ value: match[1] }),
+    const { editor, n, m } = setup({
       shouldSkip: (node) => node.marks.some(mark => mark.type.name === 'bold'),
-    }))
+    })
+    editor.set(n.doc(n.paragraph('<a>')))
 
     pasteHTML(editor.view, '<p>Hello <strong>@alice</strong> and @bob</p>')
 

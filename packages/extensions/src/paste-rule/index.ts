6dc3a06a5559d6691bb291aea37170f09b1dce11
ocavue <ocavue@gmail.com>
2025-08-14 22:02:18 +1000

    format

--- packages/extensions/src/paste-rule/index.ts
+++ packages/extensions/src/paste-rule/index.ts
@@ -1,8 +1,10 @@
 export {
+  defineMarkPasteRule,
+  type MarkPasteRuleOptions,
+} from './mark-paste-rule'
+export {
   definePasteRule,
   type PasteRuleHandler,
   type PasteRuleHandlerOptions,
   type PasteRuleOptions,
 } from './paste-rule'
-export { defineMarkPasteRule } from './mark-paste-rule'
-export type { MarkPasteRuleOptions } from './mark-paste-rule'

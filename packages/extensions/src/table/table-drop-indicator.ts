3864f71fcbf436412bf3cfced32a6a304a47fb29
ocavue <ocavue@gmail.com>
2025-08-17 05:11:29 +1000

    performance

--- packages/extensions/src/table/table-drop-indicator.ts
+++ packages/extensions/src/table/table-drop-indicator.ts
@@ -15,9 +15,26 @@ export function defineTableDropIndicator(): PlainExtension {
   })
 }
 
+const matchMap = new WeakMap<DataTransfer, boolean>()
+
 const onDrag: DragEventHandler = ({ event }): boolean => {
+  const dataTransfer = event.dataTransfer
+  if (!dataTransfer) return true
+
+  let match: boolean
+
+  if (matchMap.has(dataTransfer)) {
+    match = matchMap.get(dataTransfer)!
+  } else {
+    // On Safari, accessing `dataTransfer.types` is more than 10x slower than in
+    // Chrome. This becomes a bottleneck when `onDrag` is called frequently, so
+    // we cache the result in a WeakMap.
+    const types = dataTransfer.types
+    match = types.includes('application/x-prosekit-table-handle-drag')
+    matchMap.set(dataTransfer, match)
+  }
+
   // Don't show the drop indicator when the drag event has
   // "application/x-prosekit-table-handle-drag" type.
-  const types = event.dataTransfer?.types ?? []
-  return !types.includes('application/x-prosekit-table-handle-drag')
+  return !match
 }

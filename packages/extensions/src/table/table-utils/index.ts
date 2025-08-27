436ed55d663dfc7d2af2560bafc7df7cdd1c2b85
ocavue <ocavue@gmail.com>
2025-08-28 00:12:19 +1000

    move

--- packages/extensions/src/table/table-utils/index.ts
+++ packages/extensions/src/table/table-utils/index.ts
@@ -1,1 +1,16 @@
-export * from './query'
+import { CellSelection } from 'prosemirror-tables'
+
+export {
+  findCellPos,
+  findCellRange,
+  findTable,
+} from 'prosemirror-tables'
+
+/**
+ * Checks if the given object is a `CellSelection` instance.
+ *
+ * @public
+ */
+export function isCellSelection(value: unknown): value is CellSelection {
+  return value instanceof CellSelection
+}

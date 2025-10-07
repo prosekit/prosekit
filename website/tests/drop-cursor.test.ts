bd7021191f8bf0bd56af354027a1ff311fa718f5
ocavue <ocavue@gmail.com>
2025-10-07 23:38:44 +1100

    fix test

--- website/tests/drop-cursor.test.ts
+++ website/tests/drop-cursor.test.ts
@@ -17,6 +17,9 @@ testStory('drop-cursor', () => {
 
     const before = await images.evaluateAll((els) => els.map((e) => e.getAttribute('src') || ''))
 
+    // Ensure all images are unique
+    expect(new Set(before).size).toBe(before.length)
+
     // Drag first image to just before the third image (between the 2nd and 3rd)
     await images.nth(0).dragTo(images.nth(2), { targetPosition: { x: 5, y: 5 } })
 

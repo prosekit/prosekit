88185cfde2ade37585c78a3eb60e16d0501a847a
ocavue <ocavue@users.noreply.github.com>
2025-10-10 11:06:39 +1100

    docs: fix svelte example button import (#1248)

--- website/src/examples/svelte/list/toolbar.svelte
+++ website/src/examples/svelte/list/toolbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { useEditor } from 'prosekit/svelte'
-import Button from '../../../shared/svelte/button.svelte'
+import Button from './button.svelte'
 import type { EditorExtension } from './extension'
 
 const editor = useEditor<EditorExtension>({ update: true })

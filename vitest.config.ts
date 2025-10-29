9541c9c5a7dee2e6b2721dbabe61c7b5b7282aff
ocavue <ocavue@gmail.com>
2025-10-29 12:16:51 +1100

    update slowTestThreshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    slowTestThreshold: 2000,
     reporters: ['tree'],
     coverage: {
       enabled: false,

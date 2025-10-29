522ab7913b8047c968c4a4492cb3e1f095abb96a
ocavue <ocavue@users.noreply.github.com>
2025-10-29 17:10:07 +1100

    chore: move prosemirror-drop-indicator to a separate repo (#1269)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,13 @@ export default defineConfig({
   test: {
     slowTestThreshold: 2000,
     reporters: ['verbose'],
+    retry: process.env.CI ? 2 : 0,
     coverage: {
       enabled: false,
       reporter: ['text-summary', 'text', 'html', 'json', 'json-summary'],
       provider: 'v8',
       include: ['packages/**/src/**'],
+      exclude: ['packages/dev/**/*', 'packages/config-*/**/*'],
     },
     fileParallelism: false,
     projects: ['./packages/*', './website'],

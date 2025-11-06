5a035da83a1c99e11fd0bca12f5c880a4a1851ca
ocavue <ocavue@gmail.com>
2025-11-06 13:28:03 +1100

    fix

--- packages/core/src/extensions/plugin.spec.ts
+++ packages/core/src/extensions/plugin.spec.ts
@@ -23,16 +23,18 @@ import { definePlugin } from './plugin'
 
 describe('plugin', () => {
   it('maintains plugin order in state based on priority', () => {
-    const plugin1 = new Plugin({ key: new PluginKey('plugin-key-1') })
-    const plugin2 = new Plugin({ key: new PluginKey('plugin-key-2') })
-    const plugin3 = new Plugin({ key: new PluginKey('plugin-key-3') })
-    const plugin4 = new Plugin({ key: new PluginKey('plugin-key-4') })
-    const plugin5 = new Plugin({ key: new PluginKey('plugin-key-5') })
+    const plugins = {
+      a: new Plugin({ key: new PluginKey('a') }),
+      b: new Plugin({ key: new PluginKey('b') }),
+      c: new Plugin({ key: new PluginKey('c') }),
+      d: new Plugin({ key: new PluginKey('d') }),
+      e: new Plugin({ key: new PluginKey('e') }),
+    }
 
-    const extension1 = definePlugin(() => [plugin1, plugin5]) // default priority
-    const extension2 = withPriority(definePlugin(plugin2), Priority.highest)
-    const extension3 = withPriority(definePlugin([plugin3]), Priority.lowest)
-    const extension4 = definePlugin(plugin4) // default priority
+    const extension1 = definePlugin(() => [plugins.a, plugins.d]) // default priority
+    const extension2 = withPriority(definePlugin(plugins.b), Priority.highest)
+    const extension3 = withPriority(definePlugin([plugins.c]), Priority.lowest)
+    const extension4 = definePlugin(plugins.e) // default priority
 
     const { editor } = setupTestFromExtension(union(
       defineDoc(),
@@ -44,12 +46,12 @@ describe('plugin', () => {
       extension4,
     ))
 
-    const pluginKeys = editor.state.plugins.map((plugin): string | undefined => {
-      if (plugin === plugin1) return 'plugin-key-1'
-      if (plugin === plugin2) return 'plugin-key-2'
-      if (plugin === plugin3) return 'plugin-key-3'
-      if (plugin === plugin4) return 'plugin-key-4'
-      if (plugin === plugin5) return 'plugin-key-5'
+    const pluginKeys = editor.state.plugins.map((plugin) => {
+      for (const [key, value] of Object.entries(plugins)) {
+        if (plugin === value) {
+          return key
+        }
+      }
       return undefined
     }).filter(Boolean)
 
@@ -57,37 +59,59 @@ describe('plugin', () => {
     // first in state. The plugins with the same priority should be listed in
     // the order of the extensions.
     expect(pluginKeys).toEqual([
-      'plugin-key-2',
-      'plugin-key-1',
-      'plugin-key-5',
-      'plugin-key-4',
-      'plugin-key-3',
+      // Highest priority
+      'b',
+
+      // Default priority, added in the last extension
+      'e',
+
+      // Default priority, added as the second plugin in the first extension
+      'd',
+
+      // Default priority, added as the first plugin in the first extension
+      'a',
+
+      // Lowest priority
+      'c',
     ])
   })
 
   it('calls handlers in priority order with highest priority first', () => {
     const callOrder: string[] = []
 
-    const handleKeyDown1 = vi.fn(() => {
-      callOrder.push('default')
+    const handleKeyDownA = vi.fn(() => {
+      callOrder.push('a')
       return false
     })
-    const handleKeyDown2 = vi.fn(() => {
-      callOrder.push('highest')
+    const handleKeyDownB = vi.fn(() => {
+      callOrder.push('b')
       return false
     })
-    const handleKeyDown3 = vi.fn(() => {
-      callOrder.push('lowest')
+    const handleKeyDownC = vi.fn(() => {
+      callOrder.push('c')
+      return false
+    })
+    const handleKeyDownD = vi.fn(() => {
+      callOrder.push('d')
+      return false
+    })
+    const handleKeyDownE = vi.fn(() => {
+      callOrder.push('e')
       return false
     })
 
-    const plugin1 = new Plugin({ props: { handleKeyDown: handleKeyDown1 } })
-    const plugin2 = new Plugin({ props: { handleKeyDown: handleKeyDown2 } })
-    const plugin3 = new Plugin({ props: { handleKeyDown: handleKeyDown3 } })
+    const plugins = {
+      a: new Plugin({ props: { handleKeyDown: handleKeyDownA } }),
+      b: new Plugin({ props: { handleKeyDown: handleKeyDownB } }),
+      c: new Plugin({ props: { handleKeyDown: handleKeyDownC } }),
+      d: new Plugin({ props: { handleKeyDown: handleKeyDownD } }),
+      e: new Plugin({ props: { handleKeyDown: handleKeyDownE } }),
+    }
 
-    const extension1 = definePlugin(plugin1) // default priority
-    const extension2 = withPriority(definePlugin(plugin2), Priority.highest)
-    const extension3 = withPriority(definePlugin(plugin3), Priority.lowest)
+    const extension1 = definePlugin(() => [plugins.a, plugins.d]) // default priority
+    const extension2 = withPriority(definePlugin(plugins.b), Priority.highest)
+    const extension3 = withPriority(definePlugin([plugins.c]), Priority.lowest)
+    const extension4 = definePlugin(plugins.e) // default priority
 
     const { editor } = setupTestFromExtension(union(
       defineDoc(),
@@ -96,20 +120,34 @@ describe('plugin', () => {
       extension1,
       extension2,
       extension3,
+      extension4,
     ))
 
     editor.view.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp' }))
 
     // All handlers should be called since none returns true
-    expect(handleKeyDown1).toHaveBeenCalledTimes(1)
-    expect(handleKeyDown2).toHaveBeenCalledTimes(1)
-    expect(handleKeyDown3).toHaveBeenCalledTimes(1)
+    expect(handleKeyDownA).toHaveBeenCalledTimes(1)
+    expect(handleKeyDownB).toHaveBeenCalledTimes(1)
+    expect(handleKeyDownC).toHaveBeenCalledTimes(1)
+    expect(handleKeyDownD).toHaveBeenCalledTimes(1)
+    expect(handleKeyDownE).toHaveBeenCalledTimes(1)
 
     // The event handlers of the plugins with the highest priority should be called first
     expect(callOrder).toEqual([
-      'highest',
-      'default',
-      'lowest',
+      // Highest priority
+      'b',
+
+      // Default priority, added in the last extension
+      'e',
+
+      // Default priority, added as the second plugin in the first extension
+      'd',
+
+      // Default priority, added as the first plugin in the first extension
+      'a',
+
+      // Lowest priority
+      'c',
     ])
   })
 })

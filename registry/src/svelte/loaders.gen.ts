e0515b19c91cb05c149726c6934fd1b876b83afe
ocavue <ocavue@gmail.com>
2025-11-10 18:44:39 +1100

    feat(registry): add Svelte example for bold

--- registry/src/svelte/loaders.gen.ts
+++ registry/src/svelte/loaders.gen.ts
@@ -1,6 +1,7 @@
 // This file is generated from update-loader.ts
 
 export const loaders = {
+  'bold': () => import('./examples/bold').then((m) => ({ default: m.ExampleEditor })),
   'full': () => import('./examples/full').then((m) => ({ default: m.ExampleEditor })),
   'minimal': () => import('./examples/minimal').then((m) => ({ default: m.ExampleEditor })),
 }
--- website/src/stories/svelte.stories.ts
+++ website/src/stories/svelte.stories.ts
@@ -3,5 +3,6 @@ import component from './svelte.astro'
 
 export default { component }
 
+export const Bold = { args: { story: 'bold' } }
 export const Full = { args: { story: 'full' } }
 export const Minimal = { args: { story: 'minimal' } }

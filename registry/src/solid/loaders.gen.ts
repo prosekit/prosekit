3c33575c5a92cd81cd473ba9653f391e2c8ffa31
ocavue <ocavue@gmail.com>
2025-11-11 02:19:56 +1100

    feat(registry): add solid bold example

--- registry/src/solid/loaders.gen.ts
+++ registry/src/solid/loaders.gen.ts
@@ -4,6 +4,7 @@ import { lazy } from 'solid-js'
 export const loaders = {
   'block-handle': lazy(() => import('./examples/block-handle').then((m) => ({ default: m.ExampleEditor }))),
   'blockquote': lazy(() => import('./examples/blockquote').then((m) => ({ default: m.ExampleEditor }))),
+  'bold': lazy(() => import('./examples/bold').then((m) => ({ default: m.ExampleEditor }))),
   'full': lazy(() => import('./examples/full').then((m) => ({ default: m.ExampleEditor }))),
   'heading': lazy(() => import('./examples/heading').then((m) => ({ default: m.ExampleEditor }))),
   'minimal': lazy(() => import('./examples/minimal').then((m) => ({ default: m.ExampleEditor }))),
--- website/src/stories/solid.stories.ts
+++ website/src/stories/solid.stories.ts
@@ -5,6 +5,7 @@ export default { component }
 
 export const BlockHandle = { args: { story: 'block-handle' } }
 export const Blockquote = { args: { story: 'blockquote' } }
+export const Bold = { args: { story: 'bold' } }
 export const Full = { args: { story: 'full' } }
 export const Heading = { args: { story: 'heading' } }
 export const Minimal = { args: { story: 'minimal' } }

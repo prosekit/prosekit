0f49889622a3624667f4e2d18f035137a265eef0
ocavue <ocavue@gmail.com>
2025-11-10 18:49:31 +1100

    code block

--- registry/src/svelte/loaders.gen.ts
+++ registry/src/svelte/loaders.gen.ts
@@ -2,6 +2,7 @@
 
 export const loaders = {
   'bold': () => import('./examples/bold').then((m) => ({ default: m.ExampleEditor })),
+  'code-block': () => import('./examples/code-block').then((m) => ({ default: m.ExampleEditor })),
   'full': () => import('./examples/full').then((m) => ({ default: m.ExampleEditor })),
   'minimal': () => import('./examples/minimal').then((m) => ({ default: m.ExampleEditor })),
 }
--- website/src/stories/svelte.stories.ts
+++ website/src/stories/svelte.stories.ts
@@ -4,5 +4,6 @@ import component from './svelte.astro'
 export default { component }
 
 export const Bold = { args: { story: 'bold' } }
+export const CodeBlock = { args: { story: 'code-block' } }
 export const Full = { args: { story: 'full' } }
 export const Minimal = { args: { story: 'minimal' } }

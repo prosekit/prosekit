cf93657052c942b4dd33c66f99c366d8a2c58a41
ocavue <ocavue@gmail.com>
2025-11-11 00:56:15 +1100

    improve test

--- registry/test/helpers/expect.ts
+++ registry/test/helpers/expect.ts
@@ -13,10 +13,14 @@ export async function expectLocatorToNotExist(locator: Locator, options?: Expect
 }
 
 export async function expectLocatorToBeHidden(locator: Locator, options?: ExpectPollOptions): Promise<void> {
+  const message: string = [
+    options?.message || '',
+    `Expect locator '${locator.selector}' to be hidden, but found at least one visible element`,
+  ].filter(Boolean).join(': ')
+
   await expect.poll(() => {
-    const elements = locator.elements()
-    return elements.some((element) => isElementVisible(element))
-  }, options).toBe(false)
+    return findVisibleElement(locator)
+  }, { ...options, message }).toBe(undefined)
 }
 
 function isElementVisible(element: Element): boolean {
@@ -25,12 +29,20 @@ function isElementVisible(element: Element): boolean {
     return false
   }
 
-  if (element instanceof HTMLElement) {
-    const style = window.getComputedStyle(element)
-    if (style.visibility === 'hidden' || style.display === 'none' || Number(style.opacity) === 0) {
-      return false
-    }
+  const style = window.getComputedStyle(element)
+  if (style.visibility === 'hidden' || style.display === 'none' || Number(style.opacity) === 0) {
+    return false
   }
 
   return true
+}
+
+function findVisibleElement(locator: Locator): Element | undefined {
+  const elements = locator.elements()
+  for (const element of elements) {
+    if (isElementVisible(element)) {
+      return element
+    }
+  }
+  return undefined
 }

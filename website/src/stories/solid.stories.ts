d6ef94100067b8916ba0af1d446c093edcb3141b
ocavue <ocavue@gmail.com>
2025-11-11 02:13:25 +1100

    feat(registry): add solid blockquote example

--- website/src/stories/solid.stories.ts
+++ website/src/stories/solid.stories.ts
@@ -4,6 +4,7 @@ import component from './solid.astro'
 export default { component }
 
 export const BlockHandle = { args: { story: 'block-handle' } }
+export const Blockquote = { args: { story: 'blockquote' } }
 export const Full = { args: { story: 'full' } }
 export const Heading = { args: { story: 'heading' } }
 export const Minimal = { args: { story: 'minimal' } }

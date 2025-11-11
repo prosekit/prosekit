dd24f7bec8ff50bcb5048b9aca8f65f26168e764
ocavue <ocavue@gmail.com>
2025-11-11 18:52:18 +1100

    feat(registry): add solid emoji-rules example

--- registry/src/solid/loaders.gen.ts
+++ registry/src/solid/loaders.gen.ts
@@ -10,6 +10,7 @@ export const loaders = {
   'code-block': lazy(() => import('./examples/code-block').then((m) => ({ default: m.ExampleEditor }))),
   'code-block-themes': lazy(() => import('./examples/code-block-themes').then((m) => ({ default: m.ExampleEditor }))),
   'drop-cursor': lazy(() => import('./examples/drop-cursor').then((m) => ({ default: m.ExampleEditor }))),
+  'emoji-rules': lazy(() => import('./examples/emoji-rules').then((m) => ({ default: m.ExampleEditor }))),
   'full': lazy(() => import('./examples/full').then((m) => ({ default: m.ExampleEditor }))),
   'heading': lazy(() => import('./examples/heading').then((m) => ({ default: m.ExampleEditor }))),
   'minimal': lazy(() => import('./examples/minimal').then((m) => ({ default: m.ExampleEditor }))),
--- website/src/stories/solid.stories.ts
+++ website/src/stories/solid.stories.ts
@@ -11,6 +11,7 @@ export const Code = { args: { story: 'code' } }
 export const CodeBlock = { args: { story: 'code-block' } }
 export const CodeBlockThemes = { args: { story: 'code-block-themes' } }
 export const DropCursor = { args: { story: 'drop-cursor' } }
+export const EmojiRules = { args: { story: 'emoji-rules' } }
 export const Full = { args: { story: 'full' } }
 export const Heading = { args: { story: 'heading' } }
 export const Minimal = { args: { story: 'minimal' } }

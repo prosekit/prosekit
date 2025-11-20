9e6337209536b65c662e05142bccb087eab95307
ocavue <ocavue@gmail.com>
2025-11-21 01:12:30 +1100

    update comment

--- packages/web/src/hooks/use-editor-focused.ts
+++ packages/web/src/hooks/use-editor-focused.ts
@@ -26,10 +26,12 @@ export function useEditorFocused(
   const extension = defineFocusChangeHandler((value: boolean) => focused.set(value))
   useEditorExtension(host, editor, extension)
 
-  // When the editor is focused, its selection might change, which would cause
-  // some other changes to the editor like decorations to be updated. To avoid
-  // flickering when the editor is focused, we delay the focus state for a short
-  // period of time before it is set to true.
+  // When the editor DOM element is focused, its DOM selection might also get
+  // updated. However, ProseMirror's selection is updated after a 20ms delay
+  // (see https://github.com/ProseMirror/prosemirror-view/blob/1.41.3/src/input.ts#L773-L776).
+  //
+  // To avoid flickering, we delay the focus state for a short period of time
+  // before it is set to true.
   useEffect(host, () => {
     if (focused.get()) {
       const id = setTimeout(() => {

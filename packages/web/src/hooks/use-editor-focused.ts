631c1747b023c00e16f136d1a1441a7c1b69c422
ocavue <ocavue@gmail.com>
2025-11-21 00:53:23 +1100

    add a delay

--- packages/web/src/hooks/use-editor-focused.ts
+++ packages/web/src/hooks/use-editor-focused.ts
@@ -1,7 +1,9 @@
 import {
   createSignal,
+  useEffect,
   type ConnectableElement,
   type ReadonlySignal,
+  type Signal,
 } from '@aria-ui/core'
 import {
   defineFocusChangeHandler,
@@ -17,8 +19,27 @@ export function useEditorFocused(
   host: ConnectableElement,
   editor: ReadonlySignal<Editor | null>,
 ): ReadonlySignal<boolean> {
-  const focused = createSignal(editor.get()?.focused ?? false)
+  const initialFocused: boolean = editor.get()?.focused ?? false
+  const focused: Signal<boolean> = createSignal(initialFocused)
   const extension = defineFocusChangeHandler((value: boolean) => focused.set(value))
   useEditorExtension(host, editor, extension)
-  return focused
+
+  const delayFocused: Signal<boolean> = createSignal(false)
+
+  // When the editor is focused, its selection might change, which would cause
+  // some other changes to the editor like decorations to be updated. To avoid
+  // flickering when the editor is focused, we delay the focus state for a short
+  // period of time before it is set to true.
+  useEffect(host, () => {
+    if (focused.get()) {
+      const id = setTimeout(() => {
+        delayFocused.set(true)
+      }, 50)
+      return () => clearTimeout(id)
+    } else {
+      delayFocused.set(false)
+    }
+  })
+
+  return delayFocused
 }

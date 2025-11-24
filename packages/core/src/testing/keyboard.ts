9547da9d3a34f4459007140c6476e74701a285fa
ocavue <ocavue@gmail.com>
2025-11-24 19:43:15 +1100

    update test

--- packages/core/src/testing/keyboard.ts
+++ packages/core/src/testing/keyboard.ts
@@ -1,7 +1,5 @@
 import { userEvent } from 'vitest/browser'
 
-import { isApple } from '@prosekit/core'
-
 export async function inputText(input: string) {
   return await userEvent.keyboard(input)
 }

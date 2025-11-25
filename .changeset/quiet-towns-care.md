cbacb9419e2c94ae8245e0c5ed265dadf82001ad
ocavue <ocavue@gmail.com>
2025-11-25 18:19:10 +1100

    update loro-prosemirror

--- .changeset/quiet-towns-care.md
+++ .changeset/quiet-towns-care.md
@@ -3,4 +3,4 @@
 "@prosekit/extensions": minor
 ---
 
-Add a new option `presence` to `defineLoro()`, which is a `CursorEphemeralStore` instance. `CursorEphemeralStore` is a better alternative to the legacy `Awareness`. If you're using `loro-prosemirror`, the minimal required version of `loro-prosemirror` is now `0.4.0`.
+Add a new option `presence` to `defineLoro()`, which is a `CursorEphemeralStore` instance. `CursorEphemeralStore` is a better alternative to the legacy `Awareness`. If you're using `loro-prosemirror`, the minimal required version of `loro-prosemirror` is now `0.4.1`.
--- packages/extensions/src/loro/loro-cursor-plugin.ts
+++ packages/extensions/src/loro/loro-cursor-plugin.ts
@@ -2,26 +2,16 @@ import {
   definePlugin,
   type PlainExtension,
 } from '@prosekit/core'
+import type { Plugin } from '@prosekit/pm/state'
 import type {
-  EditorState,
-  Plugin,
-  Selection,
-} from '@prosekit/pm/state'
-import type { DecorationAttrs } from '@prosekit/pm/view'
-import type { PeerID } from 'loro-crdt'
-import type { CursorEphemeralStore } from 'loro-prosemirror'
+  CursorEphemeralStore,
+  CursorPluginOptions,
+} from 'loro-prosemirror'
 import {
   LoroCursorPlugin,
   LoroEphemeralCursorPlugin,
   type CursorAwareness,
 } from 'loro-prosemirror'
-
-// TODO: Re-use it from loro-prosemirror
-export interface CursorPluginOptions {
-  getSelection?: (state: EditorState) => Selection
-  createCursor?: (user: PeerID) => Element
-  createSelection?: (user: PeerID) => DecorationAttrs
-}
 
 export interface LoroCursorOptions extends CursorPluginOptions {
   awareness?: CursorAwareness

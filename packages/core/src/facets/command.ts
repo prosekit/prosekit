7583b191f11a1af0bbffc60ccc798eac22ac69b5
ocavue <ocavue@gmail.com>
2025-11-26 02:24:53 +1100

    small refactor

--- packages/core/src/facets/command.ts
+++ packages/core/src/facets/command.ts
@@ -13,8 +13,14 @@ type CommandPayload = CommandCreators
 
 export const commandFacet: Facet<CommandPayload, RootPayload> = defineFacet({
   reducer: (inputs) => {
-    const commands = Object.assign({}, ...inputs) as CommandPayload
-    return { commands }
+    switch (inputs.length) {
+      case 0:
+        return { commands: {} }
+      case 1:
+        return { commands: inputs[0] }
+      default:
+        return { commands: Object.assign({}, ...inputs) as CommandCreators }
+    }
   },
   parent: rootFacet,
   singleton: true,

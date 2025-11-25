0ed291c8e148297edee3207ef83db219958c1325
ocavue <ocavue@gmail.com>
2025-11-26 02:19:48 +1100

    re-use isDeepEqual

--- packages/core/src/utils/is-payload-equal.spec.ts
+++ packages/core/src/utils/is-payload-equal.spec.ts
@@ -4,23 +4,23 @@ import {
   test,
 } from 'vitest'
 
-import { deepEquals } from './deep-equals'
+import { isPayloadEqual } from './is-payload-equal'
 
 test('arrays with equal values are equal', () => {
-  expect(deepEquals([1, 2], [1, 2])).toBe(true)
-  expect(deepEquals([1, 2], [2, 1])).toBe(false)
+  expect(isPayloadEqual([1, 2], [1, 2])).toBe(true)
+  expect(isPayloadEqual([1, 2], [2, 1])).toBe(false)
 })
 
 test('nested objects', () => {
   const a = { a: 1, b: { c: 3 } }
   const b = { a: 1, b: { c: 3 } }
   const c = { a: 1, b: { c: 4 } }
-  expect(deepEquals(a, b)).toBe(true)
-  expect(deepEquals(a, c)).toBe(false)
+  expect(isPayloadEqual(a, b)).toBe(true)
+  expect(isPayloadEqual(a, c)).toBe(false)
 })
 
 test('ordered maps', () => {
   const map1 = OrderedMap.from({ a: 1, b: 2 })
-  const map2 = OrderedMap.from({ a: 1, b: 2 })
-  expect(deepEquals(map1, map2)).toBe(true)
+  const map2 = OrderedMap.from({ b: 2, a: 1 })
+  expect(isPayloadEqual(map1, map2)).toBe(true)
 })

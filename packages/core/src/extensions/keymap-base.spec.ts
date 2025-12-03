4c418d27c6d5c549be07b5d97aff203f2b59fcbc
ocavue <ocavue@users.noreply.github.com>
2025-12-04 01:49:40 +1100

    test: add more unit tests for Mod-a keybinding (#1362)

--- packages/core/src/extensions/keymap-base.spec.ts
+++ packages/core/src/extensions/keymap-base.spec.ts
@@ -13,6 +13,7 @@ import {
   defineText,
   setupTestFromExtension,
 } from '../testing'
+import type { SelectionJSON } from '../types/model'
 
 import { defineBaseKeymap } from './keymap-base'
 
@@ -27,12 +28,12 @@ describe('Mod-a', () => {
 
     editor.set(n.doc(n.paragraph('Fo<a>o<b>'), n.paragraph('Bar')))
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo")>"`)
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo"), paragraph("Bar")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
   })
 
-  it('can select the entire document if the block is already selected', async () => {
+  it('can select the entire document if the current textblock is already selected', async () => {
     const { editor, n } = setupTestFromExtension(union(
       defineDoc(),
       defineText(),
@@ -41,8 +42,37 @@ describe('Mod-a', () => {
     ))
 
     editor.set(n.doc(n.paragraph('<a>Foo<b>'), n.paragraph('Bar')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo")>"`)
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo"), paragraph("Bar")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
+  })
+
+  it('can select the entire document if multiple textblocks are already selected', async () => {
+    const { editor, n } = setupTestFromExtension(union(
+      defineDoc(),
+      defineText(),
+      defineParagraph(),
+      defineBaseKeymap(),
+    ))
+
+    editor.set(n.doc(n.paragraph('<a>Foo'), n.paragraph('Bar<b>'), n.paragraph('Baz')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo"), paragraph("Bar")>"`)
+    await keyboard.press('ControlOrMeta+a')
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar"), paragraph("Baz")>"`)
+  })
+
+  it('can select the entire document if the current textblock is empty', async () => {
+    const { editor, n } = setupTestFromExtension(union(
+      defineDoc(),
+      defineText(),
+      defineParagraph(),
+      defineBaseKeymap(),
+    ))
+
+    editor.set(n.doc(n.paragraph('Foo'), n.paragraph('<a>'), n.paragraph('Bar')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <>"`)
+    await keyboard.press('ControlOrMeta+a')
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph, paragraph("Bar")>"`)
   })
 
   it('can select the entire document directly if `preferBlockSelection` is false', async () => {
@@ -54,12 +84,15 @@ describe('Mod-a', () => {
     ))
 
     editor.set(n.doc(n.paragraph('<a>Foo<b>'), n.paragraph('Bar')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo")>"`)
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo"), paragraph("Bar")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
   })
 })
 
-function getSelectionString(editor: TestEditor) {
-  const fragment = editor.state.selection.content().content
-  return fragment.toString()
+function inspectSelection(editor: TestEditor) {
+  const selection = editor.state.selection
+  const text = selection.content().content.toString()
+  const json = selection.toJSON() as SelectionJSON
+  return `${json.type}: ${text}`
 }

aef7f0e4987722ea7a6ae252059690854a25bfed
ocavue <ocavue@gmail.com>
2025-12-04 00:45:55 +1100

    fix(core): skip block selection when only one block exists

--- packages/core/src/extensions/keymap-base.spec.ts
+++ packages/core/src/extensions/keymap-base.spec.ts
@@ -13,6 +13,7 @@ import {
   defineText,
   setupTestFromExtension,
 } from '../testing'
+import type { SelectionJSON } from '../types/model'
 
 import { defineBaseKeymap } from './keymap-base'
 
@@ -27,9 +28,9 @@ describe('Mod-a', () => {
 
     editor.set(n.doc(n.paragraph('Fo<a>o<b>'), n.paragraph('Bar')))
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo")>"`)
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo"), paragraph("Bar")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
   })
 
   it('can select the entire document if the block is already selected', async () => {
@@ -41,8 +42,9 @@ describe('Mod-a', () => {
     ))
 
     editor.set(n.doc(n.paragraph('<a>Foo<b>'), n.paragraph('Bar')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo")>"`)
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo"), paragraph("Bar")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
   })
 
   it('can select the entire document directly if `preferBlockSelection` is false', async () => {
@@ -54,12 +56,43 @@ describe('Mod-a', () => {
     ))
 
     editor.set(n.doc(n.paragraph('<a>Foo<b>'), n.paragraph('Bar')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo")>"`)
     await keyboard.press('ControlOrMeta+a')
-    expect(getSelectionString(editor)).toMatchInlineSnapshot(`"<paragraph("Foo"), paragraph("Bar")>"`)
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
+  })
+
+  it('can directly set the all selection when all content is selected', async () => {
+    const { editor, n } = setupTestFromExtension(union(
+      defineDoc(),
+      defineText(),
+      defineParagraph(),
+      defineBaseKeymap(),
+    ))
+
+    editor.set(n.doc(n.paragraph('<a>Foo'), n.paragraph('Bar<b>')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("Foo"), paragraph("Bar")>"`)
+    await keyboard.press('ControlOrMeta+a')
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo"), paragraph("Bar")>"`)
+  })
+
+  it('can directly set the all selection if there is only one textblock', async () => {
+    const { editor, n } = setupTestFromExtension(union(
+      defineDoc(),
+      defineText(),
+      defineParagraph(),
+      defineBaseKeymap(),
+    ))
+
+    editor.set(n.doc(n.paragraph('<a>F<b>oo')))
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"text: <paragraph("F")>"`)
+    await keyboard.press('ControlOrMeta+a')
+    expect(inspectSelection(editor)).toMatchInlineSnapshot(`"all: <paragraph("Foo")>"`)
   })
 })
 
-function getSelectionString(editor: TestEditor) {
-  const fragment = editor.state.selection.content().content
-  return fragment.toString()
+function inspectSelection(editor: TestEditor) {
+  const selection = editor.state.selection
+  const text = selection.content().content.toString()
+  const json = selection.toJSON() as SelectionJSON
+  return `${json.type}: ${text}`
 }

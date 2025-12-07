5b0fd522ba710e64d834ab02267c19dc40fc06a2
ocavue <ocavue@gmail.com>
2025-12-07 18:56:25 +1100

    remove optimizeDeps

--- registry/vitest.config.ts
+++ registry/vitest.config.ts
@@ -12,9 +12,6 @@ import { playwrightCommands } from 'vitest-browser-commands'
 import { classReplace } from './src/meta/vite-plugin-class-replace'
 
 export default config({
-  optimizeDeps: {
-    include: ['preact/test-utils'],
-  },
   plugins: [
     wasm(),
     classReplace(),

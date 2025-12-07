81f999e364237694808a00795c0ed99c6b5dd484
ocavue <ocavue@gmail.com>
2025-12-07 15:29:09 +1100

    Add basic tweet example

---
+++ registry/src/react/examples/twitter/extension.ts
@@ -0,0 +1,47 @@
+import { defineBasicExtension } from 'prosekit/basic'
+import {
+  defineNodeSpec,
+  union,
+} from 'prosekit/core'
+
+function defineTweetSpec() {
+  return defineNodeSpec({
+    name: 'tweet',
+    attrs: {
+      tweetId: { default: null },
+    },
+    parseDOM: [{
+      tag: 'iframe[src^="https://platform.twitter.com/embed/Tweet.html]',
+      getAttrs: (node) => {
+        const src = node.getAttribute('src')
+        const match = src?.match(/id=([^&]+)/)
+        return {
+          tweetId: match?.[1] ?? null,
+        }
+      },
+    }],
+    toDOM: (node) => {
+      return [
+        'iframe',
+        {
+          src: `https://platform.twitter.com/embed/Tweet.html?id=${node.attrs.tweetId}`,
+        },
+      ]
+    },
+  })
+}
+
+function defineTweet() {
+  return union(
+    defineTweetSpec(),
+  )
+}
+
+export function defineExtension() {
+  return union(
+    defineBasicExtension(),
+    defineTweet(),
+  )
+}
+
+export type EditorExtension = ReturnType<typeof defineExtension>

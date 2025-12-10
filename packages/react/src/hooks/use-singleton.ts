6a7964366e4b8b9ef14e41a14cc9b757f4440bb2
ocavue <ocavue@gmail.com>
2025-12-10 20:38:49 +1100

    update

--- packages/react/src/hooks/use-singleton.ts
+++ packages/react/src/hooks/use-singleton.ts
@@ -5,6 +5,12 @@ import { useRef } from 'react'
  *
  * It uses the [Avoiding recreating the ref contents] pattern from React docs.
  *
+ * @param factory - A function that returns the singleton value.
+ * @returns The singleton value that's stable during re-renders.
+ *
+ * This pattern is better then using `useMemo` or `useState` because it ensures that
+ * the factory function won't be called twice even during development mode.
+ *
  * [Avoiding recreating the ref contents]: https://react.dev/reference/react/useRef#avoiding-recreating-the-ref-contents
  *
  * @public

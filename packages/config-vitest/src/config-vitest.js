1233016f47fb6061a9b7ab409fb94f08fd3451d1
ocavue <ocavue@gmail.com>
2025-12-12 00:08:45 +1100

    fix

--- packages/config-vitest/src/config-vitest.js
+++ packages/config-vitest/src/config-vitest.js
@@ -5,35 +5,41 @@ import { defu } from 'defu'
 
 const debug = !!process.env.debug && !process.env.CI
 
-/** @type {import('vitest/config').UserWorkspaceConfig} */
-const defaultConfig = {
-  test: {
-    maxWorkers: process.env.CI ? 1 : 2,
-    browser: {
-      enabled: true,
-      viewport: {
-        width: 900,
-        height: 600,
+/**
+ * @returns {import('vitest/config').UserWorkspaceConfig}
+ */
+function getDefaultConfig() {
+  /** @type {import('vitest/config').UserWorkspaceConfig} */
+  const defaultConfig = {
+    test: {
+      maxWorkers: process.env.CI ? 1 : 2,
+      browser: {
+        enabled: true,
+        viewport: {
+          width: 900,
+          height: 600,
+        },
+        provider: playwright({
+          launchOptions: {},
+          contextOptions: {
+            reducedMotion: 'reduce',
+            hasTouch: true,
+            permissions: ['clipboard-read', 'clipboard-write'],
+          },
+        }),
+        headless: !debug,
+        ui: debug,
+        fileParallelism: true,
+        screenshotFailures: debug,
+        instances: [
+          {
+            browser: 'chromium',
+          },
+        ],
       },
-      provider: playwright({
-        launchOptions: {},
-        contextOptions: {
-          reducedMotion: 'reduce',
-          hasTouch: true,
-          permissions: ['clipboard-read', 'clipboard-write'],
-        },
-      }),
-      headless: !debug,
-      ui: debug,
-      fileParallelism: true,
-      screenshotFailures: debug,
-      instances: [
-        {
-          browser: 'chromium',
-        },
-      ],
     },
-  },
+  }
+  return defaultConfig
 }
 
 /**
@@ -41,8 +47,5 @@ const defaultConfig = {
  * @returns {import('vitest/config').UserWorkspaceConfig}
  */
 export function config(userConfig = undefined) {
-  /**
-   * @type {import('vitest/config').UserWorkspaceConfig}
-   */
-  return defu(userConfig, defaultConfig)
+  return defu(userConfig, getDefaultConfig())
 }

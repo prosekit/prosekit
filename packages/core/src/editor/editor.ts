a6f8ff63a9d8d2952d45bb883991ee4f4396a08a
ocavue <ocavue@gmail.com>
2025-12-12 19:59:43 +1100

    revert

--- packages/core/src/editor/editor.ts
+++ packages/core/src/editor/editor.ts
@@ -297,7 +297,6 @@ export class EditorInstance {
     }
     this.view = new EditorView({ mount: place }, this.directEditorProps)
     this.afterMounted.forEach((callback) => callback())
-    this.afterMounted = []
   }
 
   public unmount(): void {

cd4b25a5263e96d9236c69709a2498e68387e2fa
ocavue <ocavue@gmail.com>
2025-12-12 20:08:35 +1100

    remove flushSync was called from inside checking

--- packages/config-vitest/src/setup-vitest.js
+++ packages/config-vitest/src/setup-vitest.js
@@ -5,7 +5,6 @@ import failOnConsole from 'vitest-fail-on-console'
 /** @type {string[]} */
 const ALLOWED_MESSAGES = [
   'Cannot find the loroNode',
-  'flushSync was called from inside a lifecycle method. React cannot flush when React is already rendering.',
 ]
 
 failOnConsole({

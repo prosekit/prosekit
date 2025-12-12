082e6c544253ef17e44c44c45915df244288d7da
ocavue <ocavue@gmail.com>
2025-12-12 20:16:32 +1100

    fix yjs warning

--- packages/config-vitest/src/setup-vitest.js
+++ packages/config-vitest/src/setup-vitest.js
@@ -5,6 +5,7 @@ import failOnConsole from 'vitest-fail-on-console'
 /** @type {string[]} */
 const ALLOWED_MESSAGES = [
   'Cannot find the loroNode',
+  'TextSelection endpoint not pointing into a node with inline content',
 ]
 
 failOnConsole({

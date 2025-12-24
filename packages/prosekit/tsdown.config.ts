e3ebbf579d1ee1434b28231a6f2ff8e583d2a9ee
ocavue <ocavue@users.noreply.github.com>
2025-12-24 16:40:46 +1100

    chore(deps): update tsdown (#1395)

--- packages/prosekit/tsdown.config.ts
+++ packages/prosekit/tsdown.config.ts
@@ -12,13 +12,27 @@ if (typeof entries !== 'object') {
   throw new TypeError('entries must be an object')
 }
 
+function assertString(value: string | string[]): string {
+  if (typeof value === 'string') {
+    return value
+  }
+  if (value.length === 1) {
+    return value[0]
+  }
+  throw new TypeError('value must be a string')
+}
+
 // Skip CSS files until the following issue is resolved:
 // https://github.com/rolldown/tsdown/issues/216
 const entriesWithoutCSS = Object.fromEntries(
-  Object.entries(entries).filter(([, value]) => !value.endsWith('.css')),
+  Object.entries(entries)
+    .map(([key, value]) => [key, assertString(value)])
+    .filter(([, value]) => !value.endsWith('.css')),
 )
 const entriesWithCSS = Object.fromEntries(
-  Object.entries(entries).filter(([, value]) => value.endsWith('.css')),
+  Object.entries(entries)
+    .map(([key, value]) => [key, assertString(value)])
+    .filter(([, value]) => value.endsWith('.css')),
 )
 
 const configObjectWithoutCSS: UserConfig = {

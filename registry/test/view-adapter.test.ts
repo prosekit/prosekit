39a76bd90a4f1edff10645c6b0370958ea6eeb74
ocavue <ocavue@gmail.com>
2025-12-25 19:09:31 +1100

    wip

--- registry/test/view-adapter.test.ts
+++ registry/test/view-adapter.test.ts
@@ -1,25 +1,14 @@
-import {
-  expect,
-  it,
-} from 'vitest'
+import { it } from 'vitest'
 
 import {
-  expectLocatorToHaveCount,
   testStory,
   testStoryConsistency,
-  waitForEditor,
 } from './helpers'
 
-testStoryConsistency('badge')
+testStoryConsistency('view-adapter')
 
-testStory('badge', () => {
-  it('renders badges with node views', async () => {
-    const editor = await waitForEditor()
-    const badges = editor.locate('[data-node-view-root="true"]')
-
-    await expectLocatorToHaveCount(badges, 3)
-    await expect.element(badges.nth(0)).toHaveTextContent('Default')
-    await expect.element(badges.nth(1)).toHaveTextContent('Primary')
-    await expect.element(badges.nth(2)).toHaveTextContent('Success')
+testStory('view-adapter', () => {
+  it('renders document JSON in atom block view', async () => {
+    // TODO: Implement this test
   })
 })

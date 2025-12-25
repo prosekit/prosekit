e10ce02ab9dd20a703e65b2c3558589aa3e3cfaa
ocavue <ocavue@gmail.com>
2025-12-25 19:03:17 +1100

    add test

--- registry/src/react/examples/view-adapter/atom-block-view.tsx
+++ registry/src/react/examples/view-adapter/atom-block-view.tsx
@@ -1,9 +1,20 @@
-import type { ReactNodeViewProps } from 'prosekit/react'
+import {
+  useEditorDerivedValue,
+  type ReactNodeViewProps,
+} from 'prosekit/react'
+import { useCallback } from 'react'
 
 export function AtomBlockView(props: ReactNodeViewProps) {
+  const docJSON = useEditorDerivedValue(useCallback(editor => {
+    return JSON.stringify(editor.getDocJSON(), null, 2)
+  }, []))
+
   return (
     <div data-atom-block="true" data-atom-block-view="true" className="bg-green-500/50">
-      Atom Block View
+      <div data-testid="atom-block-view-label">Atom Block View</div>
+      <div data-testid="atom-block-view-context">
+        <pre>{docJSON}</pre>
+      </div>
     </div>
   )
 }

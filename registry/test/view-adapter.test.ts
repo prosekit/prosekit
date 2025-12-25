430c33c68d8b370f40ac2c2474c18546ed0ea9ab
ocavue <ocavue@gmail.com>
2025-12-25 19:23:10 +1100

    updae test

--- registry/test/view-adapter.test.ts
+++ registry/test/view-adapter.test.ts
@@ -15,9 +15,9 @@ testStoryConsistency('view-adapter')
 testStory('view-adapter', () => {
   it('renders atom block with custom node view', async () => {
     const editor = await waitForEditor()
-    const atomBlockViews = editor.locate('[data-atom-block-view="true"]')
+    const atomBlock = editor.locate('[data-atom-block-view="true"]')
 
-    await expectLocatorToHaveCount(atomBlockViews, 1)
-    await expect.element(atomBlockViews.nth(0)).toHaveTextContent('Atom Block View')
+    await expectLocatorToHaveCount(atomBlock, 1)
+    await expect.element(atomBlock.getByTestId('atom-block-view-label')).toHaveTextContent('Atom Block View')
   })
 })

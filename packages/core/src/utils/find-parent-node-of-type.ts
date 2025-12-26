5dcea183e80ab71b5b073d09443d6c1c78b81ffe
ocavue <ocavue@gmail.com>
2025-12-26 18:04:57 +1100

    findParentNodeOfType

--- packages/core/src/utils/find-parent-node-of-type.ts
+++ packages/core/src/utils/find-parent-node-of-type.ts
@@ -7,7 +7,7 @@ import {
   findParentNode,
   type FindParentNodeResult,
 } from './find-parent-node'
-import { getNodeType } from './get-node-type'
+import { getNodeTypes } from './get-node-types'
 
 /**
  * Finds the closest parent node that matches the given node type.
@@ -18,12 +18,12 @@ export function findParentNodeOfType(
   /**
    * The type of the node to find.
    */
-  type: NodeType | string,
+  type: string | NodeType | string[] | NodeType[],
   /**
    * The position to start searching from.
    */
   $pos: ResolvedPos,
 ): FindParentNodeResult | undefined {
-  const nodeType = getNodeType($pos.doc.type.schema, type)
-  return findParentNode((node) => node.type === nodeType, $pos)
+  const nodeTypes = getNodeTypes($pos.doc.type.schema, type)
+  return findParentNode((node) => nodeTypes.includes(node.type), $pos)
 }

607cc96da4494230ad514c5e93247271b3df5d94
ocavue <ocavue@gmail.com>
2025-12-27 15:02:48 +1100

    wip

--- website/src/example-data.ts
+++ website/src/example-data.ts
@@ -1,20 +1,39 @@
 import type { StarlightUserConfig } from '@astrojs/starlight/types'
 import registry from 'prosekit-registry/registry.gen.json'
 
-export const stories = Array.from(
-  new Set(
-    registry.items
-      .filter(item => !item.meta.hidden)
-      .map(item => item.meta.story)
-      .filter(story => !!story),
-  ),
-).sort()
+interface Story {
+  name: string
+  link: string
+  description: string
+}
+
+export const storyMap = new Map<string, Story>()
+
+for (const item of registry.items) {
+  if (item.meta.hidden) {
+    continue
+  }
+  const name = item.meta.story
+  if (!name) {
+    continue
+  }
+  if (storyMap.has(name)) {
+    continue
+  }
+  storyMap.set(name, {
+    name,
+    link: `/examples/${name}`,
+    description: item.description,
+  })
+}
 
 type Sidebar = StarlightUserConfig['sidebar']
 
-export const exampleSidebarItems = stories.map((story) => ({
-  label: story,
-  link: `/examples/${story}`,
+export const stories: Story[] = Array.from(storyMap.values())
+
+const exampleSidebarItems = stories.map(story => ({
+  label: story.name,
+  link: story.link,
 }))
 
 export const exampleSidebar = [

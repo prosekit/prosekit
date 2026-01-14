1524ca66aa13cfdd9536c12e2cb336fdf36b4c4f
ocavue <ocavue@gmail.com>
2026-01-14 13:55:07 +1100

    add comment

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,6 @@
+// We need prettier to format html string templates in ts files.
+// See https://github.com/dprint/dprint-plugin-typescript/issues/769
+
 /**
  * @see https://prettier.io/docs/configuration
  * @type {import("prettier").Config}

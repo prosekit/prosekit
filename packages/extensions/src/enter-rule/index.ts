97b5c8a2be17e93398b4b93016a4b994192499bd
ocavue <ocavue@users.noreply.github.com>
2026-02-17 22:42:29 +1100

    refactor: add prosemirror-enter-rules package (#1450)

--- packages/extensions/src/enter-rule/index.ts
+++ packages/extensions/src/enter-rule/index.ts
@@ -1,104 +1,28 @@
+import { defineFacet, defineFacetPayload, pluginFacet, type PlainExtension, type PluginPayload } from '@prosekit/core'
 import {
-  defineFacet,
-  defineFacetPayload,
-  getNodeType,
-  isTextSelection,
-  maybeRun,
-  OBJECT_REPLACEMENT_CHARACTER,
-  pluginFacet,
-  type PlainExtension,
-  type PluginPayload,
-} from '@prosekit/core'
-import { keydownHandler } from '@prosekit/pm/keymap'
-import type { Attrs, NodeType } from '@prosekit/pm/model'
-import { PluginKey, ProseMirrorPlugin, type Command, type EditorState, type Transaction } from '@prosekit/pm/state'
-import type { EditorView } from '@prosekit/pm/view'
+  createEnterRulePlugin,
+  createTextBlockEnterRule,
+  type EnterRule,
+  type EnterRuleHandler,
+  type EnterRuleHandlerOptions,
+  type TextBlockEnterRuleOptions as _TextBlockEnterRuleOptions,
+} from 'prosemirror-enter-rules'
 
-/**
- * @public
- *
- * Options for {@link EnterRuleHandler}.
- */
-export interface EnterRuleHandlerOptions {
-  /**
-   * The current editor state.
-   */
-  state: EditorState
-
-  /**
-   * The start position of the matched text.
-   */
-  from: number
-
-  /**
-   * The end position of the matched text.
-   */
-  to: number
-
-  /**
-   * The matched result from the regular expression.
-   */
-  match: RegExpExecArray
-}
-
-/**
- * @public
- */
-export type EnterRuleHandler = (options: EnterRuleHandlerOptions) => Transaction | null
+export type { EnterRuleHandler, EnterRuleHandlerOptions }
 
 /**
  * Options for {@link defineEnterRule}.
  *
  * @public
  */
-export type EnterRuleOptions = {
-  /**
-   * The regular expression to match against. It should end with `$`.
-   */
-  regex: RegExp
-
-  /**
-   * A function to be called when an enter rule is triggered.
-   */
-  handler: EnterRuleHandler
-
-  /**
-   * Whether to stop further handlers from being called if this rule is triggered.
-   *
-   * @default false
-   */
-  stop?: boolean
-}
+export type EnterRuleOptions = EnterRule
 
 /**
  * Options for {@link defineTextBlockEnterRule}.
  *
  * @public
  */
-export interface TextBlockEnterRuleOptions {
-  /**
-   * The regular expression to match against. It should end with `$`.
-   */
-  regex: RegExp
-
-  /**
-   * The node type to replace the matched text with.
-   */
-  type: string | NodeType
-
-  /**
-   * Attributes to set on the node. If a function is provided, it will be called
-   * with the matched result from the regular expression.
-   */
-  attrs?: Attrs | null | ((match: RegExpMatchArray) => Attrs | null)
-
-  /**
-   * Whether to stop further handlers from being called if this rule is triggered.
-   *
-   * @default true
-   */
-  stop?: boolean
-}
+export type TextBlockEnterRuleOptions = _TextBlockEnterRuleOptions
 
 /**
  * Defines an enter rule. An enter rule applies when the text directly in front of
@@ -109,13 +33,8 @@ export interface TextBlockEnterRuleOptions {
  *
  * @public
  */
-export function defineEnterRule({
-  regex,
-  handler,
-  stop = false,
-}: EnterRuleOptions): PlainExtension {
-  const rule: EnterRule = new EnterRule(regex, handler, stop)
-  return defineFacetPayload(enterRule, [rule]) as PlainExtension
+export function defineEnterRule(options: EnterRuleOptions): PlainExtension {
+  return defineFacetPayload(enterRuleFacet, [options]) as PlainExtension
 }
 
 /**
@@ -127,106 +46,14 @@ export function defineEnterRule({
  *
  * @public
  */
-export function defineTextBlockEnterRule({
-  regex,
-  type,
-  attrs,
-  stop = true,
-}: TextBlockEnterRuleOptions): PlainExtension {
-  return defineEnterRule({
-    regex,
-    handler: ({ state, from, to, match }) => {
-      const nodeType = getNodeType(state.schema, type)
-      const $start = state.doc.resolve(from)
-
-      if (
-        !$start
-          .node(-1)
-          .canReplaceWith($start.index(-1), $start.indexAfter(-1), nodeType)
-      ) {
-        return null
-      }
-
-      const nodeAttrs = maybeRun(attrs, match)
-      return state.tr
-        .delete(from, to)
-        .setBlockType(from, from, nodeType, nodeAttrs)
-    },
-    stop,
-  })
+export function defineTextBlockEnterRule(options: TextBlockEnterRuleOptions): PlainExtension {
+  return defineEnterRule(createTextBlockEnterRule(options))
 }
 
-/**
- * @internal
- */
-class EnterRule {
-  constructor(
-    readonly regex: RegExp,
-    readonly handler: EnterRuleHandler,
-    readonly stop: boolean,
-  ) {}
-}
-
-const enterRule = defineFacet<EnterRule, PluginPayload>({
-  reduce: () => {
-    let rules: EnterRule[] = []
-
-    const command: Command = (state, dispatch, view) => {
-      if (!view) return false
-      return execRules(view, rules, dispatch)
-    }
-    const handler = keydownHandler({ Enter: command })
-    const plugin = new ProseMirrorPlugin({
-      key: new PluginKey('prosekit-enter-rule'),
-      props: { handleKeyDown: handler },
-    })
-
-    return function reducer(inputs) {
-      rules = inputs
-      return plugin
-    }
+const enterRuleFacet = defineFacet<EnterRule, PluginPayload>({
+  reducer: (rules: EnterRule[]): PluginPayload => {
+    return createEnterRulePlugin({ rules })
   },
 
   parent: pluginFacet,
 })
-
-function execRules(
-  view: EditorView,
-  rules: readonly EnterRule[],
-  dispatch?: (tr: Transaction) => void,
-): boolean {
-  if (view.composing) return false
-  const state = view.state
-  const selection = state.selection
-  if (!isTextSelection(selection)) return false
-  const $cursor = selection.$cursor
-  if (!$cursor || $cursor.parent.type.spec.code) return false
-
-  const textBefore = $cursor.parent.textBetween(
-    Math.max(0, $cursor.parentOffset - MAX_MATCH),
-    $cursor.parentOffset,
-    null,
-    OBJECT_REPLACEMENT_CHARACTER,
-  )
-
-  for (const rule of rules) {
-    rule.regex.lastIndex = 0
-    const match = rule.regex.exec(textBefore)
-    const tr = match
-      && rule.handler({
-        state,
-        from: $cursor.pos - match[0].length,
-        to: $cursor.pos,
-        match,
-      })
-    if (!tr) continue
-    dispatch?.(tr)
-
-    if (rule.stop) {
-      return true
-    }
-  }
-  return false
-}
-
-const MAX_MATCH = 200
---
+++ packages/prosemirror-enter-rules/vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineProject } from 'vitest/config'
+
+export default defineProject({
+  test: {
+    environment: 'jsdom',
+    sequence: {
+      groupOrder: 100,
+    },
+  },
+})

97917f213e4c5936fc184d6860075114a0fc56cd
ocavue <ocavue@gmail.com>
2026-02-19 19:38:28 +1100

    add todo

--- packages/extensions/src/loro/loro-commands.ts
+++ packages/extensions/src/loro/loro-commands.ts
@@ -2,8 +2,11 @@ import { defineCommands, type Extension } from '@prosekit/core'
 import type { Command } from '@prosekit/pm/state'
 import { redo, undo } from 'loro-prosemirror'
 
+// TODO: just for debug
+const undoCommand: Command = undo
+
 const commands = {
-  undo: () => undo as unknown as Command,
+  undo: () => undoCommand,
   redo: () => redo as unknown as Command,
 }
 

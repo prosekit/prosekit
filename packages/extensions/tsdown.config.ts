057a43df29c4db0322a7d543f32461de3dbb4ca4
ocavue <ocavue@gmail.com>
2026-02-27 22:37:18 +1100

    remove config

--- packages/extensions/tsdown.config.ts
+++ packages/extensions/tsdown.config.ts
@@ -2,9 +2,5 @@ import { config } from '@prosekit/config-tsdown'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig(config({
-  entry: {
-    // Ensure that we get dist/shiki-highlighter-chunk.js instead of dist/shiki-highlighter-chunk-C5VVQ2TD.js.
-    'shiki-highlighter-chunk': './src/code-block/shiki-highlighter-chunk.ts',
-  },
   cwd: import.meta.dirname,
 }))

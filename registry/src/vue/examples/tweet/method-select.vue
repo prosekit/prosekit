214366e357f101511d48d9c85b2d0b94b78b2ab6
ocavue <ocavue@gmail.com>
2026-03-01 17:43:29 +1100

    add vue tweet

---
+++ registry/src/vue/examples/tweet/method-select.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: 'iframe' | 'vue'
+}>()
+
+const emit = defineEmits<{
+  change: [value: 'iframe' | 'vue']
+}>()
+
+const id = `method-select-${Math.random().toString(36).slice(2)}`
+const iframeId = `${id}-iframe`
+const vueId = `${id}-vue`
+</script>
+
+<template>
+  <fieldset class="not-content">
+    <legend>Select a render method:</legend>
+
+    <div>
+      <input
+        :id="iframeId"
+        type="radio"
+        :name="id"
+        value="iframe"
+        :checked="props.value === 'iframe'"
+        @change="emit('change', 'iframe')"
+      />
+      <label :for="iframeId">iframe</label>
+    </div>
+
+    <div>
+      <input
+        :id="vueId"
+        type="radio"
+        :name="id"
+        value="vue"
+        :checked="props.value === 'vue'"
+        @change="emit('change', 'vue')"
+      />
+      <label :for="vueId">vue-tweet</label>
+    </div>
+  </fieldset>
+</template>
---
+++ registry/src/vue/sample/sample-doc-tweet.ts
@@ -0,0 +1,22 @@
+import type { NodeJSON } from 'prosekit/core'
+
+export const sampleContent: NodeJSON = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'Render a tweet in your document',
+        },
+      ],
+    },
+    {
+      type: 'tweet',
+      attrs: {
+        tweetId: '1598038815599661056',
+      },
+    },
+  ],
+}

3082c8ae79e45bb54a5e0ecec116321d956d38df
ocavue <ocavue@gmail.com>
2026-03-05 16:27:13 +1100

    w

--- packages/prosekit/tsdown.config.ts
+++ packages/prosekit/tsdown.config.ts
@@ -1,5 +1,3 @@
 import { config } from '@prosekit/config-tsdown'
 
-const configObject = config({ cwd: import.meta.dirname })
-
-export default configObject
+export default config({ cwd: import.meta.dirname })

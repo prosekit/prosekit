552c93a938cae84e9e751219a015c465fd191c03
ocavue <ocavue@gmail.com>
2026-03-05 13:24:12 +1100

    chore: clean up dist

--- packages/svelte/dist/build/components/block-handle/index.gen.js
+++
@@ -1,3 +0,0 @@
-export { BlockHandleAdd } from "./block-handle-add.gen.js";
-export { BlockHandleDraggable } from "./block-handle-draggable.gen.js";
-export { BlockHandlePopover } from "./block-handle-popover.gen.js";
--- packages/svelte/dist/build/components/table-handle/table-handle-root.gen.js
+++
@@ -1,2 +0,0 @@
-import Component from './table-handle-root.gen.svelte';
-export const TableHandleRoot = Component;

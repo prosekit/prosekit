b510af072c1c06d5aa3aaceabac09fa7f75ceae2
ocavue <ocavue@gmail.com>
2026-03-05 14:11:14 +1100

    chore: [master] .d.ts

---
+++ packages/svelte/dist/build/components/table-handle/index.gen.d.ts
@@ -0,0 +1,10 @@
+export { TableHandleColumnRoot, type TableHandleColumnRootProps } from './table-handle-column-root.gen.ts';
+export { TableHandleColumnTrigger, type TableHandleColumnTriggerProps } from './table-handle-column-trigger.gen.ts';
+export { TableHandleDragPreview, type TableHandleDragPreviewProps } from './table-handle-drag-preview.gen.ts';
+export { TableHandleDropIndicator, type TableHandleDropIndicatorProps } from './table-handle-drop-indicator.gen.ts';
+export { TableHandlePopoverContent, type TableHandlePopoverContentProps } from './table-handle-popover-content.gen.ts';
+export { TableHandlePopoverItem, type TableHandlePopoverItemProps } from './table-handle-popover-item.gen.ts';
+export { TableHandleRoot, type TableHandleRootProps } from './table-handle-root.gen.ts';
+export { TableHandleRowRoot, type TableHandleRowRootProps } from './table-handle-row-root.gen.ts';
+export { TableHandleRowTrigger, type TableHandleRowTriggerProps } from './table-handle-row-trigger.gen.ts';
+//# sourceMappingURL=index.gen.d.ts.map

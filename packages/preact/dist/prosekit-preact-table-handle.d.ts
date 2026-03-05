20f6e645fa774804c6858a01c1b217a4c06a104b
ocavue <ocavue@gmail.com>
2026-03-05 14:25:02 +1100

    chore: [master] .d.ts

---
+++ packages/preact/dist/prosekit-preact-table-handle.d.ts
@@ -0,0 +1,69 @@
+import { t as CreateProps } from "./create-props-PUVFMh9y.js";
+import { ForwardRefExoticComponent, HTMLAttributes, RefAttributes } from "preact/compat";
+import { TableHandleColumnRootElement, TableHandleColumnRootEvents, TableHandleColumnRootProps as TableHandleColumnRootProps$1, TableHandleColumnTriggerElement, TableHandleColumnTriggerEvents, TableHandleColumnTriggerProps as TableHandleColumnTriggerProps$1, TableHandleDragPreviewElement, TableHandleDragPreviewEvents, TableHandleDragPreviewProps as TableHandleDragPreviewProps$1, TableHandleDropIndicatorElement, TableHandleDropIndicatorEvents, TableHandleDropIndicatorProps as TableHandleDropIndicatorProps$1, TableHandlePopoverContentElement, TableHandlePopoverContentEvents, TableHandlePopoverContentProps as TableHandlePopoverContentProps$1, TableHandlePopoverItemElement, TableHandlePopoverItemEvents, TableHandlePopoverItemProps as TableHandlePopoverItemProps$1, TableHandleRootElement, TableHandleRootEvents, TableHandleRootProps as TableHandleRootProps$1, TableHandleRowRootElement, TableHandleRowRootEvents, TableHandleRowRootProps as TableHandleRowRootProps$1, TableHandleRowTriggerElement, TableHandleRowTriggerEvents, TableHandleRowTriggerProps as TableHandleRowTriggerProps$1 } from "@prosekit/web/table-handle";
+
+//#region src/components/table-handle/table-handle-column-root.gen.d.ts
+/**
+ * Props for the {@link TableHandleColumnRoot} component.
+ */
+interface TableHandleColumnRootProps extends Partial<CreateProps<TableHandleColumnRootProps$1, TableHandleColumnRootEvents>> {}
+declare const TableHandleColumnRoot: ForwardRefExoticComponent<Partial<TableHandleColumnRootProps> & RefAttributes<TableHandleColumnRootElement> & HTMLAttributes<TableHandleColumnRootElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-column-trigger.gen.d.ts
+/**
+ * Props for the {@link TableHandleColumnTrigger} component.
+ */
+interface TableHandleColumnTriggerProps extends Partial<CreateProps<TableHandleColumnTriggerProps$1, TableHandleColumnTriggerEvents>> {}
+declare const TableHandleColumnTrigger: ForwardRefExoticComponent<Partial<TableHandleColumnTriggerProps> & RefAttributes<TableHandleColumnTriggerElement> & HTMLAttributes<TableHandleColumnTriggerElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-drag-preview.gen.d.ts
+/**
+ * Props for the {@link TableHandleDragPreview} component.
+ */
+interface TableHandleDragPreviewProps extends Partial<CreateProps<TableHandleDragPreviewProps$1, TableHandleDragPreviewEvents>> {}
+declare const TableHandleDragPreview: ForwardRefExoticComponent<Partial<TableHandleDragPreviewProps> & RefAttributes<TableHandleDragPreviewElement> & HTMLAttributes<TableHandleDragPreviewElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-drop-indicator.gen.d.ts
+/**
+ * Props for the {@link TableHandleDropIndicator} component.
+ */
+interface TableHandleDropIndicatorProps extends Partial<CreateProps<TableHandleDropIndicatorProps$1, TableHandleDropIndicatorEvents>> {}
+declare const TableHandleDropIndicator: ForwardRefExoticComponent<Partial<TableHandleDropIndicatorProps> & RefAttributes<TableHandleDropIndicatorElement> & HTMLAttributes<TableHandleDropIndicatorElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-popover-content.gen.d.ts
+/**
+ * Props for the {@link TableHandlePopoverContent} component.
+ */
+interface TableHandlePopoverContentProps extends Partial<CreateProps<TableHandlePopoverContentProps$1, TableHandlePopoverContentEvents>> {}
+declare const TableHandlePopoverContent: ForwardRefExoticComponent<Partial<TableHandlePopoverContentProps> & RefAttributes<TableHandlePopoverContentElement> & HTMLAttributes<TableHandlePopoverContentElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-popover-item.gen.d.ts
+/**
+ * Props for the {@link TableHandlePopoverItem} component.
+ */
+interface TableHandlePopoverItemProps extends Partial<CreateProps<TableHandlePopoverItemProps$1, TableHandlePopoverItemEvents>> {}
+declare const TableHandlePopoverItem: ForwardRefExoticComponent<Partial<TableHandlePopoverItemProps> & RefAttributes<TableHandlePopoverItemElement> & HTMLAttributes<TableHandlePopoverItemElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-root.gen.d.ts
+/**
+ * Props for the {@link TableHandleRoot} component.
+ */
+interface TableHandleRootProps extends Partial<CreateProps<TableHandleRootProps$1, TableHandleRootEvents>> {}
+declare const TableHandleRoot: ForwardRefExoticComponent<Partial<TableHandleRootProps> & RefAttributes<TableHandleRootElement> & HTMLAttributes<TableHandleRootElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-row-root.gen.d.ts
+/**
+ * Props for the {@link TableHandleRowRoot} component.
+ */
+interface TableHandleRowRootProps extends Partial<CreateProps<TableHandleRowRootProps$1, TableHandleRowRootEvents>> {}
+declare const TableHandleRowRoot: ForwardRefExoticComponent<Partial<TableHandleRowRootProps> & RefAttributes<TableHandleRowRootElement> & HTMLAttributes<TableHandleRowRootElement>>;
+//#endregion
+//#region src/components/table-handle/table-handle-row-trigger.gen.d.ts
+/**
+ * Props for the {@link TableHandleRowTrigger} component.
+ */
+interface TableHandleRowTriggerProps extends Partial<CreateProps<TableHandleRowTriggerProps$1, TableHandleRowTriggerEvents>> {}
+declare const TableHandleRowTrigger: ForwardRefExoticComponent<Partial<TableHandleRowTriggerProps> & RefAttributes<TableHandleRowTriggerElement> & HTMLAttributes<TableHandleRowTriggerElement>>;
+//#endregion
+export { TableHandleColumnRoot, type TableHandleColumnRootProps, TableHandleColumnTrigger, type TableHandleColumnTriggerProps, TableHandleDragPreview, type TableHandleDragPreviewProps, TableHandleDropIndicator, type TableHandleDropIndicatorProps, TableHandlePopoverContent, type TableHandlePopoverContentProps, TableHandlePopoverItem, type TableHandlePopoverItemProps, TableHandleRoot, type TableHandleRootProps, TableHandleRowRoot, type TableHandleRowRootProps, TableHandleRowTrigger, type TableHandleRowTriggerProps };
+//# sourceMappingURL=prosekit-preact-table-handle.d.ts.map
---
+++ packages/vue/dist/prosekit-vue-drop-indicator.d.ts
@@ -0,0 +1,17 @@
+import { t as CreateEmits } from "./create-emits-B8725q2G.js";
+import { DefineSetupFnComponent, HTMLAttributes } from "vue";
+import { DropIndicatorEvents, DropIndicatorProps as DropIndicatorProps$1 } from "@prosekit/web/drop-indicator";
+
+//#region src/components/drop-indicator/drop-indicator.gen.d.ts
+/**
+ * Props for the {@link DropIndicator} component.
+ */
+interface DropIndicatorProps extends Partial<DropIndicatorProps$1> {}
+/**
+ * Emits for the {@link DropIndicator} component.
+ */
+interface DropIndicatorEmits extends CreateEmits<DropIndicatorEvents> {}
+declare const DropIndicator: DefineSetupFnComponent<DropIndicatorProps & HTMLAttributes, DropIndicatorEmits>;
+//#endregion
+export { DropIndicator, type DropIndicatorEmits, type DropIndicatorProps };
+//# sourceMappingURL=prosekit-vue-drop-indicator.d.ts.map

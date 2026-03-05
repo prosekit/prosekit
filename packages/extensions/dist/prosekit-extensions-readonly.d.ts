a94a22d2c206bcf3b9b1094e3bca056308abef7e
ocavue <ocavue@gmail.com>
2026-03-05 20:53:18 +1100

    chore: [master] .d.ts

---
+++ packages/extensions/dist/prosekit-extensions-readonly.d.ts
@@ -0,0 +1,10 @@
+import { PlainExtension } from "@prosekit/core";
+
+//#region src/readonly/index.d.ts
+/**
+ * Make the editor read-only.
+ */
+declare function defineReadonly(): PlainExtension;
+//#endregion
+export { defineReadonly };
+//# sourceMappingURL=prosekit-extensions-readonly.d.ts.map
---
+++ packages/svelte/dist/build/components/drop-indicator/index.gen.d.ts
@@ -0,0 +1,2 @@
+export { DropIndicator, type DropIndicatorProps } from './drop-indicator.gen.ts';
+//# sourceMappingURL=index.gen.d.ts.map

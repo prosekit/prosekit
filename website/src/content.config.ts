8792f75451b0d27bbb13dcbb0772bf011ffd1f53
ocavue <ocavue@gmail.com>
2026-03-12 15:42:18 +1100

    typecheck

--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -1,7 +1,16 @@
 import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
+import type { CollectionConfig } from 'astro/content/config'
+import type { Loader } from 'astro/loaders'
 
-export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-}
+type ExtendedSchema = ReturnType<ReturnType<typeof docsSchema>>
+
+type Docs = CollectionConfig<ExtendedSchema, Loader>
+
+const docs: Docs = defineCollection({
+  loader: docsLoader(),
+  schema: docsSchema(),
+})
+
+export const collections = { docs }

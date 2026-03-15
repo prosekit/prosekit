fa5101d1d3279c9fa2058f36d5ce882bbb19d9f9
ocavue <ocavue@gmail.com>
2026-03-15 22:00:19 +1100

    add outline

--- packages/web/src/utils/get-client-rect.ts
+++ packages/web/src/utils/get-client-rect.ts
@@ -5,7 +5,7 @@ interface Rect {
   left: number
 }
 
-interface IncludeMarginsOptions {
+interface IncludeExtraOptions {
   top?: boolean
   bottom?: boolean
   right?: boolean
@@ -14,16 +14,16 @@ interface IncludeMarginsOptions {
 
 /**
  * Similar to `element.getBoundingClientRect`, but handles `display: contents` CSS
- * property and optionally includes margins.
+ * property and optionally includes margins and outlines.
  */
-export function getClientRect(element: Element, includeMargins?: IncludeMarginsOptions | false): Rect {
+export function getClientRect(element: Element, includeExtra?: IncludeExtraOptions | false): Rect {
   const rect = element.getBoundingClientRect()
   if (rect.width === 0 && rect.height === 0 && rect.x === 0 && rect.y === 0) {
     // Suspiciously rect, probably an element with `display: contents`, in
     // which case `element.getClientRects()` will return an empty array.
     if (element.getClientRects().length === 0) {
       const children = [...element.children]
-      const rects = children.map(child => getClientRect(child, includeMargins))
+      const rects = children.map(child => getClientRect(child, includeExtra))
       if (rects.length === 0) {
         return rect
       }
@@ -41,10 +41,10 @@ export function getClientRect(element: Element, includeMargins?: IncludeMarginsO
       return { top, bottom, left, right }
     }
   }
-  return includeMargins ? addMargins(element, rect, includeMargins) : rect
+  return includeExtra ? addExtra(element, rect, includeExtra) : rect
 }
 
-function addMargins(element: Element, rect: Rect, options: IncludeMarginsOptions): Rect {
+function addExtra(element: Element, rect: Rect, options: IncludeExtraOptions): Rect {
   const view = element.ownerDocument?.defaultView
   if (!view) {
     return rect
@@ -56,10 +56,18 @@ function addMargins(element: Element, rect: Rect, options: IncludeMarginsOptions
   const marginRight = options.right ? Number.parseFloat(style.marginRight) || 0 : 0
   const marginLeft = options.left ? Number.parseFloat(style.marginLeft) || 0 : 0
 
+  const outlineWidth = Number.parseFloat(style.outlineWidth) || 0
+  const outlineOffset = Number.parseFloat(style.outlineOffset) || 0
+  const outline = Math.max(outlineWidth + outlineOffset, 0)
+  const outlineTop = options.top ? outline : 0
+  const outlineBottom = options.bottom ? outline : 0
+  const outlineRight = options.right ? outline : 0
+  const outlineLeft = options.left ? outline : 0
+
   return {
-    top: rect.top - marginTop,
-    bottom: rect.bottom + marginBottom,
-    right: rect.right + marginRight,
-    left: rect.left - marginLeft,
+    top: rect.top - marginTop - outlineTop,
+    bottom: rect.bottom + marginBottom + outlineBottom,
+    right: rect.right + marginRight + outlineRight,
+    left: rect.left - marginLeft - outlineLeft,
   }
 }

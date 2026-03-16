d5280752f9fcc89a5367dd7e6ce84650a25497ef
ocavue <ocavue@gmail.com>
2026-03-16 17:10:09 +1100

    fix test

--- aria-ui-packages/integrations/vitest.config.ts
+++ aria-ui-packages/integrations/vitest.config.ts
@@ -1,25 +1,7 @@
-import { playwright } from '@vitest/browser-playwright'
-import { defineProject } from 'vitest/config'
+import { config } from '@prosekit/config-vitest'
 
-const coverageEnabled = !!process.env.VITEST_COVERAGE
-
-export default defineProject({
-  resolve: {
+export default config({
+      resolve: {
     conditions: ['aria-ui-dev'],
-  },
-  test: {
-    include: ['src/**/*.test.ts'],
-    browser: {
-      enabled: true,
-      headless: true,
-      provider: playwright(),
-      instances: coverageEnabled
-        ? [{ browser: 'chromium' }]
-        : [
-          { browser: 'chromium' },
-          { browser: 'firefox' },
-          { browser: 'webkit' },
-        ],
-    },
   },
 })

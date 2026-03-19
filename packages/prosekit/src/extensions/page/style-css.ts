de86e0670e12693186a3ee04ac4ac5c78ec3bdbb
ocavue <ocavue@gmail.com>
2026-03-19 18:20:57 +1100

    fix

---
+++ packages/prosekit/src/extensions/page/style-css.ts
@@ -0,0 +1,9 @@
+/**
+ * A CSS file that contains some necessary styles for page layout.
+ *
+ * [view source code](https://unpkg.com/prosekit/extensions/page/style.css)
+ *
+ * @module prosekit/extensions/page/style.css
+ */
+
+export {}

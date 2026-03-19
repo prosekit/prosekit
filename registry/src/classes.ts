55579e4fd60a319aa186c3a59c9a549da1c03e55
ocavue <ocavue@gmail.com>
2026-03-19 23:02:09 +1100

    color

--- registry/src/classes.ts
+++ registry/src/classes.ts
@@ -294,7 +294,7 @@ export const CSS_KEYMAP_FIELDSET = cn(
 export const CSS_TOGGLE_ROTATE = 'transition-transform data-rotate:rotate-90'
 
 export const CSS_ZOOM_SLIDER = cn(
-  'absolute top-4 left-4 flex items-center justify-center gap-1 border-gray-300 dark:border-gray-600 border p-2 text-sm text-gray-500 select-none print:hidden z-50 bg-gray-50 rounded',
+  'absolute top-4 left-4 flex items-center justify-center gap-1 border-gray-300 dark:border-gray-600 border p-2 text-sm text-gray-500 select-none print:hidden z-50 dark:bg-gray-800 bg-gray-100 rounded',
 )
 
 export const CSS_ZOOM_BUTTON = cn(

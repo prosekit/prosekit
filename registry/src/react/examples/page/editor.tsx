0af8be43d378b82d3e036f205b0845cbd2ee0090
ocavue <ocavue@gmail.com>
2026-03-21 23:52:48 +1100

    format

--- registry/src/react/examples/page/editor.tsx
+++ registry/src/react/examples/page/editor.tsx
@@ -34,7 +34,7 @@ export default function Editor(props: EditorProps) {
         <PaperController zoom={zoom} setZoom={setZoom} />
         <div
           data-editor-zoom="true"
-          style={{            '--zoom': zoom / 100          } as React.CSSProperties}
+          style={{ '--zoom': zoom / 100 } as React.CSSProperties}
           ref={editor.mount}
           className={clsx('ProseMirror box-border min-h-full m-0 p-10 print:p-0 outline-hidden')}
         />

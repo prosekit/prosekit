014bc291ea3b4ebeda5b78404f614e43930ba8bb
ocavue <ocavue@gmail.com>
2026-03-23 17:08:58 +1100

    clean

--- packages/svelte/src/components/mark-view-wrapper/props.ts
+++
@@ -1,7 +0,0 @@
-import type { Component } from 'svelte'
-
-import type { SvelteMarkViewProps } from '../../extensions/svelte-mark-view.ts'
-
-export interface MarkViewWrapperProps {
-  component?: Component<SvelteMarkViewProps>
-}

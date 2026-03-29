51e5e5c70677c2899a014aa758b09ebb8ea4c4d4
ocavue <ocavue@gmail.com>
2026-03-29 22:35:30 +1100

    export star

--- packages/lit/src/components/popover/index.gen.ts
+++
@@ -1,9 +0,0 @@
-export { OpenChangeEvent } from '@prosekit/web/popover'
-
-export { PopoverPopupElement as PopoverPopup } from '@prosekit/web/popover'
-
-export { PopoverPositionerElement as PopoverPositioner } from '@prosekit/web/popover'
-
-export { PopoverRootElement as PopoverRoot } from '@prosekit/web/popover'
-
-export { PopoverTriggerElement as PopoverTrigger } from '@prosekit/web/popover'
---
+++ packages/lit/src/components/resizable/index.ts
@@ -0,0 +1,2 @@
+export * from '@prosekit/web/resizable'
+

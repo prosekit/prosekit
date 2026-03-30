66938ce3cf2634272f2a123360b611a9ede00fff
ocavue <ocavue@gmail.com>
2026-03-30 16:10:49 +1100

    lit

--- packages/lit/src/components/autocomplete/index.gen.ts
+++
@@ -1,7 +0,0 @@
-export { AutocompleteEmptyElement as AutocompleteEmpty } from '@prosekit/web/autocomplete'
-
-export { AutocompleteItemElement as AutocompleteItem } from '@prosekit/web/autocomplete'
-
-export { AutocompleteListElement as AutocompleteList } from '@prosekit/web/autocomplete'
-
-export { AutocompletePopoverElement as AutocompletePopover } from '@prosekit/web/autocomplete'
--- packages/lit/src/components/popover/index.gen.ts
+++
@@ -1,5 +0,0 @@
-export { PopoverContentElement as PopoverContent } from '@prosekit/web/popover'
-
-export { PopoverRootElement as PopoverRoot } from '@prosekit/web/popover'
-
-export { PopoverTriggerElement as PopoverTrigger } from '@prosekit/web/popover'

3339a340820061d97181bd3498b1d6f68b23f228
ocavue <ocavue@gmail.com>
2026-04-01 13:53:22 +1100

    fix

--- packages/svelte/src/testing/image-refresh-view.svelte
+++ packages/svelte/src/testing/image-refresh-view.svelte
@@ -3,27 +3,24 @@ import { getId } from '@ocavue/utils'
 import { onDestroy, onMount } from 'svelte'
 
 import type { SvelteNodeViewProps } from '../svelte-node-view.ts'
-import { state } from './test-state.ts'
+import { state } from './image-refresh-state.ts'
 import { fromStore } from 'svelte/store'
 
 const props: SvelteNodeViewProps = $props()
 
 const node = $derived(fromStore(props.node))
-
 const url = $derived((node.current.attrs as { url: string }).url)
-
-let intervalId: ReturnType<typeof setInterval>
 
 $effect(() => {
   state.imageRefresh.mounted++
-  intervalId = setInterval(() => {
+  const id = setInterval(() => {
     state.imageRefresh.setAttrs++
     props.setAttrs({ url: String(getId()) })
   }, 50)
 
   return () => {
     state.imageRefresh.unmounted++
-    clearInterval(intervalId)
+    clearInterval(id)
   }
 })
 </script>

fcd4a68983c9bfa500f75db037f03eae13b8170f
ocavue <ocavue@gmail.com>
2026-04-10 20:10:30 +1000

    format

--- packages/web/src/components/inline-popover/inline-popover-positioner.ts
+++ packages/web/src/components/inline-popover/inline-popover-positioner.ts
@@ -14,8 +14,17 @@ import { InlinePopoverStoreContext } from './store.ts'
 /**
  * @public
  */
-export interface InlinePopoverPositionerProps
-  extends Omit<OverlayPositionerProps, 'placement' | 'offset' | 'hide' | 'hoist' | 'overlap' | 'inline' | 'overflowPadding'>
+export interface InlinePopoverPositionerProps extends
+  Omit<
+    OverlayPositionerProps,
+    | 'placement'
+    | 'offset'
+    | 'hide'
+    | 'hoist'
+    | 'overlap'
+    | 'inline'
+    | 'overflowPadding'
+  >
 {
   /**
    * The initial placement of the floating element

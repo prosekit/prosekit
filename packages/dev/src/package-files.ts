7bc7da80625f40f711e2efe72b2ce613131ff4e7
ocavue <ocavue@gmail.com>
2026-04-14 22:16:11 +1000

    remove useless files

--- packages/dev/src/package-files.ts
+++ packages/dev/src/package-files.ts
@@ -1,5 +1,3 @@
-import path from 'node:path'
-
 import type { Package } from '@manypkg/get-packages'
 
 import { vfs } from './vfs'
@@ -7,32 +5,4 @@ import { vfs } from './vfs'
 /** Removes generated files inside a package directory. */
 export async function cleanGeneratedFilesInPackage(pkg: Package): Promise<void> {
   await vfs.cleanFilesInDir(pkg.relativeDir, true)
-}
-
-/** Returns the first existing file inside a package from a list of candidates. */
-export async function findExistingFileInPackage(
-  pkg: Package,
-  relativeFilePaths: string[],
-): Promise<string | null> {
-  for (const relativeFilePath of relativeFilePaths) {
-    const filePath = path.join(pkg.relativeDir, relativeFilePath)
-    if (await vfs.pathExists(filePath)) {
-      return relativeFilePath
-    }
-  }
-  return null
-}
-
-/** Returns an existing file or throws if none of the candidates exist. */
-export async function getExistingFileInPackage(
-  pkg: Package,
-  relativeFilePaths: string[],
-): Promise<string> {
-  const relativeFilePath = await findExistingFileInPackage(pkg, relativeFilePaths)
-  if (!relativeFilePath) {
-    throw new Error(
-      `Failed to find file ${relativeFilePaths} in directory ${pkg.dir}`,
-    )
-  }
-  return relativeFilePath
 }

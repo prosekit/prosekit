57d60b972abd5bc13b8c8f67466796ea19bb2595
ocavue <ocavue@gmail.com>
2026-05-04 19:41:51 +1000

    fix

--- registry/src/solid/examples/text-color/inline-menu.tsx
+++ registry/src/solid/examples/text-color/inline-menu.tsx
@@ -91,7 +91,7 @@ export default function InlineMenu(): JSX.Element {
     >
       <InlinePopoverPositioner class="CSS_INLINE_MENU_POSITIONER">
         <InlinePopoverPopup class="CSS_INLINE_MENU_MAIN_POPUP">
-          <div class="flex flex-col gap-4 p-4">
+          <div class="flex flex-col gap-4 p-4 w-60">
             <div class="flex flex-col gap-2">
               <div class="text-sm">Text color</div>
               <div class="grid grid-cols-5 gap-1">

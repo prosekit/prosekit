c124699232db7c95c814a48da32bcde3b3187175
ocavue <ocavue@gmail.com>
2026-05-08 21:26:13 +1000

    update

--- packages/web/src/components/autocomplete/autocomplete-root.ts
+++ packages/web/src/components/autocomplete/autocomplete-root.ts
@@ -51,7 +51,10 @@ export interface AutocompleteRootProps {
   filter: ItemFilter | null
 
   /**
-   * An element to position the popup against. By default, the popup will be positioned against the text content that triggers the autocomplete.
+   * The reference to position the popup against. This can be a DOM element, a
+   * Floating UI virtual element, or a function that returns either of them.
+   * By default, the popup will be positioned against the text content that
+   * triggers the autocomplete.
    *
    * @default null
    */

3a956c5fcd2dd73b429b5f1b11e0a233ee3b728d
shlroland <shlroland1995@gmail.com>
2026-05-23 22:36:41 +0800

    chore: add @internal comments

--- packages/core/src/utils/is-code-block-type.ts
+++ packages/core/src/utils/is-code-block-type.ts
@@ -1,5 +1,8 @@
 import type { NodeType } from '@prosekit/pm/model'
 
+/**
+ * @internal
+ */
 export function isCodeBlockType(type: NodeType): boolean {
   return !!(type.spec.code && type.isBlock)
 }

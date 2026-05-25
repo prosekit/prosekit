0d94e5440394d0f2bd4ef1956553c85de97b35b4
shlroland <shlroland1995@gmail.com>
2026-05-25 11:17:30 +0800

    fix(prosekit): add columns style css stub module

---
+++ packages/prosekit/src/extensions/columns/style-css.ts
@@ -0,0 +1,9 @@
+/**
+ * Some basic styles for the columns extension.
+ *
+ * [view source code](https://unpkg.com/prosekit/extensions/columns/style.css)
+ *
+ * @module prosekit/extensions/columns/style.css
+ */
+
+export {}

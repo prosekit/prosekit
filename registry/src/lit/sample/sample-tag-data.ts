29a2e5cc0ffb55b472fd8c1df71c645a61f4b77c
Patrick de Klein <patrick.deklein@cito.nl>
2026-06-01 23:13:20 +0200

    feat(registry): add lit full example (#1665)

---
+++ registry/src/lit/sample/sample-tag-data.ts
@@ -0,0 +1,12 @@
+export const tags = [
+  { id: 1, label: 'book' },
+  { id: 2, label: 'movie' },
+  { id: 3, label: 'trip' },
+  { id: 4, label: 'music' },
+  { id: 5, label: 'art' },
+  { id: 6, label: 'food' },
+  { id: 7, label: 'sport' },
+  { id: 8, label: 'technology' },
+  { id: 9, label: 'fashion' },
+  { id: 10, label: 'nature' },
+]

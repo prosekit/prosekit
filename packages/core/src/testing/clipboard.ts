c1f4e5caf4e0a3d7ff10c5ae08ffb00b1dcef683
ocavue <ocavue@gmail.com>
2026-06-05 14:16:58 +1000

    PR_REVIEW

--- packages/core/src/testing/clipboard.ts
+++ packages/core/src/testing/clipboard.ts
@@ -19,3 +19,19 @@ export function pasteHTML(view: EditorView, html: string) {
   const event = new ClipboardEvent('paste', { clipboardData })
   view.pasteHTML(html, event)
 }
+
+
+/*
+
+PR_REVIEW: since we now need pasteText pasteHTML in multiple packages, let's just move them to prosekit/test so that they are available for all packages to use. Some rules to follow:
+
+1. add some JSDoc comments to these functions so that it's clear what they do and how to use them
+2. still keep them in a separate file (packages/core/src/test/clipboard.ts) and re-export them from /private/tmppackages/core/src/test/index.ts
+3. these two function should still have @internal tag in their JSDoc comments, since I don't want to maintaine the API compatibility for them
+4. avoid duplicated definitions of these functions in different packages
+5. always git commit and git push the changes to the remote branch after your changes are done
+
+
+
+
+*/

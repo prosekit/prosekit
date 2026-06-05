eb29f6a3026094aa0c4f1a23f837665d851e2467
ocavue <ocavue@users.noreply.github.com>
2026-06-05 22:12:37 +1000

    feat(pm): export EditorNode and EditorFragment aliases (#1674)

--- packages/pm/src/model.ts
+++ packages/pm/src/model.ts
@@ -1,2 +1,2 @@
 export * from 'prosemirror-model'
-export { Fragment as ProseMirrorFragment, Node as ProseMirrorNode } from 'prosemirror-model'
+export { Fragment as EditorFragment, Fragment as ProseMirrorFragment, Node as EditorNode, Node as ProseMirrorNode } from 'prosemirror-model'

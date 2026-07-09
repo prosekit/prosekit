b6a3d0aa77c00cb46af8096227ec11ee51951e29
ocavue <ocavue@gmail.com>
2026-07-09 18:53:01 +1000

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,7 @@ export default defineESLintConfig(
   },
   {
     rules: {
-      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v63.0.0/docs/rules/consistent-function-scoping.md
+      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v71.0.0/docs/rules/consistent-function-scoping.md
       'unicorn/consistent-function-scoping': 'error',
     },
     ignores: ['**/*.test.*', '**/*.spec.*', '**/*.md', '**/*.md/**/*'],

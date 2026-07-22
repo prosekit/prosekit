cd603f13a81040110f2138b274eb99b80e489b3f
ocavue <ocavue@gmail.com>
2026-07-23 04:42:56 +1000

    revert(virtual-selection): revert native selection removal

--- packages/extensions/src/virtual-selection/index.ts
+++ packages/extensions/src/virtual-selection/index.ts
@@ -1,6 +1,7 @@
 import { definePlugin, type PlainExtension } from '@prosekit/core'
 import { PluginKey, ProseMirrorPlugin, type EditorState, type Transaction } from '@prosekit/pm/state'
-import { Decoration, DecorationSet, EditorView } from '@prosekit/pm/view'
+import type { EditorView } from '@prosekit/pm/view'
+import { Decoration, DecorationSet } from '@prosekit/pm/view'
 
 /**
  * @internal

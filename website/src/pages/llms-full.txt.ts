b73ee105681aaa38b3cd7b5245ceaa273fc8df38
ocavue <ocavue@gmail.com>
2026-08-12 17:34:08 +1000

    docs: remove llms.txt

--- website/src/pages/llms-full.txt.ts
+++
@@ -1,7 +0,0 @@
-import type { APIRoute } from 'astro'
-
-import { getLlmText } from '@/content/llms/llms'
-
-export const GET: APIRoute = async () => {
-  return new Response(await getLlmText('full'))
-}
